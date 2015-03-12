31a1af288f33b6effb1e3caa552b0f8cd471699b
JefWight <JefWight@microsoft.com>
2015-03-12 13:09:21 -0700

    Parse $skipToken in ODataUri
    Adds support for parsing the $skipToken parameter in the ODataUri

--- src/Microsoft.OData.Core/UriParser/ODataQueryOptionParser.cs
+++ src/Microsoft.OData.Core/UriParser/ODataQueryOptionParser.cs
@@ -231,6 +231,16 @@ namespace Microsoft.OData.Core.UriParser
             this.searchClause = ParseSearchImplementation(searchQuery, this.Configuration);
             return searchClause;
         }
+
+        /// <summary>
+        /// Parses a $skipToken query option
+        /// </summary>
+        /// <returns>A value representing that skip token option, null if $skipToken query does not exist.</returns>
+        public string ParseSkipToken()
+        {
+            string skipTokenQuery;
+            return this.TryGetQueryOption(UriQueryConstants.SkipTokenQueryOption, out skipTokenQuery) ? skipTokenQuery : null;
+        }
         #endregion public methods
 
         #region private methods
