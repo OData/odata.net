6e82527e069c345c6ff6283d15634cf5601847b1
Congyong Su <cysu@microsoft.com>
2015-11-30 22:01:40 +0800

    Build with VS2015
    Resolve #384
    Also skip strong name validation in a community build #377
    Default to incremental build

--- build.ps1
+++ build.ps1
@@ -8,53 +8,118 @@ elseif ($args[0] -match 'Rolling')
     $TestType = "Rolling"
     $CONFIGURATION = 'Debug'
 }
+elseif ($args[0] -match 'DisableSkipStrongName')
+{
+    $TestType = "DisableSkipStrongName"
+    $CONFIGURATION = 'Debug'
+}
+elseif ($args[0] -match 'SkipStrongName')
+{
+    $TestType = "SkipStrongName"
+    $CONFIGURATION = 'Debug'
+}
 else 
 {
     Write-Host 'Please choose Nightly Test or Rolling Test!' -ForegroundColor Red
     exit
-}  
+}
+
 $PROGRAMFILESX86 = [Environment]::GetFolderPath("ProgramFilesX86")
 $env:ENLISTMENT_ROOT = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $ENLISTMENT_ROOT = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $LOGDIR = $ENLISTMENT_ROOT + "\bin"
+
+# Default to use Visual Studio 2013.
 $MSBUILD = $PROGRAMFILESX86 + "\MSBuild\12.0\Bin\MSBuild.exe"
 $MSTEST = $PROGRAMFILESX86 + "\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe"
 $FXCOPDIR = $PROGRAMFILESX86 + "\Microsoft Visual Studio 12.0\Team Tools\Static Analysis Tools\FxCop"
+$SN = $PROGRAMFILESX86 + "\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\sn.exe"
+$SNx64 = $PROGRAMFILESX86 + "\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\x64\sn.exe"
+
+# Fall back to Visual Studio 2015.
+if (!(Test-Path $MSBUILD))
+{
+    $MSBUILD = $PROGRAMFILESX86 + "\MSBuild\14.0\Bin\MSBuild.exe"
+}
+
+if (!(Test-Path $MSTEST))
+{
+    $MSTEST = $PROGRAMFILESX86 + "\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe"
+}
+
+if (!(Test-Path $FXCOPDIR))
+{
+    $FXCOPDIR = $PROGRAMFILESX86 + "\Microsoft Visual Studio 14.0\Team Tools\Static Analysis Tools\FxCop"
+}
+
 $FXCOP = $FXCOPDIR + "\FxCopCmd.exe"
 $BUILDLOG = $LOGDIR + "\msbuild.log"
 $TESTLOG = $LOGDIR + "\mstest.log"
 $TESTDIR = $ENLISTMENT_ROOT + "\bin\AnyCPU\$CONFIGURATION\Test\Desktop"
 $PRODUCTDIR = $ENLISTMENT_ROOT + "\bin\AnyCPU\$Configuration\Product\Desktop"
 $NUGETPACK = $ENLISTMENT_ROOT + "\sln\packages"
-$RollingTestSuite = "/testcontainer:Microsoft.Test.Data.Services.DDBasics.dll", 
-        "/testcontainer:Microsoft.OData.Client.Design.T4.UnitTests.dll", 
-        "/testcontainer:AstoriaUnitTests.TDDUnitTests.dll", 
-        "/testcontainer:EdmLibTests.dll", 
-        "/testcontainer:Microsoft.OData.Client.TDDUnitTests.dll", 
-        "/testcontainer:Microsoft.Spatial.TDDUnitTests.dll", 
-        "/testcontainer:Microsoft.Test.Edm.TDD.Tests.dll", 
-        "/testcontainer:Microsoft.Test.OData.TDD.Tests.dll", 
-        "/testcontainer:Microsoft.Test.OData.Query.TDD.Tests.dll", 
-        "/testcontainer:Microsoft.Test.Taupo.OData.Common.Tests.dll", 
-        "/testcontainer:Microsoft.Test.Taupo.OData.Query.Tests.dll", 
-        "/testcontainer:Microsoft.Test.Taupo.OData.Reader.Tests.dll", 
-        "/testcontainer:Microsoft.Test.Taupo.OData.Writer.Tests.dll", 
-        "/testcontainer:Microsoft.Test.Taupo.OData.Scenario.Tests.dll", 
-        "/testcontainer:AstoriaUnitTests.ClientCSharp.dll", 
-        "/testcontainer:Microsoft.Data.NamedStream.UnitTests.dll", 
-        "/testcontainer:Microsoft.Data.ServerUnitTests1.UnitTests.dll", 
-        "/testcontainer:Microsoft.Data.ServerUnitTests2.UnitTests.dll", 
-        "/testcontainer:RegressionUnitTests.dll", 
-        "/testcontainer:Microsoft.Test.OData.PluggableFormat.Tests.dll"
-$AddtionalNightlyTestSuite = "/testcontainer:Microsoft.Data.MetadataObjectModel.UnitTests.dll", 
-        "/testcontainer:AstoriaUnitTests.dll", 
-        "/testcontainer:AstoriaClientUnitTests.dll", 
-        "/testcontainer:Microsoft.Test.OData.User.Tests.dll", 
-        "/testcontainer:TestCategoryAttributeCheck.dll"
-[System.Collections.ArrayList]$NightlyTestSuite=$RollingTestSuite
-foreach ($test in $AddtionalNightlyTestSuite) {[void]$NightlyTestSuite.Add($test)}
 
-$E2eTestSuite = ,"/testcontainer:Microsoft.Test.OData.Tests.Client.dll"
+$ProductDlls = "Microsoft.OData.Client.dll",
+    "Microsoft.OData.Core.dll",
+    "Microsoft.OData.Edm.dll",
+    "Microsoft.OData.Service.Design.T4.dll",
+    "Microsoft.Spatial.dll"
+
+$TestDlls = "Microsoft.OData.Service.Design.T4.dll",
+    "Microsoft.OData.Service.dll",
+    "Microsoft.OData.Service.Test.Common.dll"
+
+$RollingTestDlls = "Microsoft.Test.Data.Services.DDBasics.dll", 
+    "Microsoft.OData.Client.Design.T4.UnitTests.dll", 
+    "AstoriaUnitTests.TDDUnitTests.dll", 
+    "EdmLibTests.dll", 
+    "Microsoft.OData.Client.TDDUnitTests.dll", 
+    "Microsoft.Spatial.TDDUnitTests.dll", 
+    "Microsoft.Test.Edm.TDD.Tests.dll", 
+    "Microsoft.Test.OData.TDD.Tests.dll", 
+    "Microsoft.Test.OData.Query.TDD.Tests.dll", 
+    "Microsoft.Test.Taupo.OData.Common.Tests.dll", 
+    "Microsoft.Test.Taupo.OData.Query.Tests.dll", 
+    "Microsoft.Test.Taupo.OData.Reader.Tests.dll", 
+    "Microsoft.Test.Taupo.OData.Writer.Tests.dll", 
+    "Microsoft.Test.Taupo.OData.Scenario.Tests.dll", 
+    "AstoriaUnitTests.ClientCSharp.dll", 
+    "Microsoft.Data.NamedStream.UnitTests.dll", 
+    "Microsoft.Data.ServerUnitTests1.UnitTests.dll", 
+    "Microsoft.Data.ServerUnitTests2.UnitTests.dll", 
+    "RegressionUnitTests.dll", 
+    "Microsoft.Test.OData.PluggableFormat.Tests.dll"
+
+$RollingTestSuite = @()
+ForEach($dll in $RollingTestDlls)
+{
+    $RollingTestSuite += "/testcontainer:" + $dll
+}
+
+$AdditionalNightlyTestDlls = "Microsoft.Data.MetadataObjectModel.UnitTests.dll", 
+        "AstoriaUnitTests.dll", 
+        "AstoriaClientUnitTests.dll", 
+        "Microsoft.Test.OData.User.Tests.dll", 
+        "TestCategoryAttributeCheck.dll"
+
+ForEach($dll in $AdditionalNightlyTestDlls)
+{
+    $AdditionalNightlyTestSuite += "/testcontainer:" + $dll
+}
+
+$NightlyTestSuite = $RollingTestSuite
+ForEach ($test in $AddtionalNightlyTestSuite)
+{
+    $NightlyTestSuite += $test
+}
+
+$E2eTestDlls = ,"Microsoft.Test.OData.Tests.Client.dll"
+
+ForEach ($dll in $E2eTestDlls)
+{
+    $E2eTestSuite += " /testcontainer:" + $dll
+}
+
 $FxCopRulesOptions = "/rule:$FxCopDir\Rules\DesignRules.dll",
         "/rule:$FxCopDir\Rules\NamingRules.dll",
         "/rule:$FxCopDir\Rules\PerformanceRules.dll",
@@ -70,14 +135,89 @@ $FxCopRulesOptions = "/rule:$FxCopDir\Rules\DesignRules.dll",
         "/ruleid:-Microsoft.Performance#CA1814"
 $DataWebRulesOption = "/rule:$TESTDIR\DataWebRules.dll"
 
+Function GetDlls
+{
+    $dlls = @()
+
+    ForEach($dll in $ProductDlls)
+    {
+        $dlls += $PRODUCTDIR + "\" + $dll
+    }
+
+    ForEach($dll in $TestDlls)
+    {
+        $dlls += $TESTDIR + "\" + $dll
+    }
+    
+    ForEach($dll in $RollingTestDlls)
+    {
+        $dlls += $TESTDIR + "\" + $dll
+    }
+
+    ForEach($dll in $AdditionalNightlyTestDlls)
+    {
+        $dlls += $TESTDIR + "\" + $dll
+    }
+
+    ForEach($dll in $E2eTestDlls)
+    {
+        $dlls += $TESTDIR + "\" + $dll
+    }
+
+    return $dlls
+}
+
+Function SkipStrongName
+{
+    $SnLog = $LOGDIR + "\SkipStrongName.log"
+    Out-File $SnLog
+
+    Write-Host 'Skip strong name validations for ODataLib assemblies...'
+
+    $dlls = GetDlls
+    ForEach ($dll in $dlls)
+    {
+        & $SN /Vr $dll | Out-File $SnLog -Append
+    }
+
+    ForEach ($dll in $dlls)
+    {
+        & $SNx64 /Vr $dll | Out-File $SnLog -Append
+    }
+
+    Write-Host "SkipStrongName Done" -ForegroundColor Green
+}
+
+Function DisableSkipStrongName
+{
+    $SnLog = $LOGDIR + "\DisableSkipStrongName.log"
+    Out-File $SnLog
+
+    Write-Host 'Disable skip strong name validations for ODataLib assemblies...'
+
+    $dlls = GetDlls
+    ForEach ($dll in $dlls)
+    {
+        & $SN /Vu $dll | Out-File $SnLog -Append
+    }
+
+    ForEach ($dll in $dlls)
+    {
+        & $SNx64 /Vu $dll | Out-File $SnLog -Append
+    }
+
+    Write-Host "DisableSkipStrongName Done" -ForegroundColor Green
+}
+
 Function Cleanup 
 {    
-    cd $ENLISTMENT_ROOT\tools\Scripts    
+    cd $ENLISTMENT_ROOT\tools\Scripts
     Write-Host "Dropping stale databases..."
     cscript "$ENLISTMENT_ROOT\tools\Scripts\artdbclean.js" //Nologo
     cd $ENLISTMENT_ROOT
     Write-Host "Clean Done" -ForegroundColor Yellow
 }
+
 Function CleanBeforeScorch
 {
     Write-Host 'killing TaupoAstoriaRunner as it should no longer be running'
@@ -106,6 +246,8 @@ Function CleanBeforeScorch
     
     Write-Host "Clean Done" -ForegroundColor Yellow
 }
+
+# Incremental build
 Function RunBuild ($sln)
 {
     Write-Host "*** Building $sln ***"
@@ -118,7 +260,37 @@ Function RunBuild ($sln)
     {
         $Conf = "/p:Configuration=Debug"
     }
-    & $MSBUILD $slnpath /t:rebuild /m /nr:false /fl "/p:Platform=Any CPU" $Conf /p:Desktop=true /flp:LogFile=$LOGDIR/msbuild.log /flp:Verbosity=Normal 1>$null 2>$null
+    & $MSBUILD $slnpath /t:build /m /nr:false /fl "/p:Platform=Any CPU" $Conf /p:Desktop=true `
+        /flp:LogFile=$LOGDIR/msbuild.log /flp:Verbosity=Normal 1>$null 2>$null
+    if($LASTEXITCODE -eq 0)
+    {
+        Write-Host "Build $sln SUCCESS" -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "Build $sln FAILED" -ForegroundColor Red
+        Write-Host "For more information, please open the following test result files:"
+        Write-Host "$LOGDIR\msbuild.log"
+        Cleanup
+        exit
+    }
+}
+
+# Rebuild
+Function RunRebuild ($sln)
+{
+    Write-Host "*** Building $sln ***"
+    $slnpath = $ENLISTMENT_ROOT + "\sln\$sln"
+    if ($TestType -eq 'Nightly')
+    {
+        $Conf = "/p:Configuration=Release"
+    }
+    else
+    {
+        $Conf = "/p:Configuration=Debug"
+    }
+    & $MSBUILD $slnpath /t:rebuild /m /nr:false /fl "/p:Platform=Any CPU" $Conf /p:Desktop=true `
+        /flp:LogFile=$LOGDIR/msbuild.log /flp:Verbosity=Normal 1>$null 2>$null
     if($LASTEXITCODE -eq 0)
     {
         Write-Host "Build $sln SUCCESS" -ForegroundColor Green
@@ -137,6 +309,7 @@ Function RestoringFile ($file , $target)
     Write-Host "Restoring $file"
     Copy-Item -Path $file -Destination $target -Force
 }
+
 Function FailedTestLog ($playlist , $reruncmd , $failedtest1 ,$failedtest2)
 {    
     Write-Output "<Playlist Version=`"1.0`">" | Out-File $playlist
@@ -196,6 +369,7 @@ Function FailedTestLog ($playlist , $reruncmd , $failedtest1 ,$failedtest2)
     Write-Host "To rerun failed tests, please run the following script:" -ForegroundColor Red
     Write-Host $reruncmd -ForegroundColor Red
 }
+
 Function TestSummary
 {
     Write-Host 'Collecting test results'
@@ -261,15 +435,18 @@ Function TestSummary
         Write-Host "Congratulation! All of the tests passed!" -ForegroundColor Green
     }
 }
-Function RunTest ($title , $testdir)
+
+Function RunTest ($title, $testdir)
 {
     Write-Host "**********Running $title***********"
+    Write-Host "$testdir"
     & $MSTEST $testdir >> $TESTLOG
     if($LASTEXITCODE -ne 0)
     {
         Write-Host "Run $title FAILED" -ForegroundColor Red
     }
 }
+
 Function BuildProcess
 {
     Write-Host '**********Start To Build The Project*********'
@@ -287,6 +464,7 @@ Function BuildProcess
     Write-Host "Build Done" -ForegroundColor Yellow
     $script:BUILD_END_TIME = Get-Date
 }
+
 Function TestProcess
 {
     Write-Host '**********Start To Run The Test*********'
@@ -318,6 +496,7 @@ Function TestProcess
     $script:TEST_END_TIME = Get-Date
     cd $ENLISTMENT_ROOT
 }
+
 Function FxCopProcess
 {
     Write-Host '**********Start To FxCop*********'
@@ -338,8 +517,25 @@ if (! (Test-Path $LOGDIR))
 {
     mkdir $LOGDIR 1>$null
 }
+
+if ($TestType -eq 'SkipStrongName')
+{
+    CleanBeforeScorch 
+    BuildProcess
+    SkipStrongName
+    Exit
+}
+elseif ($TestType -eq 'DisableSkipStrongName')
+{
+    CleanBeforeScorch 
+    BuildProcess
+    DisableSkipStrongName
+    Exit
+}
+
 CleanBeforeScorch 
 BuildProcess
+SkipStrongName
 TestProcess
 FxCopProcess
 Cleanup
