93bad387a9567abbd69242fc6c5e4b837ca85b97
Lewis Cheng <licheng@microsoft.com>
2016-04-22 17:50:48 +0800

    Remove ASTORIA_OPEN_OBJECT

--- src/Microsoft.OData.Client/Materialization/ODataLoadNavigationPropertyMaterializer.cs
+++ src/Microsoft.OData.Client/Materialization/ODataLoadNavigationPropertyMaterializer.cs
@@ -108,12 +108,7 @@ namespace Microsoft.OData.Client.Materialization
                     this.EntityTrackingAdapter.MaterializationLog.SetLink(entry, property.PropertyName, target);
 
                     // Singleton entity property
-#if ASTORIA_OPEN_OBJECT
-                    object openProperties = null;
-                    property.SetValue(entity, target, property.PropertyName, ref openProperties, false);
-#else
                     property.SetValue(entity, target, property.PropertyName, false);
-#endif
                 }
 
                 // Apply the materialization log.
