d899638baf6d1a0a481b21e4dd06e216a84558eb
Lingxi-Li <lingxl@microsoft.com>
2016-07-14 09:40:41 +0800

    Fix test VerifyGeneratedCodeString

--- test/EndToEndTests/Services/CSReferences/Microsoft.Test.OData.Services.TestServices.PrimitiveKeysServiceReference.cs
+++ test/EndToEndTests/Services/CSReferences/Microsoft.Test.OData.Services.TestServices.PrimitiveKeysServiceReference.cs
@@ -536,7 +536,7 @@ namespace Microsoft.Test.OData.Services.TestServices.PrimitiveKeysServiceReferen
                 global::System.Xml.XmlReader reader = CreateXmlReader(Edmx);
                 try
                 {
-                    return global::Microsoft.OData.Edm.Csdl.EdmxReader.Parse(reader);
+                    return global::Microsoft.OData.Edm.Csdl.CsdlReader.Parse(reader);
                 }
                 finally
                 {
