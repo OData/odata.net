88bdb157bfe41d91adca2ad6bd45c658c43052c3
Biao Li <biaol@microsoft.com>
2017-07-12 12:34:04 -0700

    CR updates.

--- src/Microsoft.Spatial/GeoJsonWriterBase.cs
+++ src/Microsoft.Spatial/GeoJsonWriterBase.cs
@@ -449,6 +449,12 @@ namespace Microsoft.Spatial
         /// </summary>
         private void WriteCrs()
         {
+            // "crs": {
+            //      "type": "name",
+            //      "properties": {
+            //          "name": "EPSG:4326"
+            //      }
+            // }
             this.AddPropertyName(GeoJsonConstants.CrsMemberName);
             this.StartObjectScope();
             this.AddPropertyName(GeoJsonConstants.TypeMemberName);
