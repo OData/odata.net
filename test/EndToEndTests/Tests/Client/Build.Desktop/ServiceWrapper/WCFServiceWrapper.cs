15de9a5e927785bec2215caee9f08f173c88f2d9
Alan Wong <alawon@microsoft.com>
2017-10-04 19:16:05 -0700

    Adding two files that didn't commit

---
+++ test/EndToEndTests/Tests/Client/Build.Desktop/ServiceWrapper/WCFServiceWrapper.cs
@@ -0,0 +1,47 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="WCFServiceWrapper.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+namespace Microsoft.Test.OData.Framework.Server
+{
+    using System;
+
+    /// <summary>
+    /// Implementation of the IServiceWrapper. This class is utilized to communicate with the service via RPC-like calls.
+    /// </summary>
+    public class WCFServiceWrapper : IServiceWrapper
+    {
+        /// <summary>
+        /// Initializes a new instance of the WCFServiceWrapper class.
+        /// </summary>
+        /// <param name="descriptor">Descriptor for the service to wrap.</param>
+        public WCFServiceWrapper(ServiceDescriptor descriptor)
+        {
+            // TODO: Save service URI from response.
+            // this.ServiceUri = descriptor.CreateServiceUri();
+        }
+
+        /// <summary>
+        /// Gets the URI for the service.
+        /// </summary>
+        public Uri ServiceUri { get; private set; }
+
+        /// <summary>
+        /// Starts the service.
+        /// </summary>
+        public void StartService()
+        {
+            // TODO: Perform RPC call to start service.
+        }
+
+        /// <summary>
+        /// Stops the service.
+        /// </summary>
+        public void StopService()
+        {
+            // TODO: Perform RPC call to stop service.
+        }
+    }
+}
