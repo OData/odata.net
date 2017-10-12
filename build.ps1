67bd6dd82c0223ee8962ba739bd080859825407f
Alan Wong <alawon@microsoft.com>
2017-10-11 18:46:45 -0700

    Removing OData.Tests.NetStandard.VS2017.sln from build command because it's repetitive with the E2E NetCore tests

--- build.ps1
+++ build.ps1
@@ -540,7 +540,6 @@ Function BuildProcess
         if($VS15MSBUILD)
         {
             Write-Host "Found VS2017 version: $VS15MSBUILD"
-            RunBuild ('OData.Tests.NetStandard.VS2017.sln') -vsToolVersion '15.0'
             RunBuild ('OData.Tests.E2E.NetCore.VS2017.sln') -vsToolVersion '15.0'
         }
         else
