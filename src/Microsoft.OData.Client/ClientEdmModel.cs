571193d535c712faa96dd39be021c7e16bf89c34
ttan <ttan@ermpower.com.au>
2017-11-16 10:20:30 +1100

    Use ConcurrentDictionary in all platforms to make client edm model thread safe.

--- src/Microsoft.OData.Client/ClientEdmModel.cs
+++ src/Microsoft.OData.Client/ClientEdmModel.cs
@@ -21,13 +21,7 @@ namespace Microsoft.OData.Client
     using Microsoft.OData.Edm;
     using Microsoft.OData.Edm.Vocabularies;
     using c = Microsoft.OData.Client;
-
-#if PORTABLELIB
-    // Windows Phone 8.0 doesn't support ConcurrentDictionary
-    using ConcurrentEdmSchemaDictionary = System.Collections.Generic.Dictionary<string, Edm.IEdmSchemaElement>;
-#else
     using ConcurrentEdmSchemaDictionary = System.Collections.Concurrent.ConcurrentDictionary<string, Edm.IEdmSchemaElement>;
-#endif
 
     #endregion Namespaces.
 
