d95f48de5f062273a68147d51f97dff9dae65baa
Yogesh Sanghi <yosan@microsoft.com>
2018-06-12 22:08:53 -0700

    Fixing some minor test project issues.

--- test/FunctionalTests/Microsoft.OData.Core.Tests/BufferUtilsTests.cs
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/BufferUtilsTests.cs
@@ -1,5 +1,5 @@
 ﻿//---------------------------------------------------------------------
-// <copyright file="BatchReferenceSegmentTests.cs" company="Microsoft">
+// <copyright file="BufferUtilsTests.cs" company="Microsoft">
 //      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 // </copyright>
 //---------------------------------------------------------------------
