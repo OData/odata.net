4b9119ffd05fe3d7df2b943b80232601d8fd60db
Yogesh Sanghi <yosan@microsoft.com>
2018-06-13 17:07:45 -0700

    Make buffer utils internal

--- src/Microsoft.OData.Core/Buffers/BufferUtils.cs
+++ src/Microsoft.OData.Core/Buffers/BufferUtils.cs
@@ -1,4 +1,5 @@
 ﻿//---------------------------------------------------------------------
+// <copyright file="BufferUtils.cs" company="Microsoft">
 //      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 // </copyright>
 //---------------------------------------------------------------------
