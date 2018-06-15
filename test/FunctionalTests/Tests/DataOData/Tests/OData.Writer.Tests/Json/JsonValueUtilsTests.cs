66ac7ef0b996910b49edf7a1ee50cafad545b927
Yogesh Sanghi <yosan@microsoft.com>
2018-06-15 13:27:18 -0700

    One more attempt to fix the failing test cases.

--- test/FunctionalTests/Tests/DataOData/Tests/OData.Writer.Tests/Json/JsonValueUtilsTests.cs
+++ test/FunctionalTests/Tests/DataOData/Tests/OData.Writer.Tests/Json/JsonValueUtilsTests.cs
@@ -9,6 +9,7 @@ namespace Microsoft.Test.Taupo.OData.Writer.Tests.Json
     using System;
     using System.IO;
     using System.Text;
+
     using Microsoft.Test.OData.Utils.CombinatorialEngine;
     using Microsoft.Test.Taupo.Execution;
     using Microsoft.Test.Taupo.OData.Common;
@@ -283,7 +284,7 @@ namespace Microsoft.Test.Taupo.OData.Writer.Tests.Json
             public static void WriteValue(TextWriter writer, TimeSpan value) { ReflectionUtils.InvokeMethod(classType, "WriteValue", writer, value); }
             public static void WriteValue(TextWriter writer, byte value) { ReflectionUtils.InvokeMethod(classType, "WriteValue", writer, value); }
             public static void WriteValue(TextWriter writer, sbyte value) { ReflectionUtils.InvokeMethod(classType, "WriteValue", writer, value); }
-            public static void WriteValue(TextWriter writer, string value) { char[] buffer = null; ReflectionUtils.InvokeMethod(classType, "WriteValue", new Type[] { typeof(TextWriter), typeof(string), typeof(char[]) }, writer, value, buffer); }
+            public static void WriteValue(TextWriter writer, string value) { char[] buffer = null; ReflectionUtils.InvokeMethod(classType, "WriteValue", new Type[] { typeof(TextWriter), typeof(string), typeof(char[]).MakeByRefType() }, writer, value, buffer); }
         }
 #endif
     }
