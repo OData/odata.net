77c46e2a44fe894e5523496584ce7ddd48d4f620
Yogesh Sanghi <yosan@microsoft.com>
2018-06-21 14:36:47 -0700

    Avoid String allocations when writing strings that require special character handling  (#1192)
    JsonWriter allocates a lot of strings when dealing with special characters that needs handling for JSON compatibility. We want to avoid allocating string to help with GC.
    
    * Avoid use of string builder in GetProperty to reduce string allocations.
    * Use a local char buffer when writing to underlying text writer

--- test/FunctionalTests/Microsoft.OData.Core.Tests/Json/NonIndentedTextWriterTests.cs
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/Json/NonIndentedTextWriterTests.cs
@@ -17,7 +17,7 @@ namespace Microsoft.OData.Tests.Json
     {
         private MemoryStream stream;
         private TextWriterWrapper writer;
-
+        private char[] buffer;
         private Dictionary<string, string> escapedCharMap = new Dictionary<string, string>()
         {
             {"\r\n", "\\r\\n"}, 
@@ -36,7 +36,7 @@ namespace Microsoft.OData.Tests.Json
         public void WriteEmptyStringShouldWork()
         {
             this.TestInit();
-            JsonValueUtils.WriteEscapedJsonString(this.writer, string.Empty);
+            JsonValueUtils.WriteEscapedJsonString(this.writer, string.Empty, ref this.buffer);
             this.StreamToString().Should().Be("\"\"");
         }
 
@@ -44,7 +44,7 @@ namespace Microsoft.OData.Tests.Json
         public void WriteNonSpecialCharactersShouldWork()
         {
             this.TestInit();
-            JsonValueUtils.WriteEscapedJsonString(this.writer, "abcdefg123");
+            JsonValueUtils.WriteEscapedJsonString(this.writer, "abcdefg123", ref this.buffer);
             this.StreamToString().Should().Be("\"abcdefg123\"");
         }
 
@@ -54,7 +54,7 @@ namespace Microsoft.OData.Tests.Json
             foreach (string specialChar in this.escapedCharMap.Keys)
             {
                 this.TestInit();
-                JsonValueUtils.WriteEscapedJsonString(this.writer, string.Format("{0}", specialChar));
+                JsonValueUtils.WriteEscapedJsonString(this.writer, string.Format("{0}", specialChar), ref this.buffer);
                 this.StreamToString().Should().Be(string.Format("\"{0}\"", this.escapedCharMap[specialChar]));
             }
         }
