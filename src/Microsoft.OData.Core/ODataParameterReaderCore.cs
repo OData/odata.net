a15b95bdd08ed7111a6075c4f15fed3a8bc70bfc
Sam Xu <saxu@microsoft.com>
2018-08-01 17:08:36 -0700

    Fix issue #1216, parameter read should not throw exception if missing optional parameter

--- src/Microsoft.OData.Core/ODataParameterReaderCore.cs
+++ src/Microsoft.OData.Core/ODataParameterReaderCore.cs
@@ -280,9 +280,14 @@ this.State == ODataParameterReaderState.Collection,
                     // Note that the binding parameter will be specified on the Uri rather than the payload, skip the binding parameter.
                     foreach (IEdmOperationParameter parameter in this.Operation.Parameters.Skip(this.Operation.IsBound ? 1 : 0))
                     {
-                        if (!this.parametersRead.Contains(parameter.Name) && !this.inputContext.EdmTypeResolver.GetParameterType(parameter).IsNullable)
+                        bool isOptionalPameter = parameter is IEdmOptionalParameter;
+                        if (!isOptionalPameter)
                         {
-                            missingParameters.Add(parameter.Name);
+                            // only check the non-optional parameter
+                            if (!this.parametersRead.Contains(parameter.Name) && !this.inputContext.EdmTypeResolver.GetParameterType(parameter).IsNullable)
+                            {
+                                missingParameters.Add(parameter.Name);
+                            }
                         }
                     }
 
