59337e4ee7a6aa23f71fcfab19e5461e3571bc1d
Biao Li <biaol@microsoft.com>
2018-09-25 13:51:59 -0700

    Set the selectionType to EntireSubtree when the selected list contains only expand tokens.

--- test/FunctionalTests/Microsoft.OData.Core.Tests/Evaluation/ODataMetadataContextTests.cs
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/Evaluation/ODataMetadataContextTests.cs
@@ -51,7 +51,7 @@ namespace Microsoft.OData.Tests.Evaluation
                 null /*requestUri*/);
             IEdmEntitySet set = this.edmModel.EntityContainer.FindEntitySet("Products");
             ODataResource entry = TestUtils.CreateODataEntry(set, new EdmStructuredValue(new EdmEntityTypeReference(set.EntityType(), true), new IEdmPropertyValue[0]), set.EntityType());
-            Action action = () => context.GetResourceMetadataBuilderForReader(new TestJsonLightReaderEntryState { Resource = entry, SelectedProperties = new SelectedPropertiesNode("*"), NavigationSource = set }, false);
+            Action action = () => context.GetResourceMetadataBuilderForReader(new TestJsonLightReaderEntryState { Resource = entry, SelectedProperties = new SelectedPropertiesNode("*", null), NavigationSource = set }, false);
             action.ShouldNotThrow();
         }
 
