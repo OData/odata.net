ce1dd6ce8f3fa0ffe1b6749b955393ee14d4cd3e
Sreejith Pazhampilly <37782322+Sreejithpin@users.noreply.github.com>
2019-11-20 14:57:36 -0800

    Fixing CopyFrom method in OdataMessageReader and OdataMessageWriter to include Arraypool (#1595)
    * Changes and test
    
    * Fixing the build error
    
    * Fixes based on Review Comments
    
    Fixes based on Review Comments
    
    * Update Microsoft.Test.OData.Tests.Client.NetCore.csproj
    
    * Revert "Merge branch 'master' into UpdateCopyFromMethod"
    
    This reverts commit 134591474c983378b711009251ab94702bb1a5ec, reversing
    changes made to 9a786edf03fea989ef418ae7e7fe880c9bba7be4.
    
    * Update Microsoft.Test.OData.Tests.Client.NetCore.csproj
    
    Reverting Microsoft.Test.OData.Tests.Client.NetCore
    
    * reverted stylecop buildnumber
    
    reverted stylecop buildnumber as its already committed
    
    * Fixed as per review comments
    
    Added copyright, sorted usings etc
    
    * Restructured EvaluateDifferences
    
    Restructured EvaluateDifferences
    
    * Update ValidationHelper.cs

---
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/TestCharArrayPool.cs
@@ -0,0 +1,33 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="TestCharArrayPool.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System;
+using Microsoft.OData.Buffers;
+
+namespace Microsoft.OData.Tests
+{
+    public class TestCharArrayPool : ICharArrayPool
+    {
+        public char[] Buffer { get { return Rent(MinSize); } }
+        
+        public int MinSize { set; get; }
+        
+        public TestCharArrayPool(int minSize)
+        {
+            this.MinSize = minSize;
+        }
+        
+        public char[] Rent(int minSize)
+        {
+            return new char[minSize];
+        }
+
+        public void Return(char[] array)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
