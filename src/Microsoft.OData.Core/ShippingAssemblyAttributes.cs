1521fae8763aeac36cfc1e559bdd7912549a8815
Sreejith Pazhampilly <sreejithpin@gmail.com>
2020-02-14 15:43:25 -0800

    updated for codesign

--- src/Microsoft.OData.Core/ShippingAssemblyAttributes.cs
+++ src/Microsoft.OData.Core/ShippingAssemblyAttributes.cs
@@ -4,21 +4,22 @@
 // </copyright>
 //---------------------------------------------------------------------
 
+
 #pragma warning disable 436
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AstoriaUnitTests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AstoriaUnitTests.TDDUnitTests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Client.TDDUnitTests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.TDD.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.User.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.Query.TDD.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Scenario.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Query.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.WCFService" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Reader.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Writer.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Client.Tests" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.DependencyInjection.NetCore" )]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.Tests.NetCore" )]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AstoriaUnitTests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AstoriaUnitTests.TDDUnitTests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Client.TDDUnitTests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.TDD.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.User.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.Query.TDD.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Scenario.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Query.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.WCFService" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Reader.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData.Writer.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Client.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.DependencyInjection.NetCore" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.Tests.NetCore" + AssemblyRef.TestPublicKey)]
 #pragma warning restore 436
--- src/Microsoft.OData.Edm/ShippingAssemblyAttributes.cs
+++ src/Microsoft.OData.Edm/ShippingAssemblyAttributes.cs
@@ -6,7 +6,7 @@
 
 // PublicKey=MicrosoftPublicKeyFull
 //[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Tests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Edm.Tests")]
+//[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Edm.Tests")]
 ////#pragma warning disable 436
 //[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EdmLibTests")]
 //[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EdmLibTests.SL")]
@@ -17,3 +17,16 @@
 //[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Edm.Tests.NetCore")]
 //[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.Tests.NetCore")]
 //[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Spatial.Tests.NetCore")]
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Edm.Tests" + AssemblyRef.TestPublicKey)]
+#pragma warning disable 436
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EdmLibTests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EdmLibTests.SL" + AssemblyRef.TestPublicKey)]
+#pragma warning restore 436
+
+// .NET Core
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.TestCommon.NetCore" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Edm.Tests.NetCore" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.Tests.NetCore" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Spatial.Tests.NetCore" + AssemblyRef.TestPublicKey)]
