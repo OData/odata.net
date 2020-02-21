b42f619aa36fae4b664eb28027cfe08c1f8ff865
Paul Ojwang' Odero <paodero@microsoft.com>
2020-02-21 22:09:27 +0300

    Fixing issue #843 (#1664)
    * Improving DataServiceClientException messages. Issue #843

--- test/EndToEndTests/Tests/Client/Build.Desktop/ClientTests/ClientQueryTests.cs
+++ test/EndToEndTests/Tests/Client/Build.Desktop/ClientTests/ClientQueryTests.cs
@@ -9,6 +9,7 @@ namespace Microsoft.Test.OData.Tests.Client
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using Microsoft.OData;
     using Microsoft.OData.Client;
     using Microsoft.Test.OData.Framework;
     using Microsoft.Test.OData.Framework.Client;
@@ -175,6 +176,11 @@ namespace Microsoft.Test.OData.Tests.Client
                     Assert.IsTrue(testCase.Value);
                     Assert.AreEqual(400, ex.Response.StatusCode);
                     StringResourceUtil.VerifyDataServicesString(ClientExceptionUtil.ExtractServerErrorMessage(ex), "BadRequest_KeyCountMismatch", "Microsoft.Test.OData.Services.AstoriaDefaultService.Message");
+                    //InnerException for DataServiceClientException must be set with the exception response from the server.
+                    ODataErrorException oDataErrorException = ex.InnerException.InnerException as ODataErrorException;
+                    Assert.IsTrue(oDataErrorException != null, "InnerException for DataServiceClientException has not been set.");
+                    Assert.AreEqual("An error was read from the payload. See the 'Error' property for more details.", oDataErrorException.Message);
+                    
                 }
             }
         }
