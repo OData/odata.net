a1999e8ffa41cc0b3b6f971297bcb33f279a4c36
Elizabeth Okerio <elokerio@microsoft.com>
2020-03-02 10:40:02 +0300

    Made some public methods virtual in the DataServiceContext, DataServiceQuery, DataServiceQueryOfT and DataServiceQuerySingleOfT classes to support mocking

---
+++ test/FunctionalTests/Microsoft.OData.Client.Tests/DataServiceContextMockTests.cs
@@ -0,0 +1,54 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="DataServiceContextMockTests.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using Moq;
+using Xunit;
+
+namespace Microsoft.OData.Client.Tests
+{
+    /// <summary>
+    /// How to mock the DataServiceContext class methods using moq framework example.
+    /// </summary>
+    public class DataServiceContextMockTests
+    {
+        [Fact]
+        public void VerifyIfDataServiceContextAddObjectMethodIsMockable()
+        {
+            string str = "user";
+            Mock<DataServiceContext> mockObject = new Mock<DataServiceContext>();
+            mockObject.CallBase = true;
+            mockObject.Setup(x => x.AddObject(It.IsAny<string>(), It.IsAny<object>())).Callback(() => { str = "Saved Successfully"; });
+            Users users = new Users();
+            string result = users.SaveUser(mockObject.Object);
+            Assert.Equal(str, result);
+        }
+
+        /// <summary>
+        /// A sample implementation class with methods calling the DataserviceContext.cs methods
+        /// </summary>
+        public class Users
+        {
+            /// <summary>
+            /// A simple method to save a user's details
+            /// </summary>
+            /// <param name="DataServiceContext">
+            /// A concrete class in the Microsoft.OData.Client library with implemented methods that this method calls.        
+            /// </param>
+            /// <returns>
+            /// return "Saved Successfully if the AddObject method runs successfully".
+            /// </returns>
+            public string SaveUser(DataServiceContext dataServiceContext)
+            {
+                string str = "user";
+                object obj = null;
+                dataServiceContext.AddObject(str, obj);
+                return "Saved Successfully";
+            }
+
+        }
+    }
+
+}
