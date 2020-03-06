f9ec9aa9df73f062f8908e91e7d916ad962ff2d5
Sreejith Pazhampilly <sreejithpin@gmail.com>
2020-03-06 00:38:10 -0800

    Update ShippingAssemblyAttributes.cs

--- src/Microsoft.OData.Edm/ShippingAssemblyAttributes.cs
+++ src/Microsoft.OData.Edm/ShippingAssemblyAttributes.cs
@@ -20,6 +20,7 @@
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Tests" + AssemblyRef.TestPublicKey)]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Edm.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.OData.Tests.Client" + AssemblyRef.TestPublicKey)]
 #pragma warning disable 436
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EdmLibTests" + AssemblyRef.TestPublicKey)]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EdmLibTests.SL" + AssemblyRef.TestPublicKey)]
