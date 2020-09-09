b0dbcdb6c4c2ef3c80f55901b3776daffcc70266
Steven Liekens <steven.liekens@gmail.com>
2020-09-09 02:12:55 +0200

    Add query provider test for Enumerable.Contains

---
+++ test/FunctionalTests/Microsoft.OData.Client.Tests/DataServiceQueryProviderTest.cs
@@ -0,0 +1,50 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="DataServiceQueryProviderTest.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.OData.Client.Tests
+{
+    /// <summary>
+    /// Tests to verify that LINQ expressions can be translated to URIs.
+    /// </summary>
+    public class DataServiceQueryProviderTest
+    {
+        private DataServiceContext dsc;
+ 
+        public DataServiceQueryProviderTest()
+        {
+            dsc = new DataServiceContext(new Uri("http://root"), ODataProtocolVersion.V4);
+        }
+
+        [Fact]
+        public void EnumerableContainsTranslatesToInOperator()
+        {
+            // Arrange
+            var sut = new DataServiceQueryProvider(dsc);
+            var productNames = new[] { "Milk", "Cheese", "Donut" };
+            var products = dsc.CreateQuery<Product>("Products")
+                .Where(product => productNames.Contains(product.Name));
+
+            // Act
+            var queryComponents = sut.Translate(products.Expression);
+
+            // Assert
+            Assert.Equal(@"http://root/Products?$filter=Name in ('Milk','Cheese','Donut')", queryComponents.Uri.ToString());
+        }
+
+        [EntityType]
+        [Key(nameof(Id))]
+        private class Product
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+    }
+}
