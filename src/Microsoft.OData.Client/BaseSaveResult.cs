fcadf7ca16d12cc17941a98ea19c04c19ea9c293
Thomas Faller <tfaller1@gmx.de>
2020-12-15 03:44:20 +0100

    Fix reading OData error at synchronous operations for .NET Core. (#1932)

--- src/Microsoft.OData.Client/BaseSaveResult.cs
+++ src/Microsoft.OData.Client/BaseSaveResult.cs
@@ -244,10 +244,21 @@ namespace Microsoft.OData.Client
 
             IDictionary<string, string> headers = new Dictionary<string, string>();
 
-            using (Stream stream = getResponseStream())
+            using (Stream responseStream = getResponseStream())
             {
-                if ((stream != null) && stream.CanRead && stream.CanSeek)
+                var stream = responseStream;
+                if ((stream != null) && stream.CanRead)
                 {
+                    if (!stream.CanSeek)
+                    {
+                        // We have to read the response twice, as a message text and as a potential OData error json.
+                        // To do this the stream needs to be buffered.
+                        var seekStream = new MemoryStream();
+                        stream.CopyTo(seekStream);
+                        stream = seekStream;
+                        stream.Position = 0;
+                        getResponseStream = () => stream;
+                    }
                     // This StreamReader can go out of scope without dispose because the underlying stream is disposed of.
                     message = new StreamReader(stream).ReadToEnd();
                     stream.Position = 0;
