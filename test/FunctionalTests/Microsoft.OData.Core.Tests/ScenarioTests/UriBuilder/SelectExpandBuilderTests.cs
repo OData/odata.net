a7850b58796228b894f03355a0efc262829c00e3
Kennedy Kangethe <kemunga@microsoft.com>
2021-03-11 11:51:33 +0300

    Add more tests

--- test/FunctionalTests/Microsoft.OData.Core.Tests/ScenarioTests/UriBuilder/SelectExpandBuilderTests.cs
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/ScenarioTests/UriBuilder/SelectExpandBuilderTests.cs
@@ -717,6 +717,22 @@ namespace Microsoft.OData.Tests.ScenarioTests.UriBuilder
         }
 
         [Fact]
+        public void SelectWithMultipleOptionsInPrimitiveCollectionShouldWork()
+        {
+            Uri queryUri = new Uri("People?$select=RelatedSSNs($count=true;$filter=endswith($this,'xyz');$orderby=$this;$top=10;$skip=5)", UriKind.Relative);
+            Uri actualUri = UriBuilder(queryUri, ODataUrlKeyDelimiter.Parentheses, settings);
+            Assert.Equal("http://gobbledygook/People?$select=" + Uri.EscapeDataString("RelatedSSNs($filter=endswith($this,'xyz');$orderby=$this;$top=10;$skip=5;$count=true)"), actualUri.OriginalString);
+        }
+
+        [Fact]
+        public void SelectWithMultipleOptionsInComplexCollectionShouldWork()
+        {
+            Uri queryUri = new Uri("People?$select=PreviousAddresses($count=true;$filter=endswith($this/City,'xyz');$orderby=$this/City;$top=10;$skip=5)", UriKind.Relative);
+            Uri actualUri = UriBuilder(queryUri, ODataUrlKeyDelimiter.Parentheses, settings);
+            Assert.Equal("http://gobbledygook/People?$select=" + Uri.EscapeDataString("PreviousAddresses($filter=endswith($this/City,'xyz');$orderby=$this/City;$top=10;$skip=5;$count=true)"), actualUri.OriginalString);
+        }
+
+        [Fact]
         public void ExpandWithNestedQueryOptionsShouldWork()
         {
             var ervFilter = new ResourceRangeVariable(ExpressionConstants.It, HardCodedTestModel.GetDogTypeReference(), HardCodedTestModel.GetDogsSet());
