7c8e57a984a2faf8b42a7b21871d28f88814c28b
Brad <ificator@users.noreply.github.com>
2021-03-25 16:43:21 -0700

    Make sure we pass the nodollar flag through (#2018)
    * Make sure we pass the nodollar flag through
    
    * Add unit test
    
    * Change names to indicate properties vs navigation, and validate sub-option values.

--- test/FunctionalTests/Microsoft.OData.Core.Tests/UriParser/Parsers/SelectExpandParserTests.cs
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/UriParser/Parsers/SelectExpandParserTests.cs
@@ -414,6 +414,88 @@ namespace Microsoft.OData.Tests.UriParser.Parsers
         }
         #endregion
 
+        #region ParseSelectExpand
+        [Fact]
+        public void NoDollarNestedOptions()
+        {
+            // Act
+            SelectExpandParserTests.ParseSelectExpand(
+                select: "prop1,prop2(select=prop3,prop4)",
+                expand: "nav1,nav2(select=prop5,prop6;expand=nav3,nav4)",
+                out SelectToken selectToken,
+                out ExpandToken expandToken,
+                enableNoDollarQueryOptions: true);
+
+            // Assert Select
+            Assert.NotNull(selectToken.Properties);
+            PathSegmentToken[] properties = selectToken.Properties.ToArray();
+            Assert.Equal(2, properties.Length);
+            properties[0].ShouldBeNonSystemToken("prop1");
+            properties[1].ShouldBeNonSystemToken("prop2");
+
+            Assert.NotNull(selectToken.SelectTerms);
+            SelectTermToken[] selectTerms = selectToken.SelectTerms.ToArray();
+            Assert.Equal(2, selectTerms.Length);
+            selectTerms[0].PathToProperty.ShouldBeNonSystemToken("prop1");
+            Assert.Null(selectTerms[0].SelectOption);
+            selectTerms[1].PathToProperty.ShouldBeNonSystemToken("prop2");
+            Assert.NotNull(selectTerms[1].SelectOption);
+            Assert.NotNull(selectTerms[1].SelectOption.SelectTerms);
+            SelectTermToken[] subSelectTerms = selectTerms[1].SelectOption.SelectTerms.ToArray();
+            Assert.Equal(2, subSelectTerms.Length);
+            subSelectTerms[0].PathToProperty.ShouldBeNonSystemToken("prop3");
+            subSelectTerms[1].PathToProperty.ShouldBeNonSystemToken("prop4");
+
+            // Assert Expand
+            Assert.NotNull(expandToken.ExpandTerms);
+            ExpandTermToken[] expandTerms = expandToken.ExpandTerms.ToArray();
+            Assert.Equal(2, expandTerms.Length);
+            expandTerms[0].PathToProperty.ShouldBeNonSystemToken("nav1");
+            Assert.Null(expandTerms[0].ExpandOption);
+            Assert.Null(expandTerms[0].SelectOption);
+            expandTerms[1].PathToProperty.ShouldBeNonSystemToken("nav2");
+            Assert.NotNull(expandTerms[1].ExpandOption);
+            ExpandTermToken[] subExpandTerms = expandTerms[1].ExpandOption.ExpandTerms.ToArray();
+            Assert.Equal(2, subExpandTerms.Length);
+            subExpandTerms[0].PathToProperty.ShouldBeNonSystemToken("nav3");
+            Assert.Null(subExpandTerms[0].ExpandOption);
+            Assert.Null(subExpandTerms[0].SelectOption);
+            subExpandTerms[1].PathToProperty.ShouldBeNonSystemToken("nav4");
+            Assert.Null(subExpandTerms[1].ExpandOption);
+            Assert.Null(subExpandTerms[1].SelectOption);
+            Assert.NotNull(expandTerms[1].SelectOption);
+            subSelectTerms = expandTerms[1].SelectOption.SelectTerms.ToArray();
+            Assert.Equal(2, subSelectTerms.Length);
+            subSelectTerms[0].PathToProperty.ShouldBeNonSystemToken("prop5");
+            Assert.Null(subSelectTerms[0].SelectOption);
+            subSelectTerms[1].PathToProperty.ShouldBeNonSystemToken("prop6");
+            Assert.Null(subSelectTerms[1].SelectOption);
+        }
+
+        private static void ParseSelectExpand(
+            string select,
+            string expand,
+            out SelectToken selectToken,
+            out ExpandToken expandToken,
+            int selectExpandLimit = ODataUriParserSettings.DefaultSelectExpandLimit,
+            bool enableCaseInsensitiveUriFunctionIdentifier = false,
+            bool enableNoDollarQueryOptions = false)
+        {
+            SelectExpandSyntacticParser.Parse(
+                select,
+                expand,
+                parentStructuredType: null,
+                new ODataUriParserConfiguration(EdmCoreModel.Instance)
+                {
+                    EnableCaseInsensitiveUriFunctionIdentifier = enableCaseInsensitiveUriFunctionIdentifier,
+                    EnableNoDollarQueryOptions = enableNoDollarQueryOptions,
+                    Settings = { SelectExpandLimit = selectExpandLimit },
+                },
+                out expandToken,
+                out selectToken);
+        }
+        #endregion
+
         #region Star Expand Testing
         [Fact]
         public void ParseStarInExpand()
