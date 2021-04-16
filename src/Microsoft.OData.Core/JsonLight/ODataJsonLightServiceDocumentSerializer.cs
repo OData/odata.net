17511b7f197104f495f38c045573c706b90ad659
John Gathogo <john.gathogo@gmail.com>
2021-04-16 14:02:42 +0300

    Implement asynchronous support in ODataJsonLightServiceDocumentSerializer (#2048)
    Co-authored-by: John Gathogo <jogathogo@microsoft.com>

--- src/Microsoft.OData.Core/JsonLight/ODataJsonLightServiceDocumentSerializer.cs
+++ src/Microsoft.OData.Core/JsonLight/ODataJsonLightServiceDocumentSerializer.cs
@@ -10,6 +10,7 @@ namespace Microsoft.OData.JsonLight
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Threading.Tasks;
     #endregion Namespaces
 
     /// <summary>
@@ -131,6 +132,132 @@ namespace Microsoft.OData.JsonLight
 
             // "}"
             this.JsonWriter.EndObjectScope();
+        }
+
+        /// <summary>
+        /// Asynchronously writes a service document in JsonLight format.
+        /// </summary>
+        /// <param name="serviceDocument">The service document to write.</param>
+        internal Task WriteServiceDocumentAsync(ODataServiceDocument serviceDocument)
+        {
+            Debug.Assert(serviceDocument != null, "serviceDocument != null");
+
+            return this.WriteTopLevelPayloadAsync(
+                async () =>
+                {
+                    // "{"
+                    await this.AsynchronousJsonWriter.StartObjectScopeAsync()
+                        .ConfigureAwait(false);
+
+                    // "@odata.context":...
+                    await this.WriteContextUriPropertyAsync(ODataPayloadKind.ServiceDocument)
+                        .ConfigureAwait(false);
+
+                    // "value":
+                    await this.AsynchronousJsonWriter.WriteValuePropertyNameAsync()
+                        .ConfigureAwait(false);
+
+                    // "["
+                    await this.AsynchronousJsonWriter.StartArrayScopeAsync()
+                        .ConfigureAwait(false);
+
+                    if (serviceDocument.EntitySets != null)
+                    {
+                        foreach (ODataEntitySetInfo collectionInfo in serviceDocument.EntitySets)
+                        {
+                            await this.WriteServiceDocumentElementAsync(collectionInfo, JsonLightConstants.ServiceDocumentEntitySetKindName)
+                                .ConfigureAwait(false);
+                        }
+                    }
+
+                    if (serviceDocument.Singletons != null)
+                    {
+                        foreach (ODataSingletonInfo singletonInfo in serviceDocument.Singletons)
+                        {
+                            await this.WriteServiceDocumentElementAsync(singletonInfo, JsonLightConstants.ServiceDocumentSingletonKindName)
+                                .ConfigureAwait(false);
+                        }
+                    }
+
+                    HashSet<string> functionImportsWritten = new HashSet<string>(StringComparer.Ordinal);
+
+                    if (serviceDocument.FunctionImports != null)
+                    {
+                        foreach (ODataFunctionImportInfo functionImportInfo in serviceDocument.FunctionImports)
+                        {
+                            if (functionImportInfo == null)
+                            {
+                                throw new ODataException(Strings.ValidationUtils_WorkspaceResourceMustNotContainNullItem);
+                            }
+
+                            if (!functionImportsWritten.Contains(functionImportInfo.Name))
+                            {
+                                functionImportsWritten.Add(functionImportInfo.Name);
+                                await this.WriteServiceDocumentElementAsync(functionImportInfo, JsonLightConstants.ServiceDocumentFunctionImportKindName)
+                                    .ConfigureAwait(false);
+                            }
+                        }
+                    }
+
+                    // "]"
+                    await this.AsynchronousJsonWriter.EndArrayScopeAsync()
+                        .ConfigureAwait(false);
+
+                    // "}"
+                    await this.AsynchronousJsonWriter.EndObjectScopeAsync()
+                        .ConfigureAwait(false);
+                });
+        }
+
+        /// <summary>
+        /// Asynchronously writes a element (EntitySet, Singleton or FunctionImport) in service document.
+        /// </summary>
+        /// <param name="serviceDocumentElement">The element in service document to write.</param>
+        /// <param name="kind">Kind of the service document element, optional for entitysets must for FunctionImport and Singleton.</param>
+        private async Task WriteServiceDocumentElementAsync(ODataServiceDocumentElement serviceDocumentElement, string kind)
+        {
+            // validate that the resource has a non-null url.
+            ValidationUtils.ValidateServiceDocumentElement(serviceDocumentElement, ODataFormat.Json);
+
+            // "{"
+            await this.AsynchronousJsonWriter.StartObjectScopeAsync()
+                .ConfigureAwait(false);
+
+            // "name": ...
+            await this.AsynchronousJsonWriter.WriteNameAsync(JsonLightConstants.ODataServiceDocumentElementName)
+                .ConfigureAwait(false);
+            await this.AsynchronousJsonWriter.WriteValueAsync(serviceDocumentElement.Name)
+                .ConfigureAwait(false);
+
+            // Do not write title if it is null or empty, or if title is the same as name.
+            if (!string.IsNullOrEmpty(serviceDocumentElement.Title) && !serviceDocumentElement.Title.Equals(serviceDocumentElement.Name, StringComparison.Ordinal))
+            {
+                // "title": ...
+                await this.AsynchronousJsonWriter.WriteNameAsync(JsonLightConstants.ODataServiceDocumentElementTitle)
+                    .ConfigureAwait(false);
+                await this.AsynchronousJsonWriter.WriteValueAsync(serviceDocumentElement.Title)
+                    .ConfigureAwait(false);
+            }
+
+            // Not always writing because it can be null if an ODataEntitySetInfo, not necessary to write this. Required for the others though.
+            if (kind != null)
+            {
+                // "kind": ...
+                await this.AsynchronousJsonWriter.WriteNameAsync(JsonLightConstants.ODataServiceDocumentElementKind)
+                    .ConfigureAwait(false);
+                await this.AsynchronousJsonWriter.WriteValueAsync(kind)
+                    .ConfigureAwait(false);
+            }
+
+            // "url": ...
+            await this.AsynchronousJsonWriter.WriteNameAsync(JsonLightConstants.ODataServiceDocumentElementUrlName)
+                .ConfigureAwait(false);
+            await this.AsynchronousJsonWriter.WriteValueAsync(this.UriToString(serviceDocumentElement.Url))
+                .ConfigureAwait(false);
+
+            // "}"
+            await this.AsynchronousJsonWriter.EndObjectScopeAsync()
+                .ConfigureAwait(false);
         }
     }
 }
