30f4e3aaecafa8554f20a62a755e7875aab7c88c
Thomas Faller <tfaller1@gmx.de>
2021-07-14 08:15:24 +0000

    HttpClientRequestMessage: Fix UnwrapAggregateException. (#2021)

---
+++ test/FunctionalTests/Microsoft.OData.Client.Tests/Serialization/HttpClientRequestMessageTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.OData.Client.Tests.Serialization
+{
+    public class HttpClientRequestMessageTests
+    {
+        [Fact]
+        public void UnwrapAggregateException()
+        {
+            var msg = new HttpClientRequestMessage(new DataServiceClientRequestMessageArgs("GET", new Uri("http://localhost"), false, false, new Dictionary<string, string>()));
+            var task = new Task<HttpResponseMessage>(() => throw new Exception("single exception"));
+            task.RunSynchronously();
+
+            var exception = Assert.Throws<DataServiceTransportException>(() => msg.EndGetResponse(task));
+            Assert.StartsWith("System.Exception: single exception\r\n", exception.Message);
+        }
+    }
+}
