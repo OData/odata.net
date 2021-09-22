2f950f54ecbd032f3284a726f7736f751ec92747
Clement Habinshuti <clhabins@microsoft.com>
2021-09-22 13:41:01 +0300

    Create benchmarks to compare JsonSerializer and OData sync/async writers

---
+++ test/PerformanceTests/SerializationBaselineTests/DataSet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SerializationBaselineTests
+{
+    static class DataSet
+    {
+        public static IEnumerable<Customer> GetCustomers(int count)
+        {
+            var data = new List<Customer>();
+            for (int i = 1; i <= count; i++)
+            {
+                data.Add(new Customer
+                {
+                    Id = i,
+                    Name = $"Cust{i}",
+                    Emails = new List<string> { $"emailA${i}", $"emailB{i} " },
+                    HomeAddress = new Address { City = $"City{i}", Street = $"Street{i}\n\"escape this\"" },
+                    Addresses = new List<Address>
+                    {
+                        new Address { City = $"CityA{i}", Street = $"StreetA{i}" },
+                        new Address { City = $"CityB{i}", Street = $"StreetA{i}" }
+                    }
+                });
+            }
+
+            return data;
+        }
+    }
+}
---
+++ test/PerformanceTests/SerializationBaselineTests/IExperimentWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationBaselineTests
+{
+    interface IExperimentWriter
+    {
+        Task WriteCustomers(IEnumerable<Customer> payload, Stream stream);
+    }
+}
