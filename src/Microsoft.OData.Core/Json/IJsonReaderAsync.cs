59cc9b6526cc3f43b638c2b15dc9060000c86579
John Gathogo <john.gathogo@gmail.com>
2021-09-28 11:13:30 +0300

    Implement interfaces in support of asynchronous reading of OData message payloads (#2187)
    Co-authored-by: John Gathogo <jogathogo@microsoft.com>

---
+++ src/Microsoft.OData.Core/Json/IJsonReaderAsync.cs
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="IJsonReaderAsync.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+namespace Microsoft.OData.Json
+{
+    /// <summary>
+    /// Interface for a class that can read arbitrary JSON asynchronously.
+    /// </summary>
+    public interface IJsonReaderAsync
+    {
+        /// <summary>
+        /// Asynchronously reads the next node from the input.
+        /// </summary>
+        /// <returns>true if a new node was found, or false if end of input was reached.</returns>
+        bool ReadAsync();
+    }
+}
