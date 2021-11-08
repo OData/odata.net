5f836e706648c3bb84b517a1699ddd1e688c9107
Henning Krause <henning.krause@netatwork.de>
2021-11-08 17:40:18 +0100

    Reverted changes in class InstanceAnnotationWriteTracker

--- src/Microsoft.OData.Core/InstanceAnnotationWriteTracker.cs
+++ src/Microsoft.OData.Core/InstanceAnnotationWriteTracker.cs
@@ -6,46 +6,46 @@
 
 namespace Microsoft.OData
 {
-	using System;
-	using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 
-	/// <summary>
-	/// Helper class to track if an annotation has been written.
-	/// </summary>
-	internal sealed class InstanceAnnotationWriteTracker
-	{
-		/// <summary>
-		/// Maintains the write status for each annotation using its key.
-		/// If a key exists in the list then it is considered written.
-		/// </summary>
-		private readonly HashSet<string> writeStatus;
+    /// <summary>
+    /// Helper class to track if an annotation has been written.
+    /// </summary>
+    internal sealed class InstanceAnnotationWriteTracker
+    {
+        /// <summary>
+        /// Maintains the write status for each annotation using its key.
+        /// If a key exists in the list then it is considered written.
+        /// </summary>
+        private readonly HashSet<string> writeStatus;
 
-		/// <summary>
-		/// Creates a new <see cref="InstanceAnnotationWriteTracker"/> to hold write status for instance annotations.
-		/// </summary>
-		public InstanceAnnotationWriteTracker()
-		{
-			this.writeStatus = new HashSet<string>(StringComparer.Ordinal);
-		}
+        /// <summary>
+        /// Creates a new <see cref="InstanceAnnotationWriteTracker"/> to hold write status for instance annotations.
+        /// </summary>
+        public InstanceAnnotationWriteTracker()
+        {
+            this.writeStatus = new HashSet<string>(StringComparer.Ordinal);
+        }
 
-		/// <summary>
-		/// Check if an annotation is already written.
-		/// </summary>
-		/// <returns>true if the annotation is written; otherwise false.</returns>
-		/// <param name="key">The key of the element to check if its written.</param>
-		public bool IsAnnotationWritten(string key)
-		{
-			return this.writeStatus.Contains(key);
-		}
+        /// <summary>
+        /// Check if an annotation is already written.
+        /// </summary>
+        /// <returns>true if the annotation is written; otherwise false.</returns>
+        /// <param name="key">The key of the element to check if its written.</param>
+        public bool IsAnnotationWritten(string key)
+        {
+            return this.writeStatus.Contains(key);
+        }
 
-		/// <summary>
-		/// Mark an annotation as written.
-		/// </summary>
-		/// <returns>true if the annotation was unmarked before; otherwise false.</returns>
-		/// <param name="key">The key of the element to mark as written.</param>
-		public bool MarkAnnotationWritten(string key)
-		{
-			return this.writeStatus.Add(key);
-		}
-	}
+        /// <summary>
+        /// Mark an annotation as written.
+        /// </summary>
+        /// <returns>true if the annotation was unmarked before; otherwise false.</returns>
+        /// <param name="key">The key of the element to mark as written.</param>
+        public bool MarkAnnotationWritten(string key)
+        {
+            return this.writeStatus.Add(key);
+        }
+    }
 }
