de5dadf6e4d0c7bb318453359cd9ff57e7c7385d
Clément Habinshuti <haby_habbes@live.com>
2022-01-18 11:34:26 +0300

    Update tools/perf/ResultsComparer/src/ResultsComparer/VsProfiler/VsProfilerReader.cs
    Co-authored-by: Kennedy Kang'ethe <kemunga@microsoft.com>

--- tools/perf/ResultsComparer/src/ResultsComparer/VsProfiler/VsProfilerReader.cs
+++ tools/perf/ResultsComparer/src/ResultsComparer/VsProfiler/VsProfilerReader.cs
@@ -1,5 +1,5 @@
 ﻿//---------------------------------------------------------------------
-// <copyright file="CommandLineOptions.cs" company="Microsoft">
+// <copyright file="VsProfilerReader.cs" company="Microsoft">
 //      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 // </copyright>
 //---------------------------------------------------------------------
