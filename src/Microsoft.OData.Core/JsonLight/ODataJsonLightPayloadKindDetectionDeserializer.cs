b812ebd8a4e4f9ddd630e531623d1dcf2d56eacf
John Gathogo <john.gathogo@microsoft.com>
2022-01-31 09:41:00 +0300

    Implement asynchronous support in ODataJsonLightPayloadKindDetectionDeserializer

--- src/Microsoft.OData.Core/JsonLight/ODataJsonLightPayloadKindDetectionDeserializer.cs
+++ src/Microsoft.OData.Core/JsonLight/ODataJsonLightPayloadKindDetectionDeserializer.cs
@@ -6,6 +6,7 @@
 
 namespace Microsoft.OData.JsonLight
 {
+    using System;
     #region Namespaces
     using System.Collections.Generic;
     using System.Diagnostics;
@@ -67,7 +68,7 @@ namespace Microsoft.OData.JsonLight
         /// </summary>
         /// <param name="detectionInfo">Additional information available for the payload kind detection.</param>
         /// <returns>A task which returns an enumerable of zero, one or more payload kinds that were detected from looking at the payload in the message stream.</returns>
-        internal Task<IEnumerable<ODataPayloadKind>> DetectPayloadKindAsync(ODataPayloadKindDetectionInfo detectionInfo)
+        internal async Task<IEnumerable<ODataPayloadKind>> DetectPayloadKindAsync(ODataPayloadKindDetectionInfo detectionInfo)
         {
             Debug.Assert(detectionInfo != null, "detectionInfo != null");
             Debug.Assert(this.ReadingResponse, "Payload kind detection is only supported in responses.");
@@ -75,28 +76,28 @@ namespace Microsoft.OData.JsonLight
             // prevent the buffering JSON reader from detecting in-stream errors - we read the error ourselves
             this.JsonReader.DisableInStreamErrorDetection = true;
 
-            return this.ReadPayloadStartAsync(
-                ODataPayloadKind.Unsupported,
-                /*propertyAndAnnotationCollector*/null,
-                /*isReadingNestedPayload*/false,
-                /*allowEmptyPayload*/false)
+            try
+            {
 
-                .FollowOnSuccessWith(t =>
-                    {
-                        return this.DetectPayloadKindImplementation(detectionInfo);
-                    })
+                await this.ReadPayloadStartAsync(
+                    ODataPayloadKind.Unsupported,
+                    propertyAndAnnotationCollector: null,
+                    isReadingNestedPayload: false,
+                    allowEmptyPayload: false).ConfigureAwait(false);
 
-                .FollowOnFaultAndCatchExceptionWith<IEnumerable<ODataPayloadKind>, ODataException>(t =>
-                    {
-                        // If we are not able to read the payload in the expected JSON format/structure
-                        // return no detected payload kind below.
-                        return Enumerable.Empty<ODataPayloadKind>();
-                    })
-
-                .FollowAlwaysWith(t =>
-                    {
-                        this.JsonReader.DisableInStreamErrorDetection = false;
-                    });
+                return await this.DetectPayloadKindImplementationAsync(detectionInfo)
+                    .ConfigureAwait(false);
+            }
+            catch (ODataException)
+            {
+                // If we are not able to read the payload in the expected JSON format/structure
+                // return no detected payload kind below.
+                return Enumerable.Empty<ODataPayloadKind>();
+            }
+            finally
+            {
+                this.JsonReader.DisableInStreamErrorDetection = false;
+            }
         }
 
         /// <summary>
@@ -141,7 +142,7 @@ namespace Microsoft.OData.JsonLight
                     }
                     else
                     {
-                        if (string.CompareOrdinal(JsonLightConstants.ODataErrorPropertyName, propertyName) == 0)
+                        if (string.Equals(JsonLightConstants.ODataErrorPropertyName, propertyName, StringComparison.Ordinal))
                         {
                             // If we find multiple errors or an invalid error value, this is not an error payload.
                             if (error != null || !this.JsonReader.StartBufferingAndTryToReadInStreamErrorPropertyValue(out error))
@@ -152,6 +153,101 @@ namespace Microsoft.OData.JsonLight
                             // At this point we successfully read the first error property.
                             // Skip the error value and check whether there are more properties.
                             this.JsonReader.SkipValue();
+                        }
+                        else
+                        {
+                            // if it contains non-annotation property, it is not an error payload.
+                            return Enumerable.Empty<ODataPayloadKind>();
+                        }
+                    }
+                }
+                else
+                {
+                    // Property annotation
+                    return Enumerable.Empty<ODataPayloadKind>();
+                }
+            }
+
+            // If we got here without finding a context URI or an error payload, we don't know what this is.
+            if (error == null)
+            {
+                return Enumerable.Empty<ODataPayloadKind>();
+            }
+
+            return new ODataPayloadKind[] { ODataPayloadKind.Error };
+        }
+
+        /// <summary>
+        /// Asynchronously detects the payload kind(s).
+        /// </summary>
+        /// <param name="detectionInfo">Additional information available for the payload kind detection.</param>
+        /// <returns>
+        /// A task that represents the asynchronous read operation.
+        /// The value of the TResult parameter contains an enumerable of zero,
+        /// one or more payload kinds that were detected from looking at the payload in the message stream.
+        /// </returns>
+        private async Task<IEnumerable<ODataPayloadKind>> DetectPayloadKindImplementationAsync(ODataPayloadKindDetectionInfo detectionInfo)
+        {
+            Debug.Assert(detectionInfo != null, "detectionInfo != null");
+            Debug.Assert(this.JsonReader.DisableInStreamErrorDetection, "The in-stream error detection should be disabled for payload kind detection.");
+
+            this.AssertJsonCondition(JsonNodeType.Property, JsonNodeType.EndObject);
+
+            // If we found a context URI and parsed it, look at the detected payload kind and return it.
+            if (this.ContextUriParseResult != null)
+            {
+                return this.ContextUriParseResult.DetectedPayloadKinds;
+            }
+
+            // Otherwise this is a payload without context URI and we have to start sniffing; only error payloads
+            // don't have a context URI so check for a single 'error' property (ignoring custom annotations).
+            ODataError error = null;
+            while (this.JsonReader.NodeType == JsonNodeType.Property)
+            {
+                string propertyName = await this.JsonReader.ReadPropertyNameAsync()
+                    .ConfigureAwait(false);
+                if (!TryParsePropertyAnnotation(propertyName, out _, out _))
+                {
+                    if (ODataJsonLightReaderUtils.IsAnnotationProperty(propertyName))
+                    {
+                        if (propertyName != null && propertyName.StartsWith(JsonLightConstants.ODataPropertyAnnotationSeparatorChar + JsonLightConstants.ODataAnnotationNamespacePrefix, StringComparison.Ordinal))
+                        {
+                            // Any @odata.* instance annotations are not allowed for errors.
+                            return Enumerable.Empty<ODataPayloadKind>();
+                        }
+                        else
+                        {
+                            // Skip custom instance annotations
+                            await this.JsonReader.SkipValueAsync()
+                                .ConfigureAwait(false);
+                        }
+                    }
+                    else
+                    {
+                        if (string.Equals(JsonLightConstants.ODataErrorPropertyName, propertyName, StringComparison.Ordinal))
+                        {
+                            // If we find multiple errors or an invalid error value, this is not an error payload.
+                            if (error != null)
+                            {
+                                return Enumerable.Empty<ODataPayloadKind>();
+                            }
+                            else
+                            {
+                                Tuple<bool, ODataError> readInStreamErrorPropertyResult = await this.JsonReader.StartBufferingAndTryToReadInStreamErrorPropertyValueAsync()
+                                    .ConfigureAwait(false);
+                                bool readInStreamError = readInStreamErrorPropertyResult.Item1;
+                                if (!readInStreamError)
+                                {
+                                    return Enumerable.Empty<ODataPayloadKind>();
+                                }
+
+                                error = readInStreamErrorPropertyResult.Item2;
+                            }
+
+                            // At this point we successfully read the first error property.
+                            // Skip the error value and check whether there are more properties.
+                            await this.JsonReader.SkipValueAsync()
+                                .ConfigureAwait(false);
                         }
                         else
                         {
