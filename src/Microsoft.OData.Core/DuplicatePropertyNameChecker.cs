2a27da512ae5b6bd44b095c5813828d57a597d05
Kennedy Kangethe Munga <kemunga@microsoft.com>
2022-04-01 11:40:00 +0300

    Code cleanup

--- src/Microsoft.OData.Core/DuplicatePropertyNameChecker.cs
+++ src/Microsoft.OData.Core/DuplicatePropertyNameChecker.cs
@@ -137,16 +137,8 @@ namespace Microsoft.OData
             }
         }
 
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~DuplicatePropertyNameChecker()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
