4810346a9a6b454aca05dddeedd38b3b3fcef5b2
Clement Habinshuti <clhabins@microsoft.com>
2022-07-26 12:48:45 +0300

    Remove erroneous usage of stackalloc

--- src/Microsoft.OData.Core/Json/ODataUtf8JsonWriter.cs
+++ src/Microsoft.OData.Core/Json/ODataUtf8JsonWriter.cs
@@ -25,7 +25,7 @@ namespace Microsoft.OData.Json
     {
         private const int DefaultBufferSize = 16 * 1024;
         private readonly float bufferFlushThreshold;
-        private readonly static ReadOnlyMemory<byte> Parantheses = new byte[] { (byte)'(', (byte)')' };
+        private readonly static ReadOnlyMemory<byte> Parentheses = new byte[] { (byte)'(', (byte)')' };
 
         private readonly Stream outputStream;
         private readonly Stream writeStream;
@@ -319,7 +319,7 @@ namespace Microsoft.OData.Json
         public async Task StartPaddingFunctionScopeAsync()
         {
             await this.FlushAsync().ConfigureAwait(false);
-            await this.writeStream.WriteAsync(stackalloc byte[] { '(' }.ConfigureAwait(false);
+            await this.writeStream.WriteAsync(Parentheses[..1]).ConfigureAwait(false);
         }
 
         public async Task WritePaddingFunctionNameAsync(string functionName)
@@ -331,7 +331,7 @@ namespace Microsoft.OData.Json
         public async Task EndPaddingFunctionScopeAsync()
         {
             await this.FlushAsync().ConfigureAwait(false);
-            await this.writeStream.WriteAsync(stackalloc byte[] {  ')' }]).ConfigureAwait(false);
+            await this.writeStream.WriteAsync(Parentheses[1..2]).ConfigureAwait(false);
         }
 
         public async Task StartObjectScopeAsync()
