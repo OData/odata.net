9626335ce7406352ee725d06cef099a9b216e4ac
Clement Habinshuti <clhabins@microsoft.com>
2022-09-05 12:47:11 +0300

    Replace string literal header with constant

--- test/FunctionalTests/Microsoft.OData.Core.Tests/JsonLight/ODataJsonLightBatchWriterTests.cs
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/JsonLight/ODataJsonLightBatchWriterTests.cs
@@ -418,7 +418,7 @@ namespace Microsoft.OData.Core.Tests.JsonLight
                     await jsonLightBatchWriter.WriteStartBatchAsync();
 
                     var customerOperationMessage = await jsonLightBatchWriter.CreateOperationResponseMessageAsync("1");
-                    customerOperationMessage.SetHeader("odata-version", "4.0");
+                    customerOperationMessage.SetHeader(ODataConstants.ODataVersionHeader, "4.0");
                     customerOperationMessage.SetHeader("Content-Type", "application/json");
 
                     // Copies operation's response body to the main batch response's stream
@@ -486,7 +486,7 @@ namespace Microsoft.OData.Core.Tests.JsonLight
                     }
 
                     var orderOperationMessage = await jsonLightBatchWriter.CreateOperationResponseMessageAsync("2");
-                    orderOperationMessage.SetHeader("odata-version", "4.0");
+                    orderOperationMessage.SetHeader(ODataConstants.ODataVersionHeader, "4.0");
                     orderOperationMessage.SetHeader("Content-Type", "application/json");
 
                     using (var orderOperationStream = CreateOrderResponseStream(orderId: "1"))
