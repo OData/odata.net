c530efaca4e350a39c4e0b887b890f19630c0e03
Clement Habinshuti <clhabins@microsoft.com>
2022-11-30 06:29:33 +0300

    Update license file

--- src/Microsoft.OData.Core/Json/BitStack.cs
+++ src/Microsoft.OData.Core/Json/BitStack.cs
@@ -1,5 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+﻿//---------------------------------------------------------------------
+// <copyright file="ODataUtf8JsonWriterAsyncTests.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
 
 using System;
 using System.Diagnostics;
