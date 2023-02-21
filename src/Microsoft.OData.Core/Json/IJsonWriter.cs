4008216a8d2669c9e10fc4a4b031cc90fa9e4632
Clement Habinshuti <clhabins@microsoft.com>
2023-02-21 12:04:20 +0300

    Debug failed tests builds

--- src/Microsoft.OData.Core/Json/IJsonWriter.cs
+++ src/Microsoft.OData.Core/Json/IJsonWriter.cs
@@ -10,7 +10,7 @@ namespace Microsoft.OData.Json
     using Microsoft.OData.Edm;
 #if NETCOREAPP3_1_OR_GREATER
     using System.Text.Json;
-#endif
+
 
     /// <summary>
     /// Interface for a class that can write arbitrary JSON.
@@ -156,14 +156,11 @@ namespace Microsoft.OData.Json
         /// <param name="value">TimeOfDay value to be written.</param>
         void WriteValue(TimeOfDay value);
 
-#if NETCOREAPP3_1_OR_GREATER
-
         /// <summary>
         /// Write a <see cref="JsonElement"/> value.
         /// </summary>
         /// <param name="value">The <see cref="JsonElement"/> value to be written.</param>
         void WriteValue(JsonElement value);
-#endif
 
         /// <summary>
         /// Write a raw value.
@@ -177,3 +174,162 @@ namespace Microsoft.OData.Json
         void Flush();
     }
 }
+
+#else
+    /// <summary>
+    /// Interface for a class that can write arbitrary JSON.
+    /// </summary>
+    [CLSCompliant(false)]
+    public interface IJsonWriter
+    {
+        /// <summary>
+        /// Start the padding function scope.
+        /// </summary>
+        void StartPaddingFunctionScope();
+
+        /// <summary>
+        /// End the padding function scope.
+        /// </summary>
+        void EndPaddingFunctionScope();
+
+        /// <summary>
+        /// Start the object scope.
+        /// </summary>
+        void StartObjectScope();
+
+        /// <summary>
+        /// End the current object scope.
+        /// </summary>
+        void EndObjectScope();
+
+        /// <summary>
+        /// Start the array scope.
+        /// </summary>
+        void StartArrayScope();
+
+        /// <summary>
+        /// End the current array scope.
+        /// </summary>
+        void EndArrayScope();
+
+        /// <summary>
+        /// Write the name for the object property.
+        /// </summary>
+        /// <param name="name">Name of the object property.</param>
+        void WriteName(string name);
+
+        /// <summary>
+        /// Writes a function name for JSON padding.
+        /// </summary>
+        /// <param name="functionName">Name of the padding function to write.</param>
+        void WritePaddingFunctionName(string functionName);
+
+        /// <summary>
+        /// Write a boolean value.
+        /// </summary>
+        /// <param name="value">Boolean value to be written.</param>
+        void WriteValue(bool value);
+
+        /// <summary>
+        /// Write an integer value.
+        /// </summary>
+        /// <param name="value">Integer value to be written.</param>
+        void WriteValue(int value);
+
+        /// <summary>
+        /// Write a float value.
+        /// </summary>
+        /// <param name="value">Float value to be written.</param>
+        void WriteValue(float value);
+
+        /// <summary>
+        /// Write a short value.
+        /// </summary>
+        /// <param name="value">Short value to be written.</param>
+        void WriteValue(short value);
+
+        /// <summary>
+        /// Write a long value.
+        /// </summary>
+        /// <param name="value">Long value to be written.</param>
+        void WriteValue(long value);
+
+        /// <summary>
+        /// Write a double value.
+        /// </summary>
+        /// <param name="value">Double value to be written.</param>
+        void WriteValue(double value);
+
+        /// <summary>
+        /// Write a Guid value.
+        /// </summary>
+        /// <param name="value">Guid value to be written.</param>
+        void WriteValue(Guid value);
+
+        /// <summary>
+        /// Write a decimal value
+        /// </summary>
+        /// <param name="value">Decimal value to be written.</param>
+        void WriteValue(decimal value);
+
+        /// <summary>
+        /// Writes a DateTimeOffset value
+        /// </summary>
+        /// <param name="value">DateTimeOffset value to be written.</param>
+        void WriteValue(DateTimeOffset value);
+
+        /// <summary>
+        /// Writes a TimeSpan value
+        /// </summary>
+        /// <param name="value">TimeSpan value to be written.</param>
+        void WriteValue(TimeSpan value);
+
+        /// <summary>
+        /// Write a byte value.
+        /// </summary>
+        /// <param name="value">Byte value to be written.</param>
+        void WriteValue(byte value);
+
+        /// <summary>
+        /// Write an sbyte value.
+        /// </summary>
+        /// <param name="value">SByte value to be written.</param>
+        void WriteValue(sbyte value);
+
+        /// <summary>
+        /// Write a string value.
+        /// </summary>
+        /// <param name="value">String value to be written.</param>
+        void WriteValue(string value);
+
+        /// <summary>
+        /// Write a byte array.
+        /// </summary>
+        /// <param name="value">Byte array to be written.</param>
+        void WriteValue(byte[] value);
+
+        /// <summary>
+        /// Write a Date value
+        /// </summary>
+        /// <param name="value">Date value to be written.</param>
+        void WriteValue(Date value);
+
+        /// <summary>
+        /// Write a TimeOfDay value
+        /// </summary>
+        /// <param name="value">TimeOfDay value to be written.</param>
+        void WriteValue(TimeOfDay value);
+
+        /// <summary>
+        /// Write a raw value.
+        /// </summary>
+        /// <param name="rawValue">Raw value to be written.</param>
+        void WriteRawValue(string rawValue);
+
+        /// <summary>
+        /// Clears all buffers for the current writer.
+        /// </summary>
+        void Flush();
+    }
+}
+#endif
