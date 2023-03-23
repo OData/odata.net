1eb9d5734dbe95e13c620f2eb8fbad3a111e730d
John Gathogo <john.gathogo@microsoft.com>
2023-03-23 09:54:10 +0300

    Apply tracing to OData write APIs

---
+++ src/Microsoft.OData.Core/TelemetryContants.cs
@@ -0,0 +1,25 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="OpenTelemetryConstants.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+namespace Microsoft.OData
+{
+    internal class TelemetryConstants
+    {
+        public const string WriteResourceSet = "OData.WriteResourceSet";
+        public const string WriteDeltaResourceSet = "OData.WriteDeltaResourceSet";
+        public const string WriteResource = "OData.WriteResource";
+        public const string WriteDeletedResource = "OData.WriteDeletedResource";
+        public const string WriteNestedResourceInfo = "OData.WriteNestedResourceInfo";
+        public const string WriteEntityReferenceLink = "OData.WriteNestedReferenceLink";
+        public const string WriteDeltaLink = "OData.WriteDeltaLink";
+        public const string WriteDeltaDeletedLink = "OData.WriteDeltaDeletedLink";
+        public const string WriteProperty = "OData.WriteProperty";
+        public const string WritePropertyInfo = "OData.WritePropertyInfo";
+        public const string WritePrimitiveValue = "OData.WritePrimitiveValue";
+        public const string WriteBinaryStream = "OData.WriteBinaryStream";
+        public const string WriteTextValue = "OData.WriteTextValue";
+    }
+}
