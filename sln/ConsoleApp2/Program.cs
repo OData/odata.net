3ca45945b6acd857c65271fe995965e9c3a94b56
Garrett DeBruin <gdebruin@microsoft.com>
2023-05-23 16:25:32 -0700

    added assembly initialize and app.config to disable debug assert dialog

---
+++ sln/ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ sln/ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
