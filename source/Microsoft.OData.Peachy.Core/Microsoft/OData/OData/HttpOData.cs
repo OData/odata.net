d54de1134a89ef3c3ae95288358b9de7b181e5e2
Garrett DeBruin <gdebruin@microsoft.com>
2023-06-14 09:18:57 -0700

    added an http implementation of iodata

--- source/Microsoft.OData.Peachy.Core/Microsoft/OData/OData/HttpOData.cs
+++ source/Microsoft.OData.Peachy.Core/Microsoft/OData/OData/HttpOData.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
 
@@ -34,9 +35,21 @@
                 using (var streamReader = new StreamReader(request))
                 {
                     string? line;
-                    while (!string.IsNullOrEmpty(line = await streamReader.ReadLineAsync()))
+                    while (!string.IsNullOrEmpty(line = await streamReader.ReadLineAsync().ConfigureAwait(false)))
                     {
+                        var header = line.Split(':', 2);
+                        if (header.Length < 1)
+                        {
+                            throw new InvalidOperationException("TODO document this in the interface");
+                        }
 
+                        httpClient.DefaultRequestHeaders.Add(header[0], header.ElementAtOrDefault(1, string.Empty));
+                    }
+
+                    //// TODO need to return response code
+                    using (var httpResponse = await httpClient.GetAsync(url).ConfigureAwait(false))
+                    {
+                        return await httpResponse.Content.ReadAsStreamAsync().ConfigureAwait(false);
                     }
                 }
             }
---
+++ source/Microsoft.OData.Peachy.Core/System/ArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Extension methods for <see cref="System.Array"/>
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        public static T ElementAtOrDefault<T>(this T[]? source, int index, T @default)
+        {
+            if (index >= source.Length)
+            {
+                return @default;
+            }
+
+            return source[index];
+        }
+    }
+}
