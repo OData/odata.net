7492702b871f13d90e2a248c6da05f1c87267e72
Garrett DeBruin <corranrogue9@gmail.com>
2023-09-05 15:08:56 -0700

    new script + todos

---
+++ tools/Release/DownloadBuildArtifacts.ps1
@@ -0,0 +1,9 @@
+<#
+TODO
+there should really be an "uber" script which ties together all of the scripts, and waits for user input when necessary, and picks up where they left off if possible
+you don't have to fully automate it right now, you *can* for example just present each commit and ask the user to tell you which category it is in, and then automate the rest in the future
+automation that is still missing:
+1. the release PR is not actually created, just a branch created and push and a note of the URL to use to manually create the PR
+2. release note generation is not fully automated; categories could be discovered by looking at work items linked to the PR
+3. anything after release note generation hasn't been automated yet
+#>
