f52135b4862966f4e0e65c337f2a496584d7efb3
Garrett DeBruin <corranrogue9@gmail.com>
2023-10-03 18:41:55 -0700

    adding test to repro percent-decoding and highlight that there is no way to get the original URL's encoded value

--- test/FunctionalTests/Microsoft.OData.Core.Tests/UriParser/ODataUriParserTests.cs
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/UriParser/ODataUriParserTests.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Xml;
 using Microsoft.OData.Edm;
 using Microsoft.OData.Edm.Csdl;
 using Microsoft.OData.Edm.Vocabularies;
@@ -26,6 +28,70 @@ namespace Microsoft.OData.Tests.UriParser
     {
         private readonly Uri ServiceRoot = new Uri("http://host");
         private readonly Uri FullUri = new Uri("http://host/People");
+
+        /// <summary>
+        /// Gets the name of the caller method of this method
+        /// </summary>
+        /// <param name="caller">The string that the method name of the caller will be written into</param>
+        /// <returns>The name of the caller method of this method</returns>
+        public static string GetCurrentMethodName([System.Runtime.CompilerServices.CallerMemberName] string caller = null)
+        {
+            return caller;
+        }
+
+#if !NETCOREAPP1_1
+        [Fact]
+        public void EscapeFunctionQuote()
+        {
+
+            // load the CSDL from the embedded resources
+            var assembly = Assembly.GetExecutingAssembly();
+            var currentMethod = GetCurrentMethodName();
+            var csdlResourceName = assembly.GetManifestResourceNames().Where(name => name.EndsWith($"{currentMethod}.xml")).Single();
+
+            // parse the CSDL
+            IEdmModel model;
+            using (var csdlResourceStream = assembly.GetManifestResourceStream(csdlResourceName))
+            {
+                using (var xmlReader = XmlReader.Create(csdlResourceStream))
+                {
+                    if (!CsdlReader.TryParse(xmlReader, out model, out var errors))
+                    {
+                        Assert.True(false, string.Join(Environment.NewLine, errors));
+                    }
+                }
+            }
+
+            var model2 = new EdmModel();
+            model2.AddElements(model.SchemaElements);
+
+            // set function to use the escape function notation
+            IEdmBooleanConstantExpression booleanConstant = new EdmBooleanConstant(true);
+            IEdmTerm term = CommunityVocabularyModel.UrlEscapeFunctionTerm;
+            foreach (var function in model2.SchemaElements.OfType<IEdmFunction>())
+            {
+                EdmVocabularyAnnotation annotation = new EdmVocabularyAnnotation(function, term, booleanConstant);
+                annotation.SetSerializationLocation(model2, EdmVocabularyAnnotationSerializationLocation.Inline);
+                model2.SetVocabularyAnnotation(annotation);
+            }
+
+            var domain = new Uri("http://tempuri.org");
+            var requestUrl = new Uri(domain, "/drive/root:/foo'bar.txt");
+            var uriParser = new ODataUriParser(model2, domain, requestUrl);
+            var odataPath = uriParser.ParsePath();
+
+            Assert.Equal(3, odataPath.Segments.Count);
+            var operationSegment = odataPath.Segments[2] as OperationSegment;
+            Assert.NotNull(operationSegment);
+            Assert.Equal(1, operationSegment.Parameters.Count());
+            var parameterValue = operationSegment.Parameters.ElementAt(0).Value as ConvertNode;
+            Assert.NotNull(parameterValue);
+            var constantNode = parameterValue.Source as ConstantNode;
+            Assert.NotNull(constantNode);
+            Assert.Equal("foo'bar.txt", constantNode.LiteralText);
+            Assert.Equal("foo'bar.txt", constantNode.Value);
+        }
+#endif
 
         [Theory]
         [InlineData(true)]
