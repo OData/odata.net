a18f7b7309e7e57b90691b3e693a22a535cfeea7
Garrett DeBruin <corranrogue9@gmail.com>
2023-10-23 16:10:09 -0700

    adding test for findlastindex

--- src/Microsoft.OData.Edm/System/Collections/Generic/ReadOnlyListExtensions.cs
+++ src/Microsoft.OData.Edm/System/Collections/Generic/ReadOnlyListExtensions.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Extensions methods <see cref="IReadOnlyList{T}"/>
     /// </summary>
-    public static class ReadOnlyCollectionExtensions
+    public static class ReadOnlyListExtensions
     {
         /// <summary>
         /// Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the last occurrence within the
@@ -16,6 +16,9 @@
         /// The zero-based index of the last occurrence of an element that matches the conditions defined by <paramref name="predicate"/>, if found; otherwise, -1
         /// </returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="list"/> or <paramref name="predicate"/> is <see langword="null"/></exception>
+        /// <remarks>
+        /// Copied from <see href="https://github.com/dotnet/runtime/blob/87c25589bda5a79baf8d056501663b8525f366a8/src/libraries/System.Private.CoreLib/src/System/Collections/Generic/List.cs#L560"/>
+        /// </remarks>
         public static int FindLastIndex<T>(this IReadOnlyList<T> list, Func<T, bool> predicate)
         {
             if (list == null)
