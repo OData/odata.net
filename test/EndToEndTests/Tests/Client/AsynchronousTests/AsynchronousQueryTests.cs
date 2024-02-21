89b63c8d11ebd0216c274f268e59573e6026e88e
Clément Habinshuti <haby_habbes@live.com>
2024-02-21 11:45:04 +0300

    [ODL 8]: Deprecate `KeyComparisonGeneratesFilterQuery` and set default to `true`. (#2866)
    * Add test
    
    * Set KeyComparisonGeneratesFilterQuery to true by default
    
    * Remove unnecessary statement from test
    
    * Address review comments

--- test/EndToEndTests/Tests/Client/AsynchronousTests/AsynchronousQueryTests.cs
+++ test/EndToEndTests/Tests/Client/AsynchronousTests/AsynchronousQueryTests.cs
@@ -1323,7 +1323,6 @@ namespace Microsoft.Test.OData.Tests.Client.AsynchronousTests
         public void Linq_Where_Generates_Filter_When_KeyComparisonGeneratesFilterQuery_Is_True()
         {
             var context = this.CreateWrappedContext<DefaultContainer>().Context;
-            context.KeyComparisonGeneratesFilterQuery = true;
             var query = context.Customer.Where(c => c.CustomerId == -10);
             var uri = query.ToString();
             Assert.EndsWith("$filter=CustomerId eq -10", uri);
@@ -1336,8 +1335,7 @@ namespace Microsoft.Test.OData.Tests.Client.AsynchronousTests
         public void Linq_Where_Generates_ByKey_When_KeyComparisonGeneratesFilterQuery_Is_False()
         {
             var context = this.CreateWrappedContext<DefaultContainer>().Context;
-            // By default context.KeyComparisonGeneratesFilterQuery = false;
-            // So we don't need to set it
+            context.KeyComparisonGeneratesFilterQuery = false;
             var query = context.Customer.Where(c => c.CustomerId == -10);
             var uri = query.ToString();
             Assert.EndsWith("Customer(-10)", uri);
---
+++ test/FunctionalTests/Microsoft.OData.Client.Tests/DataServiceContextTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Microsoft.OData.Client.Tests
+{
+    public class DataServiceContextTests
+    {
+        [Fact]
+        public void KeyComparisonGeneratesFilterQuery_Defaults_To_True()
+        {
+            var context = new DataServiceContext();
+            Assert.True(context.KeyComparisonGeneratesFilterQuery);
+        }
+    }
+}
