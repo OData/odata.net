ea3e253ee1544b3886bea838c01124b6bc89aabe
Elizabeth Okerio <elizaokerio@gmail.com>
2024-07-29 06:26:04 +0300

    Port action overloading tests (#3017)
    * Port action overloading tests
    
    * update changes

---
+++ test/EndToEndTests/Common/Microsoft.OData.Client.E2E.TestCommon/TestPriorityAttribute.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="TestPriorityAttribute.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.OData.Client.E2E.TestCommon
+{
+    /// <summary>
+    /// Specifies the priority of a test method.
+    /// </summary>
+    public class TestPriorityAttribute : Attribute
+    {
+        public TestPriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+        public int Priority { get; private set; }
+    }
+}
---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/Common/Server/OpenTypes/OpenTypesServiceEdmModel.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="OpenTypesServiceEdmModel.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace Microsoft.OData.Client.E2E.Tests.Common.Server.OpenTypes
+{
+    public class OpenTypesServiceEdmModel
+    {
+        public static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Row>("Row");
+            builder.EntitySet<RowIndex>("RowIndex");
+            builder.ComplexType<ContactDetails>();
+
+            return builder.GetEdmModel();
+        }
+    }
+}
