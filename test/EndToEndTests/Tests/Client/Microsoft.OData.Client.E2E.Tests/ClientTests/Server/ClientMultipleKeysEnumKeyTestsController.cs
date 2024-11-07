712f07eb3daf433a8baed3f2d755080dd34292e7
Samuel Wanjohi <WanjohiSammy@users.noreply.github.com>
2024-11-07 16:38:50 +0300

    Fix Cannot Attach Entity to DataServiceContext if Entity Key is an Enum (#3106)
    * Add EdmValueKind.Enum case in ToClrValue in order to be able to get the Value property of the IEdmEnumValue
    * Added Functional and E2E tests

---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/ClientTests/Server/ClientMultipleKeysEnumKeyTestsController.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="ClientMultipleKeysEnumKeyTestsController.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.OData.Client.E2E.Tests.Common.Server.MultipleKeys;
+
+namespace Microsoft.OData.Client.E2E.Tests.ClientTests.Server
+{
+    public class ClientMultipleKeysEnumKeyTestsController : ODataController
+    {
+        private static MultipleKeysEnumKeyDataSource _dataSource;
+
+        [EnableQuery]
+        [HttpGet("odata/Employees")]
+        public IActionResult Get()
+        {
+            var employees = _dataSource.Employees;
+
+            return Ok(employees);
+        }
+
+        [EnableQuery]
+        [HttpGet("odata/Employees/$count")]
+        public IActionResult GetEmployeesCount()
+        {
+            var employees = _dataSource.Employees;
+
+            return Ok(employees);
+        }
+
+        [EnableQuery]
+        [HttpGet("odata/Employees(employeeNumber={employeeNumber},employeeType={employeeType})")]
+        public IActionResult Get([FromODataUri] int employeeNumber, [FromODataUri] EmployeeType employeeType)
+        {
+            var employee = _dataSource.Employees?.FirstOrDefault(a => a.EmployeeNumber == employeeNumber && a.EmployeeType == employeeType);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost("odata/clientmultiplekeysenumkey/Default.ResetDefaultDataSource")]
+        public IActionResult ResetDefaultDataSource()
+        {
+            _dataSource = MultipleKeysEnumKeyDataSource.CreateInstance();
+
+            return Ok();
+        }
+    }
+}
---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/Common/Server/MultipleKeys/MultipleKeysEnumKeyEdmModel.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="MultipleKeysEnumKeyEdmModel.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace Microsoft.OData.Client.E2E.Tests.Common.Server.MultipleKeys
+{
+    public class MultipleKeysEnumKeyEdmModel
+    {
+        public static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<EmployeeWithEnumKey>("Employees");
+            builder.EntitySet<Organization>("Organizations");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
