c986e6a7fbb78e892c68b69a54b2bb42b42da91b
Garrett DeBruin <gdebruin@microsoft.com>
2024-11-20 14:04:00 -0800

    asdf

--- odata.tests/Test1.cs
+++ odata.tests/Test1.cs
@@ -1,11 +1,31 @@
 ﻿namespace odata.tests
 {
+    using Root.OdataResourcePath.Parsers;
+    using Root.OdataResourcePath.Transcribers;
+    using Sprache;
+    using System.Text;
+
     [TestClass]
     public sealed class Test1
     {
         [TestMethod]
         public void TestMethod1()
         {
+            var url = "$metadata";
+            var cst = OdataRelativeUriParser.Instance.Parse(url);
+
+            var ast = Root.OdataResourcePath.CstToAstTranslators.OdataRelativeUriTranslator
+                .Default
+                .Visit(cst, default);
+
+            var newCst = Root.OdataResourcePath.AstToCstTranslators.OdataRelativeUriTranslator
+                .Default
+                .Visit(ast, default);
+
+            var stringBuilder = new StringBuilder();
+            OdataRelativeUriTranscriber.Default.Visit(newCst, stringBuilder);
+
+            Assert.AreEqual(url, stringBuilder.ToString());
         }
     }
 }
