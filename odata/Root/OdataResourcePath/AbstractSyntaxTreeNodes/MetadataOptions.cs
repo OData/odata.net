6c43721060701f4eefefa8499c13daa4faa67d13
Garrett DeBruin <gdebruin@microsoft.com>
2024-11-20 14:13:49 -0800

    asdf

---
+++ odata/Root/OdataResourcePath/AbstractSyntaxTreeNodes/MetadataOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace Root.OdataResourcePath.AbstractSyntaxTreeNodes
+{
+    // TODO this is just a stub for now
+    public abstract class MetadataOptions
+    {
+        private MetadataOptions()
+        {
+        }
+
+        protected abstract TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context);
+
+        public abstract class Visitor<TResult, TContext>
+        {
+            public TResult Visit(MetadataOptions node, TContext context)
+            {
+                return node.Dispatch(this, context);
+            }
+        }
+    }
+}
