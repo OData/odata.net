c296918dec6a94ce6e51a7b0db691f4d5ea7ba68
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-04 10:00:16 -0800

    asdf

---
+++ odata/AbnfParser/CstNodes/RuleList.cs
@@ -0,0 +1,22 @@
+﻿namespace AbnfParser.CstNodes
+{
+    using System.Collections.Generic;
+
+    public sealed class RuleList
+    {
+        public RuleList(IEnumerable<Inner> inners)
+        {
+            //// TODO assert one or more elements
+            Inners = inners;
+        }
+
+        public IEnumerable<Inner> Inners { get; }
+
+        public abstract class Inner
+        {
+            private Inner()
+            {
+            }
+        }
+    }
+}
