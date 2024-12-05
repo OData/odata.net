44f4f27ddae648fe5bc83ff1e0f88461300b57cb
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-04 20:20:26 -0800

    asdf

---
+++ odata/AbnfParser/CombinatorParsers/HexValParser.cs
@@ -0,0 +1,57 @@
+﻿namespace AbnfParser.CombinatorParsers
+{
+    using AbnfParser.CombinatorParsers.Core;
+    using AbnfParser.CstNodes;
+    using Sprache;
+
+    public static class HexValParser
+    {
+        public static Parser<HexVal.HexOnly> HexOnly { get; } =
+            from x in x78Parser.Instance
+            from hexDigs in HexDigParser.Instance.Many()
+            select new HexVal.HexOnly(x, hexDigs);
+
+        public static Parser<HexVal.ConcatenatedHex> ConcatenatedHex { get; } = ConcatenatedHexParser.Instance;
+
+        public static class ConcatenatedHexParser
+        {
+            public static Parser<HexVal.ConcatenatedHex> Instance { get; } =
+                from x in x78Parser.Instance
+                from hexDigs in HexDigParser.Instance.Many()
+                from inners in InnerParser.Instance.Many()
+                select new HexVal.ConcatenatedHex(x, hexDigs, inners);
+
+            public static class InnerParser
+            {
+                public static Parser<HexVal.ConcatenatedHex.Inner> Instance { get; } =
+                    from dot in x2EParser.Instance
+                    from hexDigs in HexDigParser.Instance.Many()
+                    select new HexVal.ConcatenatedHex.Inner(dot, hexDigs);
+            }
+        }
+
+        public static Parser<HexVal.Range> Range { get; } = RangeParser.Instance;
+
+        public static class RangeParser
+        {
+            public static Parser<HexVal.Range> Instance { get; } =
+                from x in x78Parser.Instance
+                from hexDigs in HexDigParser.Instance.Many()
+                from inners in InnerParser.Instance.Many()
+                select new HexVal.Range(x, hexDigs, inners);
+
+            public static class InnerParser
+            {
+                public static Parser<HexVal.Range.Inner> Instance { get; } =
+                    from dash in x2DParser.Instance
+                    from hexDigs in HexDigParser.Instance.Many()
+                    select new HexVal.Range.Inner(dash, hexDigs);
+            }
+        }
+
+        public static Parser<HexVal> Instance { get; } =
+            HexOnly
+            .Or<HexVal>(ConcatenatedHex)
+            .Or(Range);
+    }
+}
--- odata/AbnfParser/CombinatorParsers/NumValParser.cs
+++ odata/AbnfParser/CombinatorParsers/NumValParser.cs
@@ -5,6 +5,21 @@
 
     public static class NumValParser
     {
-        public static Parser<NumVal.BinVal> BinVal { get; }
+        public static Parser<NumVal.BinVal> BinVal { get; } =
+            from value in BinValParser.Instance
+            select new NumVal.BinVal(value);
+
+        public static Parser<NumVal.DecVal> DecVal { get; } =
+            from value in DecValParser.Instance
+            select new NumVal.DecVal(value);
+
+        public static Parser<NumVal.HexVal> HexVal { get; } =
+            from value in HexValParser.Instance
+            select new NumVal.HexVal(value);
+
+        public static Parser<NumVal> Instance { get; } =
+            BinVal
+            .Or<NumVal>(DecVal)
+            .Or(HexVal);
     }
 }
