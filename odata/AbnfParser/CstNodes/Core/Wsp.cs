17f7a39dbd3cd035ff83678d1c1b99fa862e9214
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-05 09:38:27 -0800

    asdf

--- odata/AbnfParser/CstNodes/Core/Wsp.cs
+++ odata/AbnfParser/CstNodes/Core/Wsp.cs
@@ -6,6 +6,19 @@
         {
         }
 
+        protected abstract TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context);
+
+        public abstract class Visitor<TResult, TContext>
+        {
+            public TResult Visit(Wsp node, TContext context)
+            {
+                return node.Dispatch(this, context);
+            }
+
+            protected internal abstract TResult Accept(Space node, TContext context);
+            protected internal abstract TResult Accept(Tab node, TContext context);
+        }
+
         public sealed class Space : Wsp
         {
             public Space(Sp sp)
@@ -14,6 +27,11 @@
             }
 
             public Sp Sp { get; }
+
+            protected sealed override TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Accept(this, context);
+            }
         }
 
         public sealed class Tab : Wsp
@@ -24,6 +42,11 @@
             }
 
             public Htab Htab { get; }
+
+            protected sealed override TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Accept(this, context);
+            }
         }
     }
 }
