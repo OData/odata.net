fa4f1dcac39c56aaf4145c1398f06d4512bafdce
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-05 04:18:21 -0800

    asdf

--- odata/AbnfParser/CombinatorParsers/RuleListParser.cs
+++ odata/AbnfParser/CombinatorParsers/RuleListParser.cs
@@ -5,9 +5,24 @@
 
     public static class RuleListParser
     {
-        public static class Inner
+        public static Parser<RuleList> Instance { get; } =
+            from inners in InnerParser.Instance.Many()
+            select new RuleList(inners);
+
+        public static class InnerParser
         {
-            public static Parser<RuleList.Inner.RuleInner> RuleInner { get; }
+            public static Parser<RuleList.Inner.RuleInner> RuleInner { get; } =
+                from rule in RuleParser.Instance
+                select new RuleList.Inner.RuleInner(rule);
+
+            public static Parser<RuleList.Inner.CommentInner> CommentInner { get; } =
+                from cwsps in CwspParser.Instance.Many()
+                from cnl in CnlParser.Instance
+                select new RuleList.Inner.CommentInner(cwsps, cnl);
+
+            public static Parser<RuleList.Inner> Instance { get; } =
+                RuleInner
+                .Or<RuleList.Inner>(CommentInner);
         }
     }
 }
--- odata/AbnfParser/CombinatorParsers/RuleParser.cs
+++ odata/AbnfParser/CombinatorParsers/RuleParser.cs
@@ -5,6 +5,11 @@
 
     public static class RuleParser
     {
-        public static Parser<Rule> Instance { get; }
+        public static Parser<Rule> Instance { get; } =
+            from ruleName in RuleNameParser.Instance
+            from definedAs in DefinedAsParser.Instance
+            from elements in ElementsParser.Instance
+            from cnl in CnlParser.Instance
+            select new Rule(ruleName, definedAs, elements, cnl);
     }
 }
