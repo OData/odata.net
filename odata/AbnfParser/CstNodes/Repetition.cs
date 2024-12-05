bbcedcbc617c07174ab5ef0e32fe4c7667be7c43
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-05 09:53:47 -0800

    asdf

--- odata/AbnfParser/CstNodes/Repetition.cs
+++ odata/AbnfParser/CstNodes/Repetition.cs
@@ -1,9 +1,24 @@
-﻿namespace AbnfParser.CstNodes
+﻿using AbnfParser.CstNodes.Core;
+
+namespace AbnfParser.CstNodes
 {
     public abstract class Repetition
     {
         private Repetition()
         {
+        }
+
+        protected abstract TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context);
+
+        public abstract class Visitor<TResult, TContext>
+        {
+            public TResult Visit(Repetition node, TContext context)
+            {
+                return node.Dispatch(this, context);
+            }
+
+            protected internal abstract TResult Accept(ElementOnly node, TContext context);
+            protected internal abstract TResult Accept(RepeatAndElement node, TContext context);
         }
 
         public sealed class ElementOnly : Repetition
@@ -14,6 +29,11 @@
             }
 
             public Element Element { get; }
+
+            protected sealed override TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Accept(this, context);
+            }
         }
 
         public sealed class RepeatAndElement : Repetition
@@ -26,6 +46,11 @@
 
             public Repeat Repeat { get; }
             public Element Element { get; }
+
+            protected sealed override TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Accept(this, context);
+            }
         }
     }
 }
