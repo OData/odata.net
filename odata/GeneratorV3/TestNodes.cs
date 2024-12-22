72c95e58f3349916bf4d5afe99cbd7b0ee539a15
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-22 10:07:32 -0800

    asdf

--- odata/GeneratorV3/TestNodes.cs
+++ odata/GeneratorV3/TestNodes.cs
@@ -1,5 +1,7 @@
 ﻿namespace GeneratorV3
 {
+    using System.Collections.Generic;
+
     class FⲺⲻˑᱹꓸжⲤↃᱼｰⳆ꘡Ʇoo
     {
     }
@@ -30,6 +32,7 @@
             }
 
             protected internal abstract TResult Accept(firstⲻrule node, TContext context);
+            protected internal abstract TResult Accept(firstⲻrule_firstⲻrule node, TContext context);
         }
 
         public sealed class firstⲻrule : secondⲻrule
@@ -40,6 +43,11 @@
             }
 
             public GeneratorV3.firstⲻrule firstⲻrule_1 { get; }
+
+            protected sealed override TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Accept(this, context);
+            }
         }
 
         public sealed class firstⲻrule_firstⲻrule : secondⲻrule //// TODO i don't really like using _ for spaces *and* for the property name conflict resolution
@@ -50,11 +58,43 @@
             }
 
             public Inners.firstⲻrule_firstⲻrule firstⲻrule_firstⲻrule_1 { get; }
+
+            protected sealed override TResult Dispatch<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Accept(this, context);
+            }
         }
     }
 
     public static class Inners
     {
+        public sealed class firstⲻrule_ꓸжⲤfirstⲻrule_꘡firstⲻrule꘡Ↄ_ж꘡firstⲻrule_firstⲻrule꘡_ж1firstⲻrule_1жfirstⲻrule
+        {
+            public firstⲻrule_ꓸжⲤfirstⲻrule_꘡firstⲻrule꘡Ↄ_ж꘡firstⲻrule_firstⲻrule꘡_ж1firstⲻrule_1жfirstⲻrule(
+                GeneratorV3.firstⲻrule firstⲻrule_1,
+                Inners.Ⲥfirstⲻrule_꘡firstⲻrule꘡Ↄ? Ⲥfirstⲻrule_꘡firstⲻrule꘡Ↄ_1,
+                IEnumerable<Inners.firstⲻrule_firstⲻrule?> firstⲻrule_firstⲻrules_1,
+                IEnumerable<GeneratorV3.firstⲻrule> firstⲻrules_1,
+                IEnumerable<GeneratorV3.firstⲻrule> firstⲻrules_2)
+            {
+                this.firstⲻrule_1 = firstⲻrule_1;
+                this.Ⲥfirstⲻrule_꘡firstⲻrule꘡Ↄ_1 = Ⲥfirstⲻrule_꘡firstⲻrule꘡Ↄ_1;
+                this.firstⲻrule_firstⲻrules_1 = firstⲻrule_firstⲻrules_1;
+                this.firstⲻrules_1 = firstⲻrules_1;
+                this.firstⲻrules_2 = firstⲻrules_2;
+            }
+
+            public firstⲻrule firstⲻrule_1 { get; }
+            public Ⲥfirstⲻrule_꘡firstⲻrule꘡Ↄ? Ⲥfirstⲻrule_꘡firstⲻrule꘡Ↄ_1 { get; }
+            public IEnumerable<firstⲻrule_firstⲻrule?> firstⲻrule_firstⲻrules_1 { get; }
+            public IEnumerable<firstⲻrule> firstⲻrules_1 { get; }
+            public IEnumerable<firstⲻrule> firstⲻrules_2 { get; }
+        }
+
+        public sealed class Ⲥfirstⲻrule_꘡firstⲻrule꘡Ↄ
+        {
+        }
+
         public sealed class firstⲻrule_firstⲻrule
         {
             public firstⲻrule_firstⲻrule(GeneratorV3.firstⲻrule firstⲻrule_1, GeneratorV3.firstⲻrule firstⲻrule_2)
