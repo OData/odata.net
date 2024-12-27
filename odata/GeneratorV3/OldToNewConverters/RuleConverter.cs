ca6ce16db41d970c82ee5a31d3de54669ba21482
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-27 13:46:32 -0800

    asdf

---
+++ odata/GeneratorV3/OldToNewConverters/RuleConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorV3.OldToNewConverters
+{
+    public sealed class RuleConverter
+    {
+        private RuleConverter()
+        {
+        }
+
+        public static RuleConverter Instance { get; } = new RuleConverter();
+
+        public GeneratorV3.Abnf._rule Convert(AbnfParser.CstNodes.Rule rule)
+        {
+        }
+    }
+}
---
+++ odata/GeneratorV3/OldToNewConverters/RuleListConverter.cs
@@ -0,0 +1,45 @@
+﻿namespace GeneratorV3.OldToNewConverters
+{
+    using AbnfParser.CstNodes;
+    using GeneratorV3.Abnf;
+    using Root;
+    using System.Linq;
+
+    public sealed class RuleListConverter
+    {
+        private RuleListConverter()
+        {
+        }
+
+        public static RuleListConverter Instance { get; } = new RuleListConverter();
+
+        public GeneratorV3.Abnf._rulelist Convert(AbnfParser.CstNodes.RuleList ruleList)
+        {
+            return new _rulelist(ruleList
+                .Inners
+                .Select(inner => 
+                    InnerConverter.Instance.Visit(inner, default)));
+        }
+
+        public sealed class InnerConverter : AbnfParser.CstNodes.RuleList.Inner.Visitor<Abnf.Inners._openruleⳆopenasteriskcⲻwsp_cⲻnlↃↃ, Root.Void>
+        {
+            private InnerConverter()
+            {
+            }
+
+            public static InnerConverter Instance { get; } = new InnerConverter();
+
+            protected internal override Inners._openruleⳆopenasteriskcⲻwsp_cⲻnlↃↃ Accept(RuleList.Inner.RuleInner node, Void context)
+            {
+                return new Inners._openruleⳆopenasteriskcⲻwsp_cⲻnlↃↃ(
+                    new Inners._ruleⳆopenasteriskcⲻwsp_cⲻnlↃ._rule(
+                        RuleConverter.Instance.Convert(node.Rule)));
+            }
+
+            protected internal override Inners._openruleⳆopenasteriskcⲻwsp_cⲻnlↃↃ Accept(RuleList.Inner.CommentInner node, Void context)
+            {
+                
+            }
+        }
+    }
+}
