28c2a6cadd5db87e829c4a4803002f99c45ec93d
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-30 11:02:33 -0800

    asdf

--- odata/GeneratorV3/OldToNewConverters/Core/CrConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/Core/CrConverter.cs
@@ -10,6 +10,10 @@
 
         public GeneratorV3.Abnf._CR Convert(AbnfParser.CstNodes.Core.Cr cr)
         {
+            return new Abnf._CR(
+                new Abnf.Inners._percentxZEROD(
+                    Abnf.Inners._ZERO.Instance,
+                    Abnf.Inners._D.Instance));
         }
     }
 }
--- odata/GeneratorV3/OldToNewConverters/Core/DigitConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/Core/DigitConverter.cs
@@ -10,52 +10,82 @@
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x30 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREEZERO(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._ZERO.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x31 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREEONE(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._ONE.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x32 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREETWO(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._TWO.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x33 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREETHREE(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._THREE.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x34 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREEFOUR(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._FOUR.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x35 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREEFIVE(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._FIVE.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x36 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREESIX(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._SIX.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x37 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREESEVEN(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._SEVEN.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x38 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREEEIGHT(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._EIGHT.Instance));
         }
 
         protected internal override GeneratorV3.Abnf._DIGIT Accept(AbnfParser.CstNodes.Core.Digit.x39 node, Root.Void context)
         {
-            
+            return new Abnf._DIGIT(
+                new Abnf.Inners._percentxTHREEZEROⲻTHREENINE._THREENINE(
+                    Abnf.Inners._THREE.Instance,
+                    Abnf.Inners._NINE.Instance));
         }
     }
 }
