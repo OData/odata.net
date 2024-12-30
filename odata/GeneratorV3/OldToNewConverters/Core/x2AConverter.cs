230abf39cb4a6915733d3c217da237f0abeb2aee
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-30 09:50:49 -0800

    asdf

---
+++ odata/GeneratorV3/OldToNewConverters/Core/x2AConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace GeneratorV3.OldToNewConverters.Core
+{
+    public sealed class x2AConverter
+    {
+        private x2AConverter()
+        {
+        }
+
+        public static x2AConverter Instance { get; } = new x2AConverter();
+
+        public GeneratorV3.Abnf.Inners._x2A Convert(AbnfParser.CstNodes.Core.x2A x2A)
+        {
+        }
+    }
+}
--- odata/GeneratorV3/OldToNewConverters/RepeatConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/RepeatConverter.cs
@@ -1,7 +1,10 @@
 ﻿namespace GeneratorV3.OldToNewConverters
 {
+    using System.Linq;
+
     using AbnfParser.CstNodes;
     using GeneratorV3.Abnf;
+    using GeneratorV3.OldToNewConverters.Core;
     using Root;
 
     public sealed class RepeatConverter : AbnfParser.CstNodes.Repeat.Visitor<GeneratorV3.Abnf._repeat, Root.Void>
@@ -14,10 +17,22 @@
 
         protected internal override _repeat Accept(Repeat.Count node, Void context)
         {
+            return new _repeat._ONEasteriskDIGIT(
+                node.Digits.Select(digit =>
+                    DigitConverter.Instance.Visit(digit, context)));
         }
 
         protected internal override _repeat Accept(Repeat.Range node, Void context)
         {
+            return new _repeat._openasteriskDIGIT_doublequotex2Adoublequote_asteriskDIGITↃ(
+                new Inners._openasteriskDIGIT_doublequotex2Adoublequote_asteriskDIGITↃ(
+                    new Inners._asteriskDIGIT_doublequotex2Adoublequote_asteriskDIGIT(
+                        node.PrefixDigits.Select(digit =>
+                            DigitConverter.Instance.Visit(digit, context)),
+                        new Inners._doublequotex2Adoublequote(
+                            x2AConverter.Instance.Convert(node.Asterisk)),
+                        node.SuffixDigits.Select(digit =>
+                            DigitConverter.Instance.Visit(digit, context)))));
         }
     }
 }
