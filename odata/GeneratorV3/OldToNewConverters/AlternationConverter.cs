b456d6d48c37a0b17b88dd5a15f3bafd85acd617
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-30 09:22:08 -0800

    asdf

--- odata/GeneratorV3/OldToNewConverters/AlternationConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/AlternationConverter.cs
@@ -1,5 +1,10 @@
 ﻿namespace GeneratorV3.OldToNewConverters
 {
+    using System.Linq;
+
+    using AbnfParser.CstNodes;
+    using GeneratorV3.OldToNewConverters.Core;
+
     public sealed class AlternationConverter
     {
         private AlternationConverter()
@@ -10,6 +15,32 @@
 
         public GeneratorV3.Abnf._alternation Convert(AbnfParser.CstNodes.Alternation alternation)
         {
+            return new Abnf._alternation(
+                ConcatenationConverter.Instance.Convert(alternation.Concatenation),
+                alternation.Inners.Select(inner =>
+                    new Abnf.Inners._openasteriskcⲻwsp_doublequotex2Fdoublequote_asteriskcⲻwsp_concatenationↃ(
+                        InnerConverter.Instance.Convert(inner))));
+        }
+
+        private sealed class InnerConverter
+        {
+            private InnerConverter()
+            {
+            }
+
+            public static InnerConverter Instance { get; } = new InnerConverter();
+
+            public GeneratorV3.Abnf.Inners._asteriskcⲻwsp_doublequotex2Fdoublequote_asteriskcⲻwsp_concatenation Convert(Alternation.Inner inner)
+            {
+                return new Abnf.Inners._asteriskcⲻwsp_doublequotex2Fdoublequote_asteriskcⲻwsp_concatenation(
+                    inner.PrefixCwsps.Select(cwsp =>
+                        CwspConverter.Instance.Visit(cwsp, default)),
+                    new Abnf.Inners._doublequotex2Fdoublequote(
+                        x2FConverter.Instance.Convert(inner.Slash)),
+                    inner.SuffixCwsps.Select(cwsp =>
+                        CwspConverter.Instance.Visit(cwsp, default)),
+                    ConcatenationConverter.Instance.Convert(inner.Concatenation));
+            }
         }
     }
 }
---
+++ odata/GeneratorV3/OldToNewConverters/ConcatenationConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace GeneratorV3.OldToNewConverters
+{
+    public sealed class ConcatenationConverter
+    {
+        private ConcatenationConverter()
+        {
+        }
+
+        public static ConcatenationConverter Instance { get; } = new ConcatenationConverter();
+
+        public GeneratorV3.Abnf._concatenation Convert(AbnfParser.CstNodes.Concatenation concatenation)
+        {
+        }
+    }
+}
