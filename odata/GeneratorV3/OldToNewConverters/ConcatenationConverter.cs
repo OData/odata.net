43800e59278a01d5f049682d4fb87a882fcc2def
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-30 09:27:14 -0800

    asdf

--- odata/GeneratorV3/OldToNewConverters/ConcatenationConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/ConcatenationConverter.cs
@@ -1,5 +1,9 @@
 ﻿namespace GeneratorV3.OldToNewConverters
 {
+    using System.Linq;
+
+    using AbnfParser.CstNodes;
+
     public sealed class ConcatenationConverter
     {
         private ConcatenationConverter()
@@ -10,6 +14,28 @@
 
         public GeneratorV3.Abnf._concatenation Convert(AbnfParser.CstNodes.Concatenation concatenation)
         {
+            return new Abnf._concatenation(
+                RepetitionConverter.Instance.Visit(concatenation.Repetition, default),
+                concatenation.Inners.Select(inner =>
+                    InnerConverter.Instance.Convert(inner)));
+        }
+
+        private sealed class InnerConverter
+        {
+            private InnerConverter()
+            {
+            }
+
+            public static InnerConverter Instance { get; } = new InnerConverter();
+
+            public GeneratorV3.Abnf.Inners._openONEasteriskcⲻwsp_repetitionↃ Convert(Concatenation.Inner inner)
+            {
+                return new Abnf.Inners._openONEasteriskcⲻwsp_repetitionↃ(
+                    new Abnf.Inners._ONEasteriskcⲻwsp_repetition(
+                        inner.Cwsps.Select(cwsp =>
+                            CwspConverter.Instance.Visit(cwsp, default)),
+                        RepetitionConverter.Instance.Visit(inner.Repetition, default)));
+            }
         }
     }
 }
---
+++ odata/GeneratorV3/OldToNewConverters/RepetitionConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace GeneratorV3.OldToNewConverters
+{
+    public sealed class RepetitionConverter : AbnfParser.CstNodes.Repetition.Visitor<GeneratorV3.Abnf._repetition, Root.Void>
+    {
+        private RepetitionConverter()
+        {
+        }
+
+        public static RepetitionConverter Instance { get; } = new RepetitionConverter();
+
+        protected internal override GeneratorV3.Abnf._repetition Accept(
+            AbnfParser.CstNodes.Repetition.ElementOnly node, 
+            Root.Void context)
+        {
+        }
+
+        protected internal override GeneratorV3.Abnf._repetition Accept(
+            AbnfParser.CstNodes.Repetition.RepeatAndElement node, 
+            Root.Void context)
+        {
+        }
+    }
+}
