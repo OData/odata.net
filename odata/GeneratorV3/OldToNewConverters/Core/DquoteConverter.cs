01f6f13f7481c1cfdd8f66db7969df9343512f20
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-30 10:02:50 -0800

    asdf

---
+++ odata/GeneratorV3/OldToNewConverters/Core/DquoteConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace GeneratorV3.OldToNewConverters.Core
+{
+    public sealed class DquoteConverter
+    {
+        private DquoteConverter()
+        {
+        }
+
+        public static DquoteConverter Instance { get; } = new DquoteConverter();
+
+        public GeneratorV3.Abnf._DQUOTE Convert(AbnfParser.CstNodes.Core.Dquote dquote)
+        {
+        }
+    }
+}
