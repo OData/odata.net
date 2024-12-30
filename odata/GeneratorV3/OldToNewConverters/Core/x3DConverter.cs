820ba8d1bc3e08004afa771d2054ed4febb8fea8
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-30 09:13:26 -0800

    sadf

---
+++ odata/GeneratorV3/OldToNewConverters/Core/x3DConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace GeneratorV3.OldToNewConverters.Core
+{
+    public sealed class x3DConverter
+    {
+        private x3DConverter()
+        {
+        }
+
+        public static x3DConverter Instance { get; } = new x3DConverter();
+
+        public GeneratorV3.Abnf.Inners._x3D Convert(AbnfParser.CstNodes.Core.x3D x3D)
+        {
+        }
+    }
+}
--- odata/GeneratorV3/OldToNewConverters/DefinedAsConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/DefinedAsConverter.cs
@@ -1,7 +1,10 @@
 ﻿namespace GeneratorV3.OldToNewConverters
 {
+    using System.Linq;
+
     using AbnfParser.CstNodes;
     using GeneratorV3.Abnf;
+    using GeneratorV3.OldToNewConverters.Core;
 
     public sealed class DefinedAsConverter : AbnfParser.CstNodes.DefinedAs.Visitor<GeneratorV3.Abnf._definedⲻas, Root.Void>
     {
@@ -13,10 +16,29 @@
 
         protected internal override _definedⲻas Accept(DefinedAs.Declaration node, Root.Void context)
         {
+            return new _definedⲻas(
+                node.PrefixCwsps.Select(cwsp =>
+                    CwspConverter.Instance.Visit(cwsp, context)),
+                new Inners._opendoublequotex3DdoublequoteⳆdoublequotex3Dx2FdoublequoteↃ(
+                    new Inners._doublequotex3DdoublequoteⳆdoublequotex3Dx2Fdoublequote._doublequotex3Ddoublequote(
+                        new Inners._doublequotex3Ddoublequote(
+                            x3DConverter.Instance.Convert(node.Equals)))),
+                node.SuffixCwsps.Select(cwsp =>
+                    CwspConverter.Instance.Visit(cwsp, context)));
         }
 
         protected internal override _definedⲻas Accept(DefinedAs.Incremental node, Root.Void context)
         {
+            return new _definedⲻas(
+                node.PrefixCwsps.Select(cwsp =>
+                    CwspConverter.Instance.Visit(cwsp, context)),
+                new Inners._opendoublequotex3DdoublequoteⳆdoublequotex3Dx2FdoublequoteↃ(
+                    new Inners._doublequotex3DdoublequoteⳆdoublequotex3Dx2Fdoublequote._doublequotex3Dx2Fdoublequote(
+                        new Inners._doublequotex3Dx2Fdoublequote(
+                            x3DConverter.Instance.Convert(node.Equals),
+                            x2FConverter.Instance.Convert(node.Slash)))),
+                node.SuffixCwsps.Select(cwsp =>
+                    CwspConverter.Instance.Visit(cwsp, context)));
         }
     }
 }
