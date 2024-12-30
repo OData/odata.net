1b97a8956d453d867663435b3d99593cec08d2e8
Garrett DeBruin <gdebruin@microsoft.com>
2024-12-30 09:05:32 -0800

    asdf

--- odata/GeneratorV3/OldToNewConverters/CwspConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/CwspConverter.cs
@@ -2,6 +2,7 @@
 {
     using AbnfParser.CstNodes;
     using GeneratorV3.Abnf;
+    using GeneratorV3.OldToNewConverters.Core;
 
     public sealed class CwspConverter : AbnfParser.CstNodes.Cwsp.Visitor<GeneratorV3.Abnf._cⲻwsp, Root.Void>
     {
@@ -13,10 +14,17 @@
 
         protected internal override _cⲻwsp Accept(Cwsp.WspOnly node, Root.Void context)
         {
+            return new _cⲻwsp._WSP(
+                WspConverter.Instance.Visit(node.Wsp, context));
         }
 
         protected internal override _cⲻwsp Accept(Cwsp.CnlAndWsp node, Root.Void context)
         {
+            return new _cⲻwsp._opencⲻnl_WSPↃ(
+                new Inners._opencⲻnl_WSPↃ(
+                    new Inners._cⲻnl_WSP(
+                        CnlConverter.Instance.Visit(node.Cnl, context),
+                        WspConverter.Instance.Visit(node.Wsp, context))));
         }
     }
 }
