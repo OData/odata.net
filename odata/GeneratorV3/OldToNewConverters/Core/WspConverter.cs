a66d2de09c9f87e55ece0151d05d1a21b1cee6bd
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-02 12:15:52 -0800

    asdf

--- odata/GeneratorV3/OldToNewConverters/Core/WspConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/Core/WspConverter.cs
@@ -12,18 +12,18 @@
         {
             return new Abnf._WSP._SP(
                 new Abnf._SP(
-                    new Abnf.Inners._percentxTWOZERO(
-                        Abnf.Inners._TWO.Instance,
-                        Abnf.Inners._ZERO.Instance)));
+                    new Abnf.Inners._percentx20(
+                        Abnf.Inners._2.Instance,
+                        Abnf.Inners._0.Instance)));
         }
 
         protected internal override GeneratorV3.Abnf._WSP Accept(AbnfParser.CstNodes.Core.Wsp.Tab node, Root.Void context)
         {
             return new Abnf._WSP._HTAB(
                 new Abnf._HTAB(
-                    new Abnf.Inners._percentxZERONINE(
-                        Abnf.Inners._ZERO.Instance,
-                        Abnf.Inners._NINE.Instance)));
+                    new Abnf.Inners._percentx09(
+                        Abnf.Inners._0.Instance,
+                        Abnf.Inners._9.Instance)));
         }
     }
 }
