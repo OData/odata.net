5e42d7c765148b0f9be90d0cdd85482cbfcd90fe
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-02 10:28:48 -0800

    asdf

--- odata/GeneratorV3/OldToNewConverters/Core/AlphaConverter.cs
+++ odata/GeneratorV3/OldToNewConverters/Core/AlphaConverter.cs
@@ -218,7 +218,7 @@
 
         protected internal override GeneratorV3.Abnf._ALPHA Accept(AbnfParser.CstNodes.Core.Alpha.x61 node, Root.Void context)
         {
-            return new GeneratorV3.Abnf._ALPHA._percentxFOURONEⲻFIVEA(
+            return new GeneratorV3.Abnf._ALPHA._percentxSIXONEⲻSEVENA(
                 new GeneratorV3.Abnf.Inners._percentxSIXONEⲻSEVENA._SIXONE(
                     Abnf.Inners._SIX.Instance,
                     Abnf.Inners._ONE.Instance));
