a9f3e2cb90c927b6149b4cb39abc8f7da640f04d
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-03 13:24:09 -0800

    asdf

---
+++ odata/GeneratorV3/SpikeTranscribers/Rules/DquoteTranscriber.cs
@@ -0,0 +1,20 @@
+﻿namespace GeneratorV3.SpikeTranscribers.Rules
+{
+    using System.Text;
+
+    using GeneratorV3.Abnf;
+
+    public sealed class DquoteTranscriber : ITranscriber<_DQUOTE>
+    {
+        private DquoteTranscriber()
+        {
+        }
+
+        public static DquoteTranscriber Instance { get; } = new DquoteTranscriber();
+
+        public void Transcribe(_DQUOTE value, StringBuilder builder)
+        {
+            builder.Append((char)0x22);
+        }
+    }
+}
