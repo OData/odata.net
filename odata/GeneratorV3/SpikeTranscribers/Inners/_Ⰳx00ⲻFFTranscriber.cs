61c3b2ca01983643eeac3103d8c0bcc555e5e7f6
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-03 13:40:09 -0800

    asdf

---
+++ odata/GeneratorV3/SpikeTranscribers/Inners/_Ⰳx00ⲻFFTranscriber.cs
@@ -0,0 +1,19 @@
+﻿namespace GeneratorV3.SpikeTranscribers.Inners
+{
+    using GeneratorV3.Abnf;
+    using System.Text;
+
+    public sealed class _Ⰳx00ⲻFFTranscriber : ITranscriber<Inners._Ⰳx00ⲻFF>
+    {
+        private _Ⰳx00ⲻFFTranscriber()
+        {
+        }
+
+        public static _Ⰳx00ⲻFFTranscriber Instance { get; } = new _Ⰳx00ⲻFFTranscriber();
+
+        public void Transcribe(Inners._Ⰳx00ⲻFF value, StringBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ odata/GeneratorV3/SpikeTranscribers/Inners/_Ⰳx21ⲻ7ETranscriber.cs
@@ -0,0 +1,20 @@
+﻿namespace GeneratorV3.SpikeTranscribers.Inners
+{
+    using System.Text;
+
+    using GeneratorV3.Abnf;
+
+    public sealed class _Ⰳx21ⲻ7ETranscriber : ITranscriber<Inners._Ⰳx21ⲻ7E>
+    {
+        private _Ⰳx21ⲻ7ETranscriber()
+        {
+        }
+
+        public static _Ⰳx21ⲻ7ETranscriber Instance { get; } = new _Ⰳx21ⲻ7ETranscriber();
+
+        public void Transcribe(Inners._Ⰳx21ⲻ7E value, StringBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
