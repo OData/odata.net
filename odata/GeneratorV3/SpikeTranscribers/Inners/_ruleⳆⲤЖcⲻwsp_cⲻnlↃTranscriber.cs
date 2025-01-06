54e8238dae9c12aaad73973405095c885ce197d9
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-06 12:54:15 -0800

    asdf

---
+++ odata/GeneratorV3/SpikeTranscribers/Inners/_ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber.cs
@@ -0,0 +1,31 @@
+﻿namespace GeneratorV3.SpikeTranscribers.Rules
+{
+    using System.Text;
+
+    using GeneratorV3.Abnf;
+    using Root;
+    using static GeneratorV3.Abnf.Inners;
+
+    public sealed class _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber : ITranscriber<_ruleⳆⲤЖcⲻwsp_cⲻnlↃ>
+    {
+        private _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber()
+        {
+        }
+
+        public static _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber Instance { get; } = new _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber();
+
+        public void Transcribe(_ruleⳆⲤЖcⲻwsp_cⲻnlↃ value, StringBuilder builder)
+        {
+
+        }
+
+        private sealed class Visitor : _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber.Visitor<Void, StringBuilder>
+        {
+            private Visitor()
+            {
+            }
+
+            public static Visitor Instance { get; } = new Visitor();
+        }
+    }
+}
---
+++ odata/GeneratorV3/SpikeTranscribers/Rules/RuleListTranscriber.cs
@@ -0,0 +1,20 @@
+﻿namespace GeneratorV3.SpikeTranscribers.Rules
+{
+    using System.Text;
+
+    using GeneratorV3.Abnf;
+
+    public sealed class RuleListTranscriber : ITranscriber<_rulelist>
+    {
+        private RuleListTranscriber()
+        {
+        }
+
+        public static RuleListTranscriber Instance { get; } = new RuleListTranscriber();
+
+        public void Transcribe(_rulelist value, StringBuilder builder)
+        {
+
+        }
+    }
+}
