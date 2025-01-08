00b4fb8ad102a352e2327a738aa8b25eae85e1df
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-08 10:51:22 -0800

    asdf

--- odata/GeneratorV3/SpikeTranscribers/Inners/_ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber.cs
+++ odata/GeneratorV3/SpikeTranscribers/Inners/_ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber.cs
@@ -1,13 +1,11 @@
 ﻿namespace GeneratorV3.SpikeTranscribers.Rules
 {
-    using System;
     using System.Text;
 
     using GeneratorV3.Abnf;
-    using Root;
-    using static GeneratorV3.Abnf.Inners;
+    using GeneratorV3.SpikeTranscribers.Inners;
 
-    public sealed class _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber : ITranscriber<_ruleⳆⲤЖcⲻwsp_cⲻnlↃ>
+    public sealed class _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber : ITranscriber<Inners._ruleⳆⲤЖcⲻwsp_cⲻnlↃ>
     {
         private _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber()
         {
@@ -15,12 +13,12 @@
 
         public static _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber Instance { get; } = new _ruleⳆⲤЖcⲻwsp_cⲻnlↃTranscriber();
 
-        public void Transcribe(_ruleⳆⲤЖcⲻwsp_cⲻnlↃ value, StringBuilder builder)
+        public void Transcribe(Inners._ruleⳆⲤЖcⲻwsp_cⲻnlↃ value, StringBuilder builder)
         {
             Visitor.Instance.Visit(value, builder);
         }
 
-        private sealed class Visitor : _ruleⳆⲤЖcⲻwsp_cⲻnlↃ.Visitor<Root.Void, StringBuilder>
+        private sealed class Visitor : Inners._ruleⳆⲤЖcⲻwsp_cⲻnlↃ.Visitor<Root.Void, StringBuilder>
         {
             private Visitor()
             {
@@ -28,14 +26,18 @@
 
             public static Visitor Instance { get; } = new Visitor();
 
-            protected internal override Root.Void Accept(_ruleⳆⲤЖcⲻwsp_cⲻnlↃ._rule node, StringBuilder context)
+            protected internal override Root.Void Accept(Inners._ruleⳆⲤЖcⲻwsp_cⲻnlↃ._rule node, StringBuilder context)
             {
+                RuleTranscriber.Instance.Transcribe(node._rule_1, context);
 
+                return default;
             }
 
-            protected internal override Root.Void Accept(_ruleⳆⲤЖcⲻwsp_cⲻnlↃ._ⲤЖcⲻwsp_cⲻnlↃ node, StringBuilder context)
+            protected internal override Root.Void Accept(Inners._ruleⳆⲤЖcⲻwsp_cⲻnlↃ._ⲤЖcⲻwsp_cⲻnlↃ node, StringBuilder context)
             {
-                throw new NotImplementedException();
+                _ⲤЖcⲻwsp_cⲻnlↃTranscriber.Instance.Transcribe(node._ⲤЖcⲻwsp_cⲻnlↃ_1, context);
+
+                return default;
             }
         }
     }
