46d4847e3722e6730a294ff52cae757e6d83d395
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-08 17:36:06 -0800

    asfd

---
+++ odata/_GeneratorV4/TranscribersGenerator.cs
@@ -0,0 +1,69 @@
+﻿namespace _GeneratorV4
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using AbnfParserGenerator;
+
+    public sealed class TranscribersGenerator
+    {
+        private readonly string rulesNamespace;
+        private readonly string innersNamespace;
+
+        public TranscribersGenerator(string rulesNamespace, string innersNamespace)
+        {
+            this.rulesNamespace = rulesNamespace;
+            this.innersNamespace = innersNamespace;
+        }
+
+        public IEnumerable<Class> Generate(IEnumerable<Class> cstNodes)
+        {
+            foreach (var cstNode in cstNodes)
+            {
+                var transcriberName = $"{cstNode.Name}Transcriber";
+                yield return new Class(
+                    AccessModifier.Public,
+                    ClassModifier.Sealed,
+                    transcriberName,
+                    Enumerable.Empty<string>(),
+                    $"ITranscriber<{cstNode.Name}>",
+                    new[]
+                    {
+                        new ConstructorDefinition(
+                            AccessModifier.Private,
+                            Enumerable.Empty<MethodParameter>(),
+                            Enumerable.Empty<string>()),
+                    },
+                    new[]
+                    {
+                        new MethodDefinition(
+                            AccessModifier.Public,
+                            ClassModifier.None,
+                            false,
+                            "void",
+                            Enumerable.Empty<string>(),
+                            "Transcribe",
+                            new[]
+                            {
+                                //// TODO you should be using a fully qualified name here, but `class` doesn't include the namespace
+                                new MethodParameter(cstNode.Name, "value"),
+                                new MethodParameter("StringBuilder", "builder"),
+                            },
+                            string.Empty), //// TODO generate the method body
+                    },
+                    Enumerable.Empty<Class>(), //// TODO sometimes you need a nested visitor
+                    new[]
+                    {
+                        new PropertyDefinition(
+                            AccessModifier.Public,
+                            true,
+                            transcriberName,
+                            "Instance",
+                            true,
+                            false,
+                            $"new {transcriberName}();"),
+                    });
+            }
+        }
+    }
+}
