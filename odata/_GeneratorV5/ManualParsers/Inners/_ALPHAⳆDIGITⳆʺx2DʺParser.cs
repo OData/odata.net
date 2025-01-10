e1bc3330a1bf98265507a927ef8ddca0ce4b64fd
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-10 09:08:44 -0800

    asdf

---
+++ odata/_GeneratorV5/ManualParsers/Inners/_ALPHAⳆDIGITⳆʺx2DʺParser.cs
@@ -0,0 +1,9 @@
+﻿namespace _GeneratorV5.ManualParsers.Inners
+{
+    using Sprache;
+
+    public static class _ALPHAⳆDIGITⳆʺx2DʺParser
+    {
+        public static Parser<__Generated.CstNodes.Inners._ALPHAⳆDIGITⳆʺx2Dʺ> Instance { get; }
+    }
+}
--- odata/_GeneratorV5/ManualParsers/Inners/_ⲤALPHAⳆDIGITⳆʺx2DʺↃParser.cs
+++ odata/_GeneratorV5/ManualParsers/Inners/_ⲤALPHAⳆDIGITⳆʺx2DʺↃParser.cs
@@ -4,6 +4,8 @@
 
     public static class _ⲤALPHAⳆDIGITⳆʺx2DʺↃParser
     {
-        public static Parser<__Generated.CstNodes.Inners._ⲤALPHAⳆDIGITⳆʺx2DʺↃ> Instance { get; }
+        public static Parser<__Generated.CstNodes.Inners._ⲤALPHAⳆDIGITⳆʺx2DʺↃ> Instance { get; } =
+            from _ALPHAⳆDIGITⳆʺx2Dʺ_1 in _GeneratorV5.ManualParsers.Inners._ALPHAⳆDIGITⳆʺx2DʺParser.Instance
+            select new __Generated.CstNodes.Inners._ⲤALPHAⳆDIGITⳆʺx2DʺↃ(_ALPHAⳆDIGITⳆʺx2Dʺ_1);
     }
 }
