2298abdba04bbf81c5a7fd204a349072cf767699
Samuel Wanjohi <sam.wanjohi94@gmail.com>
2025-01-10 14:38:41 +0300

    Add batch request tests

---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/BatchRequestTests/Server/BatchRequestTestsController.cs
@@ -0,0 +1,92 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="BatchRequestTestsController.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.OData.Client.E2E.Tests.Common.Server.Default;
+
+namespace Microsoft.OData.Client.E2E.Tests.BatchRequestTests.Server;
+
+public class BatchRequestTestsController : ODataController
+{
+    private static DefaultDataSource _dataSource;
+
+    [EnableQuery]
+    [HttpGet("odata/Accounts")]
+    public IActionResult GetAccounts()
+    {
+        var result = _dataSource.Accounts;
+
+        return Ok(result);
+    }
+
+    [EnableQuery]
+    [HttpGet("odata/Accounts({key})")]
+    public IActionResult GetAccount([FromRoute] int key)
+    {
+        var result = _dataSource.Accounts?.SingleOrDefault(a => a.AccountID == key);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [EnableQuery]
+    [HttpGet("odata/Accounts({key})/MyPaymentInstruments")]
+    public IActionResult GetAccountMyPaymentInstruments([FromRoute] int key)
+    {
+        var result = _dataSource.Accounts?.SingleOrDefault(a => a.AccountID == key);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result.MyPaymentInstruments);
+    }
+
+    [EnableQuery]
+    [HttpGet("odata/Accounts({accountKey})/MyPaymentInstruments({myPaymentInstrumentKey})/BillingStatements({billingStatementKey})")]
+    public IActionResult GetAccountBillingStatement([FromRoute] int accountKey, [FromRoute] int myPaymentInstrumentKey, [FromRoute] int billingStatementKey)
+    {
+        var account = _dataSource.Accounts?.SingleOrDefault(a => a.AccountID == accountKey);
+        var myPaymentInstrument = account?.MyPaymentInstruments?.SingleOrDefault(a => a.PaymentInstrumentID == myPaymentInstrumentKey);
+        var billingStatement = myPaymentInstrument?.BillingStatements?.SingleOrDefault(a => a.StatementID == billingStatementKey);
+        if (billingStatement == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(billingStatement);
+    }
+
+    [HttpPost("odata/Accounts({key})/MyPaymentInstruments")]
+    public IActionResult GetAccountBillingStatement([FromRoute] int key, [FromBody] PaymentInstrument paymentInstrument)
+    {
+        var account = _dataSource.Accounts?.SingleOrDefault(a => a.AccountID == key);
+        if (account == null)
+        {
+            return NotFound();
+        }
+
+        account.MyPaymentInstruments ??= [];
+        account.MyPaymentInstruments.Add(paymentInstrument);
+
+        return Created(paymentInstrument);
+    }
+
+    [HttpPost("odata/batchrequests/Default.ResetDefaultDataSource")]
+    public IActionResult ResetDefaultDataSource()
+    {
+        _dataSource = DefaultDataSource.CreateInstance();
+
+        return Ok();
+    }
+}
