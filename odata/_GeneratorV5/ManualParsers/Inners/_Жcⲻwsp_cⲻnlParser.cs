3f3011a48de583d033d18c2248e044ff82f99619
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-10 09:01:25 -0800

    asdf

--- odata/_GeneratorV5/ManualParsers/Inners/_Жcⲻwsp_cⲻnlParser.cs
+++ odata/_GeneratorV5/ManualParsers/Inners/_Жcⲻwsp_cⲻnlParser.cs
@@ -4,6 +4,9 @@
 
     public static class _Жcⲻwsp_cⲻnlParser
     {
-        public static Parser<__Generated.CstNodes.Inners._Жcⲻwsp_cⲻnl> Instance { get; }
+        public static Parser<__Generated.CstNodes.Inners._Жcⲻwsp_cⲻnl> Instance { get; } =
+            from _cⲻwsp_1 in _GeneratorV5.ManualParsers.Rules._cⲻwspParser.Instance.Many()
+            from _cⲻnl_1 in _GeneratorV5.ManualParsers.Rules._cⲻnlParser.Instance
+            select new __Generated.CstNodes.Inners._Жcⲻwsp_cⲻnl(_cⲻwsp_1, _cⲻnl_1);
     }
 }
