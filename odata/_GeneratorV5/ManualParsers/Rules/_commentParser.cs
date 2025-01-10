ab623cd46695aacf697351561e1b71ef6800b7d8
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-09 18:00:09 -0800

    asdf

---
+++ odata/_GeneratorV5/ManualParsers/Rules/_commentParser.cs
@@ -0,0 +1,9 @@
+﻿namespace _GeneratorV5.ManualParsers.Rules
+{
+    using Sprache;
+
+    public static class _commentParser
+    {
+        public static Parser<__Generated.CstNodes.Rules._comment> Instance { get; }
+    }
+}
--- odata/_GeneratorV5/ManualParsers/Rules/_cⲻnlParser.cs
+++ odata/_GeneratorV5/ManualParsers/Rules/_cⲻnlParser.cs
@@ -4,6 +4,17 @@
 
     public static class _cⲻnlParser
     {
-        public static Parser<__Generated.CstNodes.Rules._cⲻnl> Instance { get; }
+        public static Parser<__Generated.CstNodes.Rules._cⲻnl> Instance { get; } =
+            Inners._comment
+            .Or<__Generated.CstNodes.Rules._cⲻnl>(Inners._CRLF);
+
+        private static class Inners
+        {
+            public static Parser<__Generated.CstNodes.Rules._cⲻnl._comment> _comment { get; } =
+                from _comment_1 in _GeneratorV5.ManualParsers.Rules._commentParser.Instance
+                select new __Generated.CstNodes.Rules._cⲻnl._comment(_comment_1);
+
+            public static Parser<__Generated.CstNodes.Rules._cⲻnl._CRLF> _CRLF { get; }
+        }
     }
 }
