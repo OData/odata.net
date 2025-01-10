b4d61ceb1dbd33e596c7ec6f7d7040dcec7b80d7
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-09 17:57:18 -0800

    asdf

---
+++ odata/_GeneratorV5/ManualParsers/Rules/_alternationParser.cs
@@ -0,0 +1,9 @@
+﻿namespace _GeneratorV5.ManualParsers.Rules
+{
+    using Sprache;
+
+    public static class _alternationParser
+    {
+        public static Parser<__Generated.CstNodes.Rules._alternation> Instance { get; }
+    }
+}
