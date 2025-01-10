b6c58f6c0a396efb29a1709e4a53ae9143edd4de
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-10 09:16:43 -0800

    sdf

---
+++ odata/_GeneratorV5/ManualParsers/Inners/_ʺx3DʺⳆʺx3Dx2FʺParser.cs
@@ -0,0 +1,9 @@
+﻿namespace _GeneratorV5.ManualParsers.Inners
+{
+    using Sprache;
+
+    public static class _ʺx3DʺⳆʺx3Dx2FʺParser
+    {
+        public static Parser<__Generated.CstNodes.Inners._ʺx3DʺⳆʺx3Dx2Fʺ> Instance { get; }
+    }
+}
--- odata/_GeneratorV5/ManualParsers/Inners/_Ⲥʺx3DʺⳆʺx3Dx2FʺↃ_1Parser.cs
+++ odata/_GeneratorV5/ManualParsers/Inners/_Ⲥʺx3DʺⳆʺx3Dx2FʺↃ_1Parser.cs
@@ -4,6 +4,8 @@
 
     public static class _Ⲥʺx3DʺⳆʺx3Dx2FʺↃ_1Parser
     {
-        public static Parser<__Generated.CstNodes.Inners._Ⲥʺx3DʺⳆʺx3Dx2FʺↃ> Instance { get; }
+        public static Parser<__Generated.CstNodes.Inners._Ⲥʺx3DʺⳆʺx3Dx2FʺↃ> Instance { get; } =
+            from _ʺx3DʺⳆʺx3Dx2Fʺ_1 in _GeneratorV5.ManualParsers.Inners._ʺx3DʺⳆʺx3Dx2FʺParser.Instance
+            select new __Generated.CstNodes.Inners._Ⲥʺx3DʺⳆʺx3Dx2FʺↃ(_ʺx3DʺⳆʺx3Dx2Fʺ_1);
     }
 }
