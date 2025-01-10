b914541ac80d6f1857b6cb361f53d0ea63979e0b
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-09 17:23:56 -0800

    asdf

---
+++ odata/_GeneratorV5/ManualParsers/Inners/_ruleⳆⲤЖcⲻwsp_cⲻnlↃParser.cs
@@ -0,0 +1,9 @@
+﻿namespace _GeneratorV5.ManualParsers.Inners
+{
+    using Sprache;
+
+    public static class _ruleⳆⲤЖcⲻwsp_cⲻnlↃParser
+    {
+        public static Parser<__Generated.CstNodes.Inners._ruleⳆⲤЖcⲻwsp_cⲻnlↃ> Instance { get; }
+    }
+}
---
+++ odata/_GeneratorV5/ManualParsers/Rules/_rulelistParser.cs
@@ -0,0 +1,12 @@
+﻿namespace _GeneratorV5.ManualParsers.Rules
+{
+    using Sprache;
+
+    public static class _rulelistParser
+    {
+        public static Parser<__Generated.CstNodes.Rules._rulelist> Instance { get; } =
+            from _ⲤruleⳆⲤЖcⲻwsp_cⲻnlↃↃ_1 in _GeneratorV5.ManualParsers.Inners._ⲤruleⳆⲤЖcⲻwsp_cⲻnlↃↃParser.Instance.AtLeastOnce()
+            select new __Generated.CstNodes.Rules._rulelist(_ⲤruleⳆⲤЖcⲻwsp_cⲻnlↃↃ_1);
+
+    }
+}
