ebff353a2d302c22a6e78acd7634d30e835c4ed9
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-10 10:13:51 -0800

    asdf

--- odata/_GeneratorV5/ParsersGenerator.cs
+++ odata/_GeneratorV5/ParsersGenerator.cs
@@ -1,8 +1,9 @@
 ﻿namespace _GeneratorV5
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
-
+    using System.Runtime.CompilerServices;
     using AbnfParserGenerator;
 
     public sealed class ParsersGenerator
@@ -22,14 +23,20 @@
                 (
                     new Namespace(
                         this.ruleParsersNamespace,
-                        GenerateParsers(cstNodes.RuleCstNodes),
+                        GenerateParsers(
+                            cstNodes.RuleCstNodes,
+                            cstNodes.RuleCstNodes.Name,
+                            cstNodes.InnerCstNodes.Name),
                         new[]
                         {
                             "Sprache",
                         }),
                     new Namespace(
                         this.innerParsersNamespace,
-                        GenerateParsers(cstNodes.InnerCstNodes),
+                        GenerateParsers(
+                            cstNodes.InnerCstNodes,
+                            cstNodes.RuleCstNodes.Name,
+                            cstNodes.InnerCstNodes.Name),
                         new[]
                         {
                             "Sprache",
@@ -37,13 +44,45 @@
                 );
         }
 
-        private IEnumerable<Class> GenerateParsers(Namespace @namespace)
+        private IEnumerable<Class> GenerateParsers(Namespace @namespace, string ruleCstNodesNamespace, string innerCstNodesNamespace)
         {
             foreach (var @class in @namespace.Classes)
             {
+                var nonStaticProperties = @class.Properties.Where(property => !property.IsStatic);
+
                 IEnumerable<Class> nestedClasses;
                 IEnumerable<PropertyDefinition> property;
-                if (@class.NestedClasses.Any())
+                if (nonStaticProperties.Any())
+                {
+                    nestedClasses = Enumerable.Empty<Class>();
+                    var initializer = string
+                        .Concat(
+                            string.Join(
+                                Environment.NewLine,
+                                    nonStaticProperties
+                                    .Select(
+                                        property =>
+                                            $"from {property.Name} in {FromNodeNamespaceToParserNamespace(@namespace.Name, ruleCstNodesNamespace, innerCstNodesNamespace)}{StripPropertyType(property.Type)}Parser.Instance{(property.Type.StartsWith("System.Collections.Generic.IEnumerable<") ? ".Many()" : string.Empty)}")), //// TODO how to handle different ranges of ienumerable (at most two, at least 3, etc.)
+                            Environment.NewLine,
+                            $"select new {@namespace.Name}.{@class.Name}(",
+                            string.Join(
+                                ", ",
+                                nonStaticProperties
+                                    .Select(property => property.Name)),
+                            ");");
+                    property = new[]
+                    {
+                        new PropertyDefinition(
+                            AccessModifier.Public,
+                            true,
+                            $"Parser<{@namespace.Name}.{@class.Name}>",
+                            "Instance",
+                            true,
+                            false,
+                            initializer),
+                    };
+                }
+                else if (@class.NestedClasses.Any())
                 {
                     //// TODO implement this for dus
                     nestedClasses = Enumerable.Empty<Class>();
@@ -61,6 +100,7 @@
                 }
                 else
                 {
+                    //// TODO implement this for terminal nodes
                     nestedClasses = Enumerable.Empty<Class>();
                     property = new[]
                     {
@@ -71,7 +111,7 @@
                             "Instance",
                             true,
                             false,
-                            null), //// TODO initializer
+                            null),
                     };
                 }
 
@@ -86,6 +126,38 @@
                     nestedClasses,
                     property);
             }
+        }
+
+        private string StripPropertyType(string propertyType)
+        {
+            var collectionDelimiter = "System.Collections.Generic.IEnunerable<";
+            if (propertyType.StartsWith(collectionDelimiter))
+            {
+                return propertyType.Substring(collectionDelimiter.Length, propertyType.Length - collectionDelimiter.Length - 1);
+            }
+
+            var delimiterIndex = propertyType.LastIndexOf(".");
+            if (delimiterIndex < 0)
+            {
+                return propertyType;
+            }
+
+            return propertyType.Substring(delimiterIndex + 1);
+        }
+
+        private string FromNodeNamespaceToParserNamespace(string namespaceToUpdate, string ruleCstNodesNamespace, string innerCstNodesNamespace)
+        {
+            if (namespaceToUpdate.StartsWith(ruleCstNodesNamespace))
+            {
+                return $"{this.ruleParsersNamespace}.{namespaceToUpdate.Substring(ruleCstNodesNamespace.Length)}";
+            }
+
+            if (namespaceToUpdate.StartsWith(innerCstNodesNamespace))
+            {
+                return $"{this.innerParsersNamespace}.{namespaceToUpdate.Substring(innerCstNodesNamespace.Length)}";
+            }
+
+            return namespaceToUpdate;
         }
     }
 }
