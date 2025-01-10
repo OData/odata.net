e4c08624a78291799927b7172d3e4e8b576d46e5
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-10 09:07:08 -0800

    asdf

---
+++ odata/_GeneratorV5/ManualParsers/Inners/_Ⰳx61ⲻ7AParser.cs
@@ -0,0 +1,9 @@
+﻿namespace _GeneratorV5.ManualParsers.Inners
+{
+    using Sprache;
+
+    public static class _Ⰳx61ⲻ7AParser
+    {
+        public static Parser<__Generated.CstNodes.Inners._Ⰳx61ⲻ7A> Instance { get; }
+    }
+}
