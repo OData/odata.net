5f479128be02c8b3ece022b58dd351aac5e3a5a3
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-10 09:24:54 -0800

    asdf

--- odata/_GeneratorV5/ManualParsers/Rules/_CRLFParser.cs
+++ odata/_GeneratorV5/ManualParsers/Rules/_CRLFParser.cs
@@ -4,6 +4,9 @@
 
     public static class _CRLFParser
     {
-        public static Parser<__Generated.CstNodes.Rules._CRLF> Instance { get; }
+        public static Parser<__Generated.CstNodes.Rules._CRLF> Instance { get; } =
+            from _CR_1 in _GeneratorV5.ManualParsers.Rules._CRParser.Instance
+            from _LF_1 in _GeneratorV5.ManualParsers.Rules._LFParser.Instance
+            select new __Generated.CstNodes.Rules._CRLF(_CR_1, _LF_1);
     }
 }
---
+++ odata/_GeneratorV5/ManualParsers/Rules/_CRParser.cs
@@ -0,0 +1,9 @@
+﻿namespace _GeneratorV5.ManualParsers.Rules
+{
+    using Sprache;
+
+    public static class _CRParser
+    {
+        public static Parser<__Generated.CstNodes.Rules._CR> Instance { get; }
+    }
+}
