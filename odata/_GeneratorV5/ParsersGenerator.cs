8eac0d32461ef949609028a13bd0ce6a3f8a7637
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-13 15:36:56 -0800

    asfd

--- odata/_GeneratorV5/ParsersGenerator.cs
+++ odata/_GeneratorV5/ParsersGenerator.cs
@@ -20,6 +20,8 @@
         public (Namespace RuleParsers, Namespace InnerParsers) Generate((Namespace RuleCstNodes, Namespace InnerCstNodes) cstNodes)
         {
             //// TODO create a test that illustrates the innner vs rule name conflicts that can happen
+            //// TODO how to detect ambiguities
+            //// TODO correctly handle "repeats" of values (at least one, at most 5, between 2 and six, etc)
 
             return
                 (
