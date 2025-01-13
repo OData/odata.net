6e2da41b6b6a366a9b8562bfa4e92be4e0fd40f7
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-13 10:13:29 -0800

    updating generated parsers to sometimes call atleastonce when necessary to see if that's the problem

--- odata/__Generated/Parsers/Inners/_1Жcⲻwsp_repetitionParser.cs
+++ odata/__Generated/Parsers/Inners/_1Жcⲻwsp_repetitionParser.cs
@@ -4,7 +4,7 @@ namespace __Generated.Parsers.Inners
     
     public static class _1Жcⲻwsp_repetitionParser
     {
-        public static Parser<__Generated.CstNodes.Inners._1Жcⲻwsp_repetition> Instance { get; } = from _cⲻwsp_1 in __Generated.Parsers.Rules._cⲻwspParser.Instance.Many()
+        public static Parser<__Generated.CstNodes.Inners._1Жcⲻwsp_repetition> Instance { get; } = from _cⲻwsp_1 in __Generated.Parsers.Rules._cⲻwspParser.Instance.AtLeastOnce()
 from _repetition_1 in __Generated.Parsers.Rules._repetitionParser.Instance
 select new __Generated.CstNodes.Inners._1Жcⲻwsp_repetition(_cⲻwsp_1, _repetition_1);
     }
--- odata/__Generated/Parsers/Rules/_decⲻvalParser.cs
+++ odata/__Generated/Parsers/Rules/_decⲻvalParser.cs
@@ -5,7 +5,7 @@ namespace __Generated.Parsers.Rules
     public static class _decⲻvalParser
     {
         public static Parser<__Generated.CstNodes.Rules._decⲻval> Instance { get; } = from _ʺx64ʺ_1 in __Generated.Parsers.Inners._ʺx64ʺParser.Instance
-from _DIGIT_1 in __Generated.Parsers.Rules._DIGITParser.Instance.Many()
+from _DIGIT_1 in __Generated.Parsers.Rules._DIGITParser.Instance.AtLeastOnce()
 from _1ЖⲤʺx2Eʺ_1ЖDIGITↃⳆⲤʺx2Dʺ_1ЖDIGITↃ_1 in __Generated.Parsers.Inners._1ЖⲤʺx2Eʺ_1ЖDIGITↃⳆⲤʺx2Dʺ_1ЖDIGITↃParser.Instance.Optional()
 select new __Generated.CstNodes.Rules._decⲻval(_ʺx64ʺ_1, _DIGIT_1, _1ЖⲤʺx2Eʺ_1ЖDIGITↃⳆⲤʺx2Dʺ_1ЖDIGITↃ_1.GetOrElse(null));
     }
