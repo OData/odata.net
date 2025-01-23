b76f6f2b68088229862917d02ef82d2db7740c1d
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-23 09:54:12 -0800

    asdf

---
+++ odata/CombinatorParsingV2/IParser.cs
@@ -0,0 +1,25 @@
+﻿namespace CombinatorParsingV2
+{
+    //// TODO covariance and contravariance
+    
+    public interface IParser<TToken, out TParsed>
+    {
+        IOutput<TToken, TParsed> Parse(IInput<TToken> input);
+    }
+
+    public interface IInput<out TToken>
+    {
+        TToken Current { get; }
+
+        IInput<TToken> Next();
+    }
+
+    public interface IOutput<out TToken, out TParsed>
+    {
+        bool Success { get; }
+
+        public TParsed Parsed { get; }
+
+        IInput<TToken> Remainder { get; }
+    }
+}
