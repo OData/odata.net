aa16fa54766004c4c2484209126da70b5c7dd432
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-23 09:55:15 -0800

    sadf

--- odata/CombinatorParsingV2/IParser.cs
+++ odata/CombinatorParsingV2/IParser.cs
@@ -7,14 +7,14 @@
         IOutput<TToken, TParsed> Parse(IInput<TToken> input);
     }
 
-    public interface IInput<out TToken>
+    public interface IInput<out TToken> //// TODO make a struct an use `in` parameter for `iparser.parse` method?
     {
         TToken Current { get; }
 
         IInput<TToken> Next();
     }
 
-    public interface IOutput<out TToken, out TParsed>
+    public interface IOutput<out TToken, out TParsed> //// TODO make a struct and use `in` parameter whereever applicable?
     {
         bool Success { get; }
 
