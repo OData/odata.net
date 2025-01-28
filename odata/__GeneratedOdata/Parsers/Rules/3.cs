ed1bd5338f38c40dbb02e6309eec3ec0300e10f9
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-28 10:13:07 -0800

    sdg

--- odata/__GeneratedOdata/Parsers/Rules/3.cs
+++ odata/__GeneratedOdata/Parsers/Rules/3.cs
@@ -8,7 +8,9 @@ namespace __GeneratedOdata.Parsers.Rules
     public static class _odataRelativeUriParser
     {
         public static IParser<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri> Instance { get; } = (_ʺx24x62x61x74x63x68ʺ_꘡ʺx3Fʺ_batchOptions꘡Parser.Instance).Or<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri>(_ʺx24x65x6Ex74x69x74x79ʺ_ʺx3Fʺ_entityOptionsParser.Instance).Or<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri>(_ʺx24x65x6Ex74x69x74x79ʺ_ʺx2Fʺ_qualifiedEntityTypeName_ʺx3Fʺ_entityCastOptionsParser.Instance).Or<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri>(_ʺx24x6Dx65x74x61x64x61x74x61ʺ_꘡ʺx3Fʺ_metadataOptions꘡_꘡context꘡Parser.Instance).Or<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri>(_resourcePath_꘡ʺx3Fʺ_queryOptions꘡Parser.Instance);
-            ////new Parser();
+                                                                                                           ////new Parser();
+            //// PERF
+            ////_resourcePath_꘡ʺx3Fʺ_queryOptions꘡Parser.Instance;
 
         private sealed class Parser : IParser<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri>
         {
@@ -435,6 +437,23 @@ select new __GeneratedOdata.CstNodes.Rules._odataRelativeUri._ʺx24x6Dx65x74x61x
             public static IParser<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri._resourcePath_꘡ʺx3Fʺ_queryOptions꘡> Instance { get; } = from _resourcePath_1 in __GeneratedOdata.Parsers.Rules._resourcePathParser.Instance
 from _ʺx3Fʺ_queryOptions_1 in __GeneratedOdata.Parsers.Inners._ʺx3Fʺ_queryOptionsParser.Instance.Optional()
 select new __GeneratedOdata.CstNodes.Rules._odataRelativeUri._resourcePath_꘡ʺx3Fʺ_queryOptions꘡(_resourcePath_1, _ʺx3Fʺ_queryOptions_1.GetOrElse(null));
+
+            //// PERF
+            /*public static IParser<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri._resourcePath_꘡ʺx3Fʺ_queryOptions꘡> Instance { get; } =
+                new Parser();*/
+
+            private sealed class Parser : IParser<char, __GeneratedOdata.CstNodes.Rules._odataRelativeUri._resourcePath_꘡ʺx3Fʺ_queryOptions꘡>
+            {
+                public IOutput<char, _odataRelativeUri._resourcePath_꘡ʺx3Fʺ_queryOptions꘡> Parse(IInput<char>? input)
+                {
+                    var _resourcePath_1 = __GeneratedOdata.Parsers.Rules._resourcePathParser.Instance.Parse(input);
+                    var _ʺx3Fʺ_queryOptions_1 = __GeneratedOdata.Parsers.Inners._ʺx3Fʺ_queryOptionsParser.Instance.Optional().Parse(_resourcePath_1.Remainder);
+                    return Output.Create(
+                        true, 
+                        new __GeneratedOdata.CstNodes.Rules._odataRelativeUri._resourcePath_꘡ʺx3Fʺ_queryOptions꘡(_resourcePath_1.Parsed, _ʺx3Fʺ_queryOptions_1.Parsed.GetOrElse(null)), 
+                        _ʺx3Fʺ_queryOptions_1.Remainder);
+                }
+            }
         }
     }
     
