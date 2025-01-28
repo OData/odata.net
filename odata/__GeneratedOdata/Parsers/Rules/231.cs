f5c6709dea74feeb4178a9a79b91ea7fc8906dc3
Garrett DeBruin <gdebruin@microsoft.com>
2025-01-28 10:14:51 -0800

    asdf

--- odata/__GeneratedOdata/Parsers/Rules/231.cs
+++ odata/__GeneratedOdata/Parsers/Rules/231.cs
@@ -1,11 +1,28 @@
 namespace __GeneratedOdata.Parsers.Rules
 {
+    using __GeneratedOdata.CstNodes.Rules;
     using CombinatorParsingV2;
     
     public static class _entitySetNameParser
     {
         public static IParser<char, __GeneratedOdata.CstNodes.Rules._entitySetName> Instance { get; } = from _odataIdentifier_1 in __GeneratedOdata.Parsers.Rules._odataIdentifierParser.Instance
 select new __GeneratedOdata.CstNodes.Rules._entitySetName(_odataIdentifier_1);
+
+        //// PERF
+        //// public static IParser<char, __GeneratedOdata.CstNodes.Rules._entitySetName> Instance { get; } = new Parser();
+
+        private sealed class Parser : IParser<char, __GeneratedOdata.CstNodes.Rules._entitySetName>
+        {
+            public IOutput<char, _entitySetName> Parse(IInput<char>? input)
+            {
+                var _odataIdentifier_1 = __GeneratedOdata.Parsers.Rules._odataIdentifierParser.Instance.Parse(input);
+
+                return Output.Create(
+                    true,
+                    new __GeneratedOdata.CstNodes.Rules._entitySetName(_odataIdentifier_1.Parsed),
+                    _odataIdentifier_1.Remainder);
+            }
+        }
     }
     
 }
