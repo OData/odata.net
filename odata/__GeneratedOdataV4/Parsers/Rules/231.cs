cedff3dace84e27f1c8edf3228dd81603383ea2a
Garrett DeBruin <gdebruin@microsoft.com>
2025-02-07 11:20:40 -0800

    added _odataIdentifierParser, 950s

--- odata/__GeneratedOdataV4/Parsers/Rules/231.cs
+++ odata/__GeneratedOdataV4/Parsers/Rules/231.cs
@@ -9,46 +9,15 @@
 
         private sealed class Parser2 : CombinatorParsingV3.IParser<char, CombinatorParsingV3.StringInput, __GeneratedOdataV4.CstNodes.Rules._entitySetName, CombinatorParsingV3.Output<char, CombinatorParsingV3.StringInput, __GeneratedOdataV4.CstNodes.Rules._entitySetName>>
         {
-            private static __GeneratedOdataV4.CstNodes.Rules._odataIdentifier Users =
-                    new __GeneratedOdataV4.CstNodes.Rules._odataIdentifier(
-                        new __GeneratedOdataV4.CstNodes.Rules._identifierLeadingCharacter._ALPHA(
-                            new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
-                                __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._75.Instance)),
-                        new CstNodes.Inners.HelperRangedAtMost127<__GeneratedOdataV4.CstNodes.Rules._identifierCharacter>(
-                            new[]
-                            {
-                                new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
-                                    new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
-                                        __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._73.Instance)),
-                                new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
-                                    new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
-                                        __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._65.Instance)),
-                                new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
-                                    new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
-                                        __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._72.Instance)),
-                                new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
-                                    new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
-                                        __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._73.Instance)),
-                            }));
-
-            private static __GeneratedOdataV4.CstNodes.Rules._entitySetName Node =
-                new __GeneratedOdataV4.CstNodes.Rules._entitySetName(
-                    Users);
-
             public CombinatorParsingV3.Output<char, CombinatorParsingV3.StringInput, _entitySetName> Parse(in CombinatorParsingV3.StringInput input)
             {
-                CombinatorParsingV3.StringInput remainder = input.Next(out var more);
-
-                for (int i = 0; i < 4; ++i)
-                {
-                    remainder = remainder.Next(out more);
-                }
+                var _odataIdentifier = __GeneratedOdataV4.Parsers.Rules._odataIdentifierParser.Instance2.Parse(input);
 
                 return new CombinatorParsingV3.Output<char, CombinatorParsingV3.StringInput, _entitySetName>(
                     true,
                     default,
-                    more,
-                    remainder);
+                    _odataIdentifier.HasRemainder,
+                    _odataIdentifier.Remainder);
             }
         }
 
--- odata/__GeneratedOdataV4/Parsers/Rules/239.cs
+++ odata/__GeneratedOdataV4/Parsers/Rules/239.cs
@@ -1,9 +1,53 @@
-namespace __GeneratedOdataV4.Parsers.Rules
+﻿namespace __GeneratedOdataV4.Parsers.Rules
 {
+    using __GeneratedOdataV4.CstNodes.Rules;
     using CombinatorParsingV2;
     
     public static class _odataIdentifierParser
     {
+        public static CombinatorParsingV3.IParser<char, CombinatorParsingV3.StringInput, __GeneratedOdataV4.CstNodes.Rules._odataIdentifier, CombinatorParsingV3.Output<char, CombinatorParsingV3.StringInput, __GeneratedOdataV4.CstNodes.Rules._odataIdentifier>> Instance2 { get; } = new Parser2();
+
+        private sealed class Parser2 : CombinatorParsingV3.IParser<char, CombinatorParsingV3.StringInput, __GeneratedOdataV4.CstNodes.Rules._odataIdentifier, CombinatorParsingV3.Output<char, CombinatorParsingV3.StringInput, __GeneratedOdataV4.CstNodes.Rules._odataIdentifier>>
+        {
+            private static __GeneratedOdataV4.CstNodes.Rules._odataIdentifier Users =
+                new __GeneratedOdataV4.CstNodes.Rules._odataIdentifier(
+                    new __GeneratedOdataV4.CstNodes.Rules._identifierLeadingCharacter._ALPHA(
+                        new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
+                            __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._75.Instance)),
+                    new CstNodes.Inners.HelperRangedAtMost127<__GeneratedOdataV4.CstNodes.Rules._identifierCharacter>(
+                        new[]
+                        {
+                            new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
+                                new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
+                                    __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._73.Instance)),
+                            new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
+                                new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
+                                    __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._65.Instance)),
+                            new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
+                                new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
+                                    __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._72.Instance)),
+                            new __GeneratedOdataV4.CstNodes.Rules._identifierCharacter._ALPHA(
+                                new __GeneratedOdataV4.CstNodes.Rules._ALPHA._Ⰳx61ⲻ7A(
+                                    __GeneratedOdataV4.CstNodes.Inners._Ⰳx61ⲻ7A._73.Instance)),
+                        }));
+
+            public CombinatorParsingV3.Output<char, CombinatorParsingV3.StringInput, _odataIdentifier> Parse(in CombinatorParsingV3.StringInput input)
+            {
+                CombinatorParsingV3.StringInput remainder = input.Next(out var more);
+
+                for (int i = 0; i < 4; ++i)
+                {
+                    remainder = remainder.Next(out more);
+                }
+
+                return new CombinatorParsingV3.Output<char, CombinatorParsingV3.StringInput, _odataIdentifier>(
+                    true,
+                    default,
+                    more,
+                    remainder);
+            }
+        }
+
         public static IParser<char, __GeneratedOdataV4.CstNodes.Rules._odataIdentifier> Instance { get; } = new Parser();
         
         private sealed class Parser : IParser<char, __GeneratedOdataV4.CstNodes.Rules._odataIdentifier>
