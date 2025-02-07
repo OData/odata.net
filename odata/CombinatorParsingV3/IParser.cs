1c6b87c6fdc05ceef770a08b5abc55956696e1c3
Garrett DeBruin <gdebruin@microsoft.com>
2025-02-07 10:44:00 -0800

    asdf

--- odata/CombinatorParsingV3/IParser.cs
+++ odata/CombinatorParsingV3/IParser.cs
@@ -9,7 +9,7 @@ namespace CombinatorParsingV3
     //// 
     //// you did try updating `parse` to have `tparsed` as an `out` parameter hoping to reduce the amount of copying needed when creating `output` instances, but it didn't seem to have much impact; it may be worth revisiting this in the future when the call stack is deeper and more instances need to be created; you might also consider having *all* of the values as `out` parameters so that you can always pass something through
 
-    //// TODO try readonly ref struct for stringinput and output
+    //// TODO instead of having a `remainder` property on `output`, can you have output tell the caller how much they need to advance? they constructor on `output` can't take `stringinput` as a reference, so this might be a way around that
     //// TODO add success checks back
     //// TODO add ors back
 
