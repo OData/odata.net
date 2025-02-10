fd99bc520a185c42ed3d5624530236a4a7421a4b
Garrett DeBruin <gdebruin@microsoft.com>
2025-02-10 15:07:59 -0800

    added _eqExprParser

--- odata/__GeneratedOdataV4/Parsers/Inners/260.cs
+++ odata/__GeneratedOdataV4/Parsers/Inners/260.cs
@@ -26,6 +26,23 @@ namespace __GeneratedOdataV4.Parsers.Inners
         
         public static class _eqExprParser
         {
+            public static CombinatorParsingV3.IParser<char, CombinatorParsingV3.ParserExtensions.StringAdapter, __GeneratedOdataV4.CstNodes.Inners._eqExprⳆneExprⳆltExprⳆleExprⳆgtExprⳆgeExprⳆhasExprⳆinExpr._eqExpr> Instance2 { get; } = new Parser2();
+
+            private sealed class Parser2 : CombinatorParsingV3.IParser<char, CombinatorParsingV3.ParserExtensions.StringAdapter, __GeneratedOdataV4.CstNodes.Inners._eqExprⳆneExprⳆltExprⳆleExprⳆgtExprⳆgeExprⳆhasExprⳆinExpr._eqExpr>
+            {
+                [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+                public __GeneratedOdataV4.CstNodes.Inners._eqExprⳆneExprⳆltExprⳆleExprⳆgtExprⳆgeExprⳆhasExprⳆinExpr._eqExpr Parse(CombinatorParsingV3.ParserExtensions.StringAdapter input, int start, out int newStart)
+                {
+                    newStart = start;
+                    for (; newStart < start + 17; ++newStart)
+                    {
+                        var next = input[newStart];
+                    }
+
+                    return default;
+                }
+            }
+
             public static IParser<char, __GeneratedOdataV4.CstNodes.Inners._eqExprⳆneExprⳆltExprⳆleExprⳆgtExprⳆgeExprⳆhasExprⳆinExpr._eqExpr> Instance { get; } = new Parser();
             
             private sealed class Parser : IParser<char, __GeneratedOdataV4.CstNodes.Inners._eqExprⳆneExprⳆltExprⳆleExprⳆgtExprⳆgeExprⳆhasExprⳆinExpr._eqExpr>
