1c46e61fc2c21edc7a2e8d9e58cee7e4bbd1084a
Garrett DeBruin <gdebruin@microsoft.com>
2025-02-10 12:51:41 -0800

    added _ʺx2Fʺ_keyPathLiteralParser, still sub-500

--- odata/__GeneratedOdataV4/Parsers/Inners/43.cs
+++ odata/__GeneratedOdataV4/Parsers/Inners/43.cs
@@ -4,6 +4,23 @@ namespace __GeneratedOdataV4.Parsers.Inners
     
     public static class _ʺx2Fʺ_keyPathLiteralParser
     {
+        public static CombinatorParsingV3.IParser<char, CombinatorParsingV3.ParserExtensions.StringAdapter, __GeneratedOdataV4.CstNodes.Inners._ʺx2Fʺ_keyPathLiteral> Instance2 { get; } = new Parser2();
+
+        private sealed class Parser2 : CombinatorParsingV3.IParser<char, CombinatorParsingV3.ParserExtensions.StringAdapter, __GeneratedOdataV4.CstNodes.Inners._ʺx2Fʺ_keyPathLiteral>
+        {
+            [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+            public __GeneratedOdataV4.CstNodes.Inners._ʺx2Fʺ_keyPathLiteral Parse(CombinatorParsingV3.ParserExtensions.StringAdapter input, int start, out int newStart)
+            {
+                newStart = start;
+                for (; newStart < start + 5; ++newStart)
+                {
+                    var next = input[newStart];
+                }
+
+                return default;
+            }
+        }
+
         public static IParser<char, __GeneratedOdataV4.CstNodes.Inners._ʺx2Fʺ_keyPathLiteral> Instance { get; } = new Parser();
         
         private sealed class Parser : IParser<char, __GeneratedOdataV4.CstNodes.Inners._ʺx2Fʺ_keyPathLiteral>
--- odata/__GeneratedOdataV4/Parsers/Inners/44.cs
+++ odata/__GeneratedOdataV4/Parsers/Inners/44.cs
@@ -11,11 +11,7 @@ namespace __GeneratedOdataV4.Parsers.Inners
             [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
             public __GeneratedOdataV4.CstNodes.Inners._Ⲥʺx2Fʺ_keyPathLiteralↃ Parse(CombinatorParsingV3.ParserExtensions.StringAdapter input, int start, out int newStart)
             {
-                newStart = start;
-                for (; newStart < start + 5; ++newStart)
-                {
-                    var next = input[newStart];
-                }
+                var _ʺx2Fʺ_keyPathLiteral_1 = __GeneratedOdataV4.Parsers.Inners._ʺx2Fʺ_keyPathLiteralParser.Instance2.Parse(input, start, out newStart);
 
                 return default;
             }
