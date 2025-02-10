bbc4ba235675996900f3f30075b3ca4563f5fbfc
Garrett DeBruin <gdebruin@microsoft.com>
2025-02-10 13:09:10 -0800

    added _ʺx24x66x69x6Cx74x65x72ʺParser, this migth have put you over 500?

--- odata/__GeneratedOdataV4/Parsers/Inners/136.cs
+++ odata/__GeneratedOdataV4/Parsers/Inners/136.cs
@@ -11,11 +11,7 @@ namespace __GeneratedOdataV4.Parsers.Inners
             [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
             public __GeneratedOdataV4.CstNodes.Inners._ʺx24x66x69x6Cx74x65x72ʺⳆʺx66x69x6Cx74x65x72ʺ Parse(CombinatorParsingV3.ParserExtensions.StringAdapter input, int start, out int newStart)
             {
-                newStart = start;
-                for (; newStart < start + 7; ++newStart)
-                {
-                    var next = input[newStart];
-                }
+                _ʺx24x66x69x6Cx74x65x72ʺParser.Instance2.Parse(input, start, out newStart);
 
                 return default;
             }
@@ -25,6 +21,23 @@ namespace __GeneratedOdataV4.Parsers.Inners
         
         public static class _ʺx24x66x69x6Cx74x65x72ʺParser
         {
+            public static CombinatorParsingV3.IParser<char, CombinatorParsingV3.ParserExtensions.StringAdapter, __GeneratedOdataV4.CstNodes.Inners._ʺx24x66x69x6Cx74x65x72ʺⳆʺx66x69x6Cx74x65x72ʺ._ʺx24x66x69x6Cx74x65x72ʺ> Instance2 { get; } = new Parser2();
+
+            private sealed class Parser2 : CombinatorParsingV3.IParser<char, CombinatorParsingV3.ParserExtensions.StringAdapter, __GeneratedOdataV4.CstNodes.Inners._ʺx24x66x69x6Cx74x65x72ʺⳆʺx66x69x6Cx74x65x72ʺ._ʺx24x66x69x6Cx74x65x72ʺ>
+            {
+                [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+                public __GeneratedOdataV4.CstNodes.Inners._ʺx24x66x69x6Cx74x65x72ʺⳆʺx66x69x6Cx74x65x72ʺ._ʺx24x66x69x6Cx74x65x72ʺ Parse(CombinatorParsingV3.ParserExtensions.StringAdapter input, int start, out int newStart)
+                {
+                    newStart = start;
+                    for (; newStart < start + 7; ++newStart)
+                    {
+                        var next = input[newStart];
+                    }
+
+                    return default;
+                }
+            }
+
             public static IParser<char, __GeneratedOdataV4.CstNodes.Inners._ʺx24x66x69x6Cx74x65x72ʺⳆʺx66x69x6Cx74x65x72ʺ._ʺx24x66x69x6Cx74x65x72ʺ> Instance { get; } = new Parser();
             
             private sealed class Parser : IParser<char, __GeneratedOdataV4.CstNodes.Inners._ʺx24x66x69x6Cx74x65x72ʺⳆʺx66x69x6Cx74x65x72ʺ._ʺx24x66x69x6Cx74x65x72ʺ>
