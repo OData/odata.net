c6c82bbad4490894437de46f83a0065af6a88c2b
Elizabeth Okerio <elokerio@microsoft.com>
2025-02-14 11:42:14 +0300

    Port WriteJsonPayloadTests

--- src/Microsoft.OData.Core/ShippingAssemblyAttributes.cs
+++ src/Microsoft.OData.Core/ShippingAssemblyAttributes.cs
@@ -20,4 +20,5 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Test.Taupo.OData" + AssemblyRef.TestPublicKey)]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.Tests" + AssemblyRef.TestPublicKey)]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Client.Tests" + AssemblyRef.TestPublicKey)]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.OData.Core.E2E.Tests" + AssemblyRef.TestPublicKey)]
 #pragma warning restore 436
