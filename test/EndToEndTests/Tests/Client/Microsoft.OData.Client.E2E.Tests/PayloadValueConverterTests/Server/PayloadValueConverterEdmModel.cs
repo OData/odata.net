91cd90f8f87977ff85930aa8f01f81b953fd029e
Elizabeth Okerio <elokerio@microsoft.com>
2025-02-17 18:37:38 +0300

    Port PayloadValueConverter tests

---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/PayloadValueConverterTests/Server/PayloadValueConverterEdmModel.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="PayloadValueConverterEdmModel.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace Microsoft.OData.Client.E2E.Tests.PayloadValueConverterTests.Server
+{
+    public class PayloadValueConverterEdmModel
+    {
+        public static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Person>("People");
+            builder.EntitySet<Product>("Products");
+            var action = builder.EntityType<Product>()
+                .Action("AddProduct");
+            action.CollectionParameter<Product>("Set");
+            action.Parameter<Product>("Value");
+            action.Parameter<bool>("Override");
+
+            builder.Action("ResetDataSource");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
