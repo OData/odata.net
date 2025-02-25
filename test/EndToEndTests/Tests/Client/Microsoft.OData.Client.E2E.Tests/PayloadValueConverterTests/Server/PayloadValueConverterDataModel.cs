f598ee67583cdab23194dcff68ccb2fd82ff18ec
Elizabeth Okerio <elizaokerio@gmail.com>
2025-02-25 11:35:50 +0300

    Port PayloadValueConverter tests (#3180)

---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/PayloadValueConverterTests/Server/PayloadValueConverterDataModel.cs
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="PayloadValueConverterDataModel.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.OData.Client.E2E.Tests.PayloadValueConverterTests.Server
+{
+    public class ContactInfo : OpenClrObject
+    {
+        public string? N { get; set; }
+    }
+
+    /// <summary>
+    /// The class represents the Person model type.
+    /// </summary>
+    public class Person : ClrObject
+    {
+        public int Id { get; set; }
+
+        public byte[]? Picture { get; set; }
+
+        public List<int>? Numbers { get; set; }
+
+        public ContactInfo? BusinessCard { get; set; }
+    }
+
+    public class Product : ClrObject
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public ProductInfo? Info { get; set; }
+    }
+
+    public class ProductInfo : ClrObject
+    {
+        public string? Site { get; set; }
+
+        public long Serial { get; set; }
+    }
+
+    [Serializable]
+    public abstract class ClrObject
+    {
+        protected ClrObject()
+        {
+            this.UpdatedTime = DateTime.Now;
+        }
+
+        public DateTime UpdatedTime { get; set; }
+    }
+
+    [Serializable]
+    public abstract class OpenClrObject : ClrObject
+    {
+        //Open properties
+        private readonly Dictionary<string, object> openProperties = new Dictionary<string, object>();
+
+        public Dictionary<string, object> OpenProperties
+        {
+            get
+            {
+                return openProperties;
+            }
+        }
+    }
+}
---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/PayloadValueConverterTests/Server/PayloadValueConverterDataSource.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="PayloadValueConverterDataSource.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.OData.Client.E2E.Tests.PayloadValueConverterTests.Server
+{
+    public class PayloadValueConverterDataSource
+    {
+        public static PayloadValueConverterDataSource CreateInstance()
+        {
+            return new PayloadValueConverterDataSource();
+        }
+
+        public PayloadValueConverterDataSource()
+        {
+            ResetDataSource();
+            Initialize();
+        }
+
+        public IList<Person>? People { get; private set; }
+        public IList<Product>? Products { get; private set; }
+
+        /// <summary>
+        /// Populate the data source.
+        /// </summary>
+        public void Initialize()
+        {
+            this.People = new List<Person>()
+            {
+                new Person()
+                {
+                    Id = 31,
+                    Picture = new byte[] { 5, 8 },
+                    Numbers = new List<int> { 3, 5, 7 },
+                    BusinessCard = new ContactInfo()
+                    {
+                        N = "Name1"
+                    }
+                },
+                new Person()
+                {
+                    Id = 32,
+                    Numbers = new List<int>{},
+                    BusinessCard = new ContactInfo()
+                    {
+                        N = "Name2"
+                    }
+                },
+            };
+
+            this.Products = new[]
+            {
+                new Product()
+                {
+                    Id = 1,
+                    Name = "Pear"
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Name = "Banana",
+                    Info = new ProductInfo()
+                    {
+                        Site = "G2",
+                        Serial = 0x3FF
+                    }
+                },
+                new Product()
+                {
+                    Id = 3,
+                    Name = "Mangosteen",
+                    Info = new ProductInfo()
+                    {
+                        Site = "G3",
+                        Serial = 0x3FE,
+                    }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Resets the data source
+        /// </summary>
+        public void ResetDataSource()
+        {
+            People?.Clear();
+            Products?.Clear();
+        }
+    }
+}
