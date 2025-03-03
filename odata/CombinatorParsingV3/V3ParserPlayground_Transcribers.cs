ee568aff0a59304fe6237106688f8b468c8c06ab
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-03 11:30:14 -0800

    started writing transcribers

---
+++ odata/CombinatorParsingV3/V3ParserPlayground_Transcribers.cs
@@ -0,0 +1,34 @@
+﻿namespace CombinatorParsingV3
+{
+    using System.Text;
+
+    using GeneratorV3;
+
+    public static partial class V3ParserPlayground
+    {
+        public sealed class OdataUriTranscriber : ITranscriber<OdataUri<ParseMode.Realized>>
+        {
+            public void Transcribe(OdataUri<ParseMode.Realized> value, StringBuilder builder)
+            {
+                
+            }
+        }
+
+        public sealed class AtLeastOneTranscriber<TDeferredAstNode, TRealizedAstNode> : ITranscriber<AtLeastOne<TDeferredAstNode, TRealizedAstNode, ParseMode.Realized>>
+            where TDeferredAstNode : IDeferredAstNode<char, TRealizedAstNode>
+            where TRealizedAstNode : IFromRealizedable<TDeferredAstNode>
+        {
+            private readonly ITranscriber<TRealizedAstNode> realizedAstNodeTranscriber;
+
+            public AtLeastOneTranscriber(ITranscriber<TRealizedAstNode> realizedAstNodeTranscriber)
+            {
+                this.realizedAstNodeTranscriber = realizedAstNodeTranscriber;
+            }
+
+            public void Transcribe(AtLeastOne<TDeferredAstNode, TRealizedAstNode, ParseMode.Realized> value, StringBuilder builder)
+            {
+                
+            }
+        }
+    }
+}
