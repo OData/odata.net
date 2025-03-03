2d04ea155a38115f7ea5143b99ef40dd1dc624a3
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-03 14:32:48 -0800

    adding queryable extensions

---
+++ src/Microsoft.OData.Core/QueryableExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.OData.Core
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    /*public static class QueryableExtensions
+    {
+        public static double Average(this IQueryable<short> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            return source.Provider.Execute<double>(
+                Expression.Call(
+                    null,
+                    new Func<IQueryable<short>, double>(QueryableExtensions.Average).Method,
+                    source.Expression));
+        }
+
+        public static double? Average(this IQueryable<short?> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            return source.Provider.Execute<double?>(
+                Expression.Call(
+                    null,
+                    new Func<IQueryable<short?>, double?>(Average).Method,
+                    source.Expression));
+        }
+    }*/
+}
