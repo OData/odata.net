8221ac9c133c6dcc6c06e8a1302ae36b49bfdca2
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-03 14:38:00 -0800

    asdf

---
+++ src/Microsoft.OData.Core/EnumerableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Microsoft.OData.Core
+{
+    public static class EnumerableExtensions
+    {
+        public static double Average(this IEnumerable<short> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                {
+                    throw new Exception("TODO");
+                }
+
+                long sum = enumerator.Current;
+                long count = 1;
+                while (enumerator.MoveNext())
+                {
+                    checked
+                    {
+                        sum += enumerator.Current;
+                    }
+
+                    ++count;
+                }
+
+                return (double)sum / count;
+            }
+        }
+    }
+}
