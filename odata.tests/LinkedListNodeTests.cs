3b2c26c2312964781442956c086a8567bb104691
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-11 12:45:15 -0700

    asdf

--- odata.tests/LinkedListNodeTests.cs
+++ odata.tests/LinkedListNodeTests.cs
@@ -32,15 +32,6 @@
             }
         }
 
-        private LinkedListNode<Wrapper<int>> CreateListAndEnumerateWithRefStruct2()
-        {
-            var list = new LinkedListNode<Wrapper<int>>(BetterSpan.FromInstance(new Wrapper<int>(-1)));
-            Span<byte> memory = stackalloc byte[Unsafe.SizeOf<LinkedListNode<Wrapper<int>>>()];
-            var wrapper = new Wrapper<int>(0);
-            list = list.Append(BetterSpan.FromInstance(wrapper), memory);
-            return list;
-        }
-
         private void Test()
         {
             Span<byte> span = stackalloc byte[4];
@@ -157,7 +148,7 @@
             return list;
         }
 
-        private LinkedListNode<int> Test10()
+        private LinkedListNode<int> Test13()
         {
             var list = new LinkedListNode<int>(BetterSpan.FromInstance(42));
             for (int i = 0; i < 10; ++i)
@@ -167,6 +158,14 @@
             }
 
             //// THIS IS A GOOD THING
+            return list;
+        }
+
+        private LinkedListNode<int> Test14()
+        {
+            var list = new LinkedListNode<int>(BetterSpan.FromInstance(42));
+
+            //// TODO this should be allowed
             return list;
         }
     }
