1a70b564cd55d938b361f7a20ceacdb068e9a86a
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-19 13:41:22 -0700

    asdf

---
+++ odata.tests/V2/Fx/BetterReadOnlySpanMemoryIntegrityTests.cs
@@ -0,0 +1,17 @@
+﻿namespace V2.Fx
+{
+    using System.Runtime.CompilerServices;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;    
+
+    [TestClass]
+    public sealed class BetterReadOnlySpanMemoryIntegrityTests
+    {
+        [TestMethod]
+        public void StackAllocWithinFrame()
+        {
+            Span<byte> span = stackalloc byte[Unsafe.SizeOf<string>()];
+            var betterSpan = BetterReadOnlySpan.FromMemory<string>(span, 1);
+        }
+    }
+}
---
+++ odata.tests/V2/Fx/BetterReadOnlySpanMemoryIntegrityTestsResources/StackAllocWithinFrame.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+using V2.Fx;
+
+public static class StackAllocWithinFrame
+{
+    public static void Method()
+    {
+        Span<byte> span = stackalloc byte[Unsafe.SizeOf<string>()];
+        var betterSpan = BetterReadOnlySpan.FromMemory<string>(span, 1);
+    }
+}
