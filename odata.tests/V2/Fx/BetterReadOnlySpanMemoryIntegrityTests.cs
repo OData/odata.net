fb7e715e26a4792d9a49aa021eb51d7e8a167966
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-19 13:42:49 -0700

    asdf

--- odata.tests/V2/Fx/BetterReadOnlySpanMemoryIntegrityTests.cs
+++ odata.tests/V2/Fx/BetterReadOnlySpanMemoryIntegrityTests.cs
@@ -13,5 +13,22 @@
             Span<byte> span = stackalloc byte[Unsafe.SizeOf<string>()];
             var betterSpan = BetterReadOnlySpan.FromMemory<string>(span, 1);
         }
+
+        private string GetResource(string path)
+        {
+            var assembly = this.GetType().Assembly;
+            using (var resourceStream = assembly.GetManifestResourceStream(path)) //// TODO you've previously done something in onboarding for embedded resources, check out that code
+            {
+                if (resourceStream == null)
+                {
+                    throw new Exception("TODO");
+                }
+
+                using (var textReader = new StreamReader(resourceStream))
+                {
+                    return textReader.ReadToEnd();
+                }
+            }
+        }
     }
 }
