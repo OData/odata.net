85d0b1d24d3b7cd30b71710b033ec645116e9dbf
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-21 13:07:10 -0700

    asdf

--- odata/V2/Fx/BetterReadOnlySpan{T}.cs
+++ odata/V2/Fx/BetterReadOnlySpan{T}.cs
@@ -29,7 +29,6 @@
             this.length = length;
         }
 
-        //// TODO update all of your stackallocs to use differentmemory 
         //// TODO since these factory methods are public, you need to add the same tests as you added to betterreadonlyspan
 
         public static BetterReadOnlySpan<byte> Create(DifferentMemory memory)
