79ec807ce2223f2e28d8e5ff2d05ae23814e88bb
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-24 07:22:23 -0700

    asdf

--- odata/V2/Fx/BetterReadOnlySpan{T}.cs
+++ odata/V2/Fx/BetterReadOnlySpan{T}.cs
@@ -8,6 +8,9 @@
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <remarks>
+    /// The purpose of this type is to be equivalent to <see cref="System.Span{T}"/> but allows for <typeparamref name="T"/> to be
+    /// a <see langword="ref"/> <see langword="struct"/>
+    /// 
     /// You looked into if you could have something like `BetterReadOnlySpan<byte> = stackalloc byte[10];`. You *can* by having an implicit operator taking in `Span<byte>`. However, the return type of that operator must be `BetterReadOnlySpan<T>`, so you will need to assert something about the length of the input `span`, and having assertions in implicit operators is a bad idea. Note that `differentmemory` can be used instead for this purpose.
     /// </remarks>
     public readonly ref struct BetterReadOnlySpan<T> where T : allows ref struct //// TODO is there other span stuff that you should add in here?
@@ -16,12 +19,22 @@
 
         private readonly int length;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="memory"></param>
+        /// <param name="length"></param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the length of <paramref name="memory"/> is not the same as the product of <paramref name="length"/> and the
+        /// <see langword="sizeof"/> <typeparamref name="T"/>
+        /// </exception>
         private BetterReadOnlySpan(ByteSpan memory, int length)
         {
             var elementSize = Unsafe.SizeOf<T>();
             if (memory.Length != elementSize * length)
             {
-                throw new ArgumentOutOfRangeException($"The number of bytes in '{nameof(memory)}' must exactly fit the number of elements in the '{nameof(BetterReadOnlySpan)}'. The number of bytes provide was '{memory.Length}'. The number of requested elements was '{length}'. The size of each element was '{elementSize}'.", (Exception?)null);
+                var message = $"The number of bytes in '{nameof(memory)}' must exactly fit the number of elements in the '{nameof(BetterReadOnlySpan)}'. The number of bytes provide was '{memory.Length}'. The number of requested elements was '{length}'. The type of each element was '{typeof(T).FullName}'; its size was '{elementSize}'.";
+                throw new ArgumentOutOfRangeException(message, (Exception?)null);
             }
 
             this.data = memory;
