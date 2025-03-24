3d064e2f47004a67d36c911cf41f5c2bf54d73d3
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-24 07:30:51 -0700

    asdf

--- odata.tests/V2/Fx/BetterReadOnlySpan{T}MemoryIntegrityTests.cs
+++ odata.tests/V2/Fx/BetterReadOnlySpan{T}MemoryIntegrityTests.cs
@@ -11,7 +11,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;    
 
     [TestClass]
-    public sealed class BetterReadOnlyFactorySpanMemoryIntegrityTests
+    public sealed class BetterReadOnlySpanMemoryIntegrityTests
     {
         [TestMethod]
         public void StackAllocWithinFrame()
