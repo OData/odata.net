dd8f41e503b664e22037b1ab919501bc477851fa
Garrett DeBruin <gdebruin@microsoft.com>
2025-03-24 07:49:34 -0700

    asdf

--- odata/V2/Fx/SpanEx.cs
+++ odata/V2/Fx/SpanEx.cs
@@ -1,7 +1,9 @@
-﻿using V2.Fx.Runtime.InteropServices;
-
-namespace V2.Fx
+﻿namespace V2.Fx
 {
+    using System;
+
+    using V2.Fx.Runtime.InteropServices;
+
     public static class SpanEx
     {
         public static SpanEx<byte> FromMemory(ByteSpan memory)
@@ -9,9 +11,19 @@ namespace V2.Fx
             return SpanEx<byte>.Create(memory);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="memory"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the length of <paramref name="memory"/> is not the same as the product of <paramref name="length"/> and the
+        /// <see langword="sizeof"/> <typeparamref name="T"/>
+        /// </exception>
         public static SpanEx<T> FromMemory<T>(ByteSpan memory, int length) where T : allows ref struct
         {
-            //// TODO can this be an extension called `cast` or something instead?
             return SpanEx<T>.Create(memory, length);
         }
 
