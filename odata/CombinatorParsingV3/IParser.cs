56a5ecd85cfa7a4100555b66beb7383268cf263b
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-07 13:03:57 -0700

    asdf

--- odata/CombinatorParsingV3/IParser.cs
+++ odata/CombinatorParsingV3/IParser.cs
@@ -63,4 +63,54 @@
             return new CharacterTokenStream(this.input, newIndex);
         }
     }
+    public interface IDeferredAstNode<out TToken, out TRealizedAstNode>
+    {
+        IRealizationResult<TToken, TRealizedAstNode> Realize();
+    }
+
+    public interface IFromRealizedable<out TDeferredAstNode>
+    {
+        TDeferredAstNode Convert();
+    }
+
+    public interface IRealizationResult<out TToken>
+    {
+        bool Success { get; }
+
+        ITokenStream<TToken>? RemainingTokens { get; }
+    }
+
+    public sealed class RealizationResult<TToken> : IRealizationResult<TToken>
+    {
+        public RealizationResult(bool success, ITokenStream<TToken>? remainder)
+        {
+            Success = success;
+            RemainingTokens = remainder;
+        }
+
+        public bool Success { get; }
+
+        public ITokenStream<TToken>? RemainingTokens { get; }
+    }
+
+    public abstract class ParseMode
+    {
+        private ParseMode()
+        {
+        }
+
+        public sealed class Deferred : ParseMode
+        {
+            private Deferred()
+            {
+            }
+        }
+
+        public sealed class Realized : ParseMode
+        {
+            private Realized()
+            {
+            }
+        }
+    }
 }
