ca2951e6fde1464b4931e1780bcf1cc87cb64cf5
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-07 13:20:21 -0700

    asdf

---
+++ odata/CombinatorParsingV3/IFuture.cs
@@ -0,0 +1,53 @@
+﻿namespace CombinatorParsingV3
+{
+    using System;
+
+    public interface IFuture<out T> //// TODO i'm not sure i like having an interface...
+    {
+        T Value { get; }
+    }
+    public sealed class Future<T> : IFuture<T>
+    {
+        private readonly Func<T> promise;
+
+        private Optional<T> value;
+
+        public Future(Func<T> promise)
+        {
+            this.promise = promise;
+
+            this.value = default;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (!this.value.TryGetValue(out var value))
+                {
+                    value = this.promise();
+
+                    this.value = value;
+                }
+
+                return value;
+            }
+        }
+    }
+
+    public static class Future
+    {
+        public static Future<T> Create<T>(Func<T> promise)
+        {
+            return new Future<T>(promise);
+        }
+    }
+
+    public static class FutureExtensions
+    {
+        public static IFuture<TResult> Select<TValue, TResult>(this IFuture<TValue> future, Func<TValue, TResult> selector)
+        {
+            return new Future<TResult>(() => selector(future.Value));
+        }
+    }
+}
---
+++ odata/CombinatorParsingV3/Optional.cs
@@ -0,0 +1,30 @@
+﻿namespace CombinatorParsingV3
+{
+    /// <summary>
+    /// NOTE: you considered having a class variant of this for cases where the caller needs to avoid boxing, but based on nullabletests.test4 there is basically no different in perforamnce
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public readonly struct Optional<T>
+    {
+        private readonly T value;
+
+        private readonly bool hasValue;
+
+        public Optional(T value)
+        {
+            this.value = value;
+            this.hasValue = true;
+        }
+
+        public bool TryGetValue([System.Diagnostics.CodeAnalysis.MaybeNullWhen(false)] out T value)
+        {
+            value = this.value;
+            return this.hasValue;
+        }
+
+        public static implicit operator Optional<T>(T value)
+        {
+            return new Optional<T>(value);
+        }
+    }
+}
