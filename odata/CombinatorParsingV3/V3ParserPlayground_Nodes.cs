6da16ad4571bc209772bd8d128acf2d9ab3ac0c2
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-14 11:11:41 -0700

    asdf

--- odata/CombinatorParsingV3/V3ParserPlayground_Nodes.cs
+++ odata/CombinatorParsingV3/V3ParserPlayground_Nodes.cs
@@ -195,11 +195,6 @@
                 {
                     internal static IRealizationResult<char, AlphaNumeric<ParseMode.Realized>.Realized.A> Create(IFuture<IRealizationResult<char>> previousNodeRealizationResult)
                     {
-
-                        /*if (!_CRLF_1.Success)
-                        {
-                            return new RealizationResult<char, _WSPⳆCRLF_WSP<TMode>.Realized._CRLF_WSP>(false, default, _CRLF_1.RemainingTokens);
-                        }*/
                         var output = previousNodeRealizationResult.Value;
                         if (!output.Success)
                         {
