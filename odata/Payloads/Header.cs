58b67783fb8ff172f5ac35c856622c9d5d9a52fb
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-24 13:44:13 -0700

    asdf

--- odata/Payloads/Header.cs
+++ odata/Payloads/Header.cs
@@ -1,5 +1,6 @@
 ﻿namespace Payloads
 {
+    using System;
     using System.Collections.Generic;
 
     /// <summary>
@@ -69,22 +70,174 @@
         }
     }
 
+    /// <summary>
+    /// https://www.rfc-editor.org/rfc/rfc2616#section-14.1
+    /// </summary>
     public sealed class MediaRangeAcceptParamsPair
     {
-        public MediaRangeAcceptParamsPair(MediaRange mediaRange, AcceptParams acceptParams)
+        public MediaRangeAcceptParamsPair(MediaRange mediaRange, AcceptParams? acceptParams)
         {
             MediaRange = mediaRange;
             AcceptParams = acceptParams;
         }
 
         public MediaRange MediaRange { get; }
-        public AcceptParams AcceptParams { get; }
+        public AcceptParams? AcceptParams { get; }
     }
 
-    public sealed class MediaRange
+    /// <summary>
+    /// https://www.rfc-editor.org/rfc/rfc2616#section-2.2
+    /// </summary>
+    public sealed class TokenChar
+    {
+        public TokenChar(char character)
+        {
+            switch (character)
+            {
+                case (char)0x00:
+                case (char)0x01:
+                case (char)0x02:
+                case (char)0x03:
+                case (char)0x04:
+                case (char)0x05:
+                case (char)0x06:
+                case (char)0x07:
+                case (char)0x08:
+                case (char)0x09:
+                case (char)0x0A:
+                case (char)0x0B:
+                case (char)0x0C:
+                case (char)0x0D:
+                case (char)0x0E:
+                case (char)0x0F:
+                case (char)0x10:
+                case (char)0x11:
+                case (char)0x12:
+                case (char)0x13:
+                case (char)0x14:
+                case (char)0x15:
+                case (char)0x16:
+                case (char)0x17:
+                case (char)0x18:
+                case (char)0x19:
+                case (char)0x1A:
+                case (char)0x1B:
+                case (char)0x1C:
+                case (char)0x1D:
+                case (char)0x1E:
+                case (char)0x1F:
+                case (char)0x20:
+                case (char)0x21:
+                case (char)0x22:
+                case (char)0x23:
+                case (char)0x24:
+                case (char)0x25:
+                case (char)0x26:
+                case (char)0x27:
+                case (char)0x28:
+                case (char)0x29:
+                case (char)0x2A:
+                case (char)0x2B:
+                case (char)0x2C:
+                case (char)0x2D:
+                case (char)0x2E:
+                case (char)0x2F:
+                case (char)0x30:
+                case (char)0x31:
+                case (char)127:
+                    throw new ArgumentException("TODO");
+            }
+
+            Character = character;
+            //// TODO if you did this "right", you would have a discriminated union with each supported character
+        }
+
+        public char Character { get; }
+    }
+
+    /// <summary>
+    /// https://www.rfc-editor.org/rfc/rfc2616#section-2.2
+    /// </summary>
+    public sealed class Token
+    {
+        public Token(TokenChar firstCharacter, IEnumerable<TokenChar> subsequentCharacters)
+        {
+            FirstCharacter = firstCharacter;
+            SubsequentCharacters = subsequentCharacters;
+        }
+
+        public TokenChar FirstCharacter { get; }
+        public IEnumerable<TokenChar> SubsequentCharacters { get; }
+    }
+
+    /// <summary>
+    /// https://www.rfc-editor.org/rfc/rfc2616#section-3.7
+    /// </summary>
+    public sealed class Type
+    {
+        public Type(Token token)
+        {
+            Token = token;
+        }
+
+        public Token Token { get; }
+    }
+
+    /// <summary>
+    /// https://www.rfc-editor.org/rfc/rfc2616#section-3.7
+    /// </summary>
+    public sealed class Subtype
+    {
+        public Subtype(Token token)
+        {
+            Token = token;
+        }
+
+        public Token Token { get; }
+    }
+
+    public sealed class Parameter
+    {
+        private Parameter()
+        {
+        }
+    }
+
+    /// <summary>
+    /// https://www.rfc-editor.org/rfc/rfc2616#section-14.1
+    /// </summary>
+    public abstract class MediaRange
     {
         private MediaRange()
         {
+        }
+
+        public sealed class All : MediaRange
+        {
+            private All()
+            {
+            }
+        }
+
+        public sealed class TypeOnly : MediaRange
+        {
+            private TypeOnly()
+            {
+            }
+        }
+
+        public sealed class TypeAndSubType : MediaRange
+        {
+            public TypeAndSubType(Type type, Subtype subtype, IEnumerable<Parameter> parameters)
+            {
+                Type = type;
+                Subtype = subtype;
+                Parameters = parameters;
+            }
+
+            public Type Type { get; }
+            public Subtype Subtype { get; }
+            public IEnumerable<Parameter> Parameters { get; }
         }
     }
 
--- odata/Payloads/OdataRequestPayload.cs
+++ odata/Payloads/OdataRequestPayload.cs
@@ -1,5 +1,9 @@
 ﻿namespace Payloads
 {
+    using System.Collections.Generic;
+
+    //// TODO i'm still not clear if i'm defining an AST or the CLR definitions of the conventions
+
     public abstract class OdataRequestPayload
     {
         private OdataRequestPayload()
@@ -8,18 +12,74 @@
 
         public sealed class Json : OdataRequestPayload
         {
-            private Json()
+            private Json(Payloads.Json.FormatRequestHeader? accept)
             {
+                Accept = accept;
             }
 
-
+            /// <summary>
+            /// https://docs.oasis-open.org/odata/odata-json-format/v4.01/odata-json-format-v4.01.html#sec_RequestingtheJSONFormat
+            /// </summary>
+            public Payloads.Json.FormatRequestHeader? Accept { get; } //// TODO i *think* this is optional, but i can't tell; i guess it's not optional if the service supports other formats and the client only understands JSON, so they want to ensure they only get a JSON response?
         }
     }
 
-    public sealed class JsonRequestHeaders
+    namespace Json
     {
-        private JsonRequestHeaders()
+        /// <summary>
+        /// https://docs.oasis-open.org/odata/odata-json-format/v4.01/odata-json-format-v4.01.html#sec_RequestingtheJSONFormat
+        /// 
+        /// based off of https://www.rfc-editor.org/rfc/rfc2616#section-14.1
+        /// </summary>
+        public sealed class FormatRequestHeader
         {
+            public FormatRequestHeader(Type type, Subtype subtype, IEnumerable<FormatParameter> parameters)
+            {
+                Type = type;
+                Subtype = subtype;
+                Parameters = parameters;
+            }
+
+            public Type Type { get; }
+            public Subtype Subtype { get; }
+            public IEnumerable<FormatParameter> Parameters { get; }
+            
+            //// TODO you could have an extension that returns an http accept header from this
+        }
+
+        public abstract class FormatParameter
+        {
+            private FormatParameter()
+            {
+            }
+
+            public sealed class ExponentialDecimals : FormatParameter
+            {
+                private ExponentialDecimals()
+                {
+                }
+            }
+
+            public sealed class IEEE754Compatible : FormatParameter
+            {
+                private IEEE754Compatible()
+                {
+                }
+            }
+
+            public sealed class Metadata : FormatParameter
+            {
+                private Metadata()
+                {
+                }
+            }
+
+            public sealed class Streaming : FormatParameter
+            {
+                private Streaming()
+                {
+                }
+            }
         }
     }
 }
