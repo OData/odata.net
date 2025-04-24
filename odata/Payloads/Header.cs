2c8dddad139e85265cd748435cd6d3c6616b0dad
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-24 12:43:15 -0700

    starting to add request/response payload types

---
+++ odata/Payloads/Header.cs
@@ -0,0 +1,97 @@
+﻿namespace Payloads
+{
+    /// <summary>
+    /// RFC 2616: https://www.rfc-editor.org/rfc/rfc2616#page-31
+    /// </summary>
+    public abstract class Header
+    {
+        private Header()
+        {
+        }
+
+        public sealed class General
+        {
+            public General(GeneralHeader header)
+            {
+                Header = header;
+            }
+
+            public GeneralHeader Header { get; }
+        }
+
+        public sealed class Request
+        {
+            public Request(RequestHeader header)
+            {
+                Header = header;
+            }
+
+            public RequestHeader Header { get; }
+        }
+
+        public sealed class Response
+        {
+            public Response(ResponseHeader header)
+            {
+                Header = header;
+            }
+
+            public ResponseHeader Header { get; }
+        }
+
+        public sealed class Entity
+        {
+            public Entity(EntityHeader header)
+            {
+                Header = header;
+            }
+
+            public EntityHeader Header { get; }
+        }
+
+        public sealed class Custom
+        {
+            public Custom(CustomHeader header)
+            {
+                Header = header;
+            }
+
+            public CustomHeader Header { get; }
+        }
+    }
+
+    public sealed class GeneralHeader
+    {
+        private GeneralHeader()
+        {
+        }
+    }
+
+    public sealed class RequestHeader
+    {
+        private RequestHeader()
+        {
+        }
+    }
+
+    public sealed class ResponseHeader
+    {
+        private ResponseHeader()
+        {
+        }
+    }
+
+    public sealed class EntityHeader
+    {
+        private EntityHeader()
+        {
+        }
+    }
+
+    public sealed class CustomHeader
+    {
+        private CustomHeader()
+        {
+        }
+    }
+}
---
+++ odata/Payloads/OdataRequestPayload.cs
@@ -0,0 +1,25 @@
+﻿namespace Payloads
+{
+    public abstract class OdataRequestPayload
+    {
+        private OdataRequestPayload()
+        {
+        }
+
+        public sealed class Json : OdataRequestPayload
+        {
+            private Json()
+            {
+            }
+
+
+        }
+    }
+
+    public sealed class JsonRequestHeaders
+    {
+        private JsonRequestHeaders()
+        {
+        }
+    }
+}
