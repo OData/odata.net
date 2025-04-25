6488b3f26ace13c476b145dd57786820364ae05c
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-25 14:00:44 -0700

    asdf

--- odata/NewStuff/Odata/Headers/Inners/_Ⰳx50ⲻ6E.cs
+++ odata/NewStuff/Odata/Headers/Inners/_Ⰳx50ⲻ6E.cs
@@ -1,9 +1,288 @@
 ﻿namespace NewStuff.Odata.Headers.Inners
 {
-    public sealed class _Ⰳx50ⲻ6E
+    public abstract class _Ⰳx50ⲻ6E
     {
         private _Ⰳx50ⲻ6E()
         {
+        }
+
+        public sealed class _50 : _Ⰳx50ⲻ6E
+        {
+            private _50()
+            {
+            }
+
+            public static _50 Instance { get; } = new _50();
+        }
+
+        public sealed class _51 : _Ⰳx50ⲻ6E
+        {
+            private _51()
+            {
+            }
+
+            public static _51 Instance { get; } = new _51();
+        }
+
+        public sealed class _52 : _Ⰳx50ⲻ6E
+        {
+            private _52()
+            {
+            }
+
+            public static _52 Instance { get; } = new _52();
+        }
+
+        public sealed class _53 : _Ⰳx50ⲻ6E
+        {
+            private _53()
+            {
+            }
+
+            public static _53 Instance { get; } = new _53();
+        }
+
+        public sealed class _54 : _Ⰳx50ⲻ6E
+        {
+            private _54()
+            {
+            }
+
+            public static _54 Instance { get; } = new _54();
+        }
+
+        public sealed class _55 : _Ⰳx50ⲻ6E
+        {
+            private _55()
+            {
+            }
+
+            public static _55 Instance { get; } = new _55();
+        }
+
+        public sealed class _56 : _Ⰳx50ⲻ6E
+        {
+            private _56()
+            {
+            }
+
+            public static _56 Instance { get; } = new _56();
+        }
+
+        public sealed class _57 : _Ⰳx50ⲻ6E
+        {
+            private _57()
+            {
+            }
+
+            public static _57 Instance { get; } = new _57();
+        }
+
+        public sealed class _58 : _Ⰳx50ⲻ6E
+        {
+            private _58()
+            {
+            }
+
+            public static _58 Instance { get; } = new _58();
+        }
+
+        public sealed class _59 : _Ⰳx50ⲻ6E
+        {
+            private _59()
+            {
+            }
+
+            public static _59 Instance { get; } = new _59();
+        }
+
+        public sealed class _5A : _Ⰳx50ⲻ6E
+        {
+            private _5A()
+            {
+            }
+
+            public static _5A Instance { get; } = new _5A();
+        }
+
+        public sealed class _5B : _Ⰳx50ⲻ6E
+        {
+            private _5B()
+            {
+            }
+
+            public static _5B Instance { get; } = new _5B();
+        }
+
+        public sealed class _5C : _Ⰳx50ⲻ6E
+        {
+            private _5C()
+            {
+            }
+
+            public static _5C Instance { get; } = new _5C();
+        }
+
+        public sealed class _5D : _Ⰳx50ⲻ6E
+        {
+            private _5D()
+            {
+            }
+
+            public static _5D Instance { get; } = new _5D();
+        }
+
+        public sealed class _5E : _Ⰳx50ⲻ6E
+        {
+            private _5E()
+            {
+            }
+
+            public static _5E Instance { get; } = new _5E();
+        }
+
+        public sealed class _5F : _Ⰳx50ⲻ6E
+        {
+            private _5F()
+            {
+            }
+
+            public static _5F Instance { get; } = new _5F();
+        }
+
+        public sealed class _60 : _Ⰳx50ⲻ6E
+        {
+            private _60()
+            {
+            }
+
+            public static _60 Instance { get; } = new _60();
+        }
+
+        public sealed class _61 : _Ⰳx50ⲻ6E
+        {
+            private _61()
+            {
+            }
+
+            public static _61 Instance { get; } = new _61();
+        }
+
+        public sealed class _62 : _Ⰳx50ⲻ6E
+        {
+            private _62()
+            {
+            }
+
+            public static _62 Instance { get; } = new _62();
+        }
+
+        public sealed class _63 : _Ⰳx50ⲻ6E
+        {
+            private _63()
+            {
+            }
+
+            public static _63 Instance { get; } = new _63();
+        }
+
+        public sealed class _64 : _Ⰳx50ⲻ6E
+        {
+            private _64()
+            {
+            }
+
+            public static _64 Instance { get; } = new _64();
+        }
+
+        public sealed class _65 : _Ⰳx50ⲻ6E
+        {
+            private _65()
+            {
+            }
+
+            public static _65 Instance { get; } = new _65();
+        }
+
+        public sealed class _66 : _Ⰳx50ⲻ6E
+        {
+            private _66()
+            {
+            }
+
+            public static _66 Instance { get; } = new _66();
+        }
+
+        public sealed class _67 : _Ⰳx50ⲻ6E
+        {
+            private _67()
+            {
+            }
+
+            public static _67 Instance { get; } = new _67();
+        }
+
+        public sealed class _68 : _Ⰳx50ⲻ6E
+        {
+            private _68()
+            {
+            }
+
+            public static _68 Instance { get; } = new _68();
+        }
+
+        public sealed class _69 : _Ⰳx50ⲻ6E
+        {
+            private _69()
+            {
+            }
+
+            public static _69 Instance { get; } = new _69();
+        }
+
+        public sealed class _6A : _Ⰳx50ⲻ6E
+        {
+            private _6A()
+            {
+            }
+
+            public static _6A Instance { get; } = new _6A();
+        }
+
+        public sealed class _6B : _Ⰳx50ⲻ6E
+        {
+            private _6B()
+            {
+            }
+
+            public static _6B Instance { get; } = new _6B();
+        }
+
+        public sealed class _6C : _Ⰳx50ⲻ6E
+        {
+            private _6C()
+            {
+            }
+
+            public static _6C Instance { get; } = new _6C();
+        }
+
+        public sealed class _6D : _Ⰳx50ⲻ6E
+        {
+            private _6D()
+            {
+            }
+
+            public static _6D Instance { get; } = new _6D();
+        }
+
+        public sealed class _6E : _Ⰳx50ⲻ6E
+        {
+            private _6E()
+            {
+            }
+
+            public static _6E Instance { get; } = new _6E();
         }
     }
 }
