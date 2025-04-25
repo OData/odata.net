1bd375b482479de0ade8b761e3e23930710ddae9
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-25 15:33:49 -0700

    asdf

--- odata/NewStuff/Odata/Headers/Inners/NotDHeaderCharacter.cs
+++ odata/NewStuff/Odata/Headers/Inners/NotDHeaderCharacter.cs
@@ -1,9 +1,39 @@
 ﻿namespace NewStuff.Odata.Headers.Inners
 {
-    public sealed class NotDHeaderCharacter
+    public abstract class NotDHeaderCharacter
     {
         private NotDHeaderCharacter()
         {
+        }
+
+        public sealed class _Ⰳx32ⲻ44 : NotDHeaderCharacter
+        {
+            public _Ⰳx32ⲻ44(_Ⰳx32ⲻ44 _Ⰳx32ⲻ44)
+            {
+                Ⰳx32ⲻ44 = _Ⰳx32ⲻ44;
+            }
+
+            public _Ⰳx32ⲻ44 Ⰳx32ⲻ44 { get; }
+        }
+
+        public sealed class _Ⰳx46ⲻ63 : NotDHeaderCharacter
+        {
+            public _Ⰳx46ⲻ63(_Ⰳx46ⲻ63 _Ⰳx46ⲻ63)
+            {
+                Ⰳx46ⲻ63 = _Ⰳx46ⲻ63;
+            }
+
+            public _Ⰳx46ⲻ63 Ⰳx46ⲻ63 { get; }
+        }
+
+        public sealed class _Ⰳx70ⲻ7E : NotDHeaderCharacter
+        {
+            public _Ⰳx70ⲻ7E(_Ⰳx70ⲻ7E _Ⰳx70ⲻ7E)
+            {
+                Ⰳx70ⲻ7E = _Ⰳx70ⲻ7E;
+            }
+
+            public _Ⰳx70ⲻ7E Ⰳx70ⲻ7E { get; }
         }
     }
 }
--- odata/NewStuff/Odata/Headers/Inners/NotOHeaderCharacter.cs
+++ odata/NewStuff/Odata/Headers/Inners/NotOHeaderCharacter.cs
@@ -8,32 +8,32 @@
 
         public sealed class _Ⰳx32ⲻ4E : NotOHeaderCharacter
         {
-            public _Ⰳx32ⲻ4E(_Ⰳx32ⲻ4E _Ⰳx32ⲻ4E)
+            public _Ⰳx32ⲻ4E(Inners._Ⰳx32ⲻ4E _Ⰳx32ⲻ4E)
             {
-                Ⰳx32ⲻE = _Ⰳx32ⲻ4E;
+                Ⰳx32ⲻ4E = _Ⰳx32ⲻ4E;
             }
 
-            public _Ⰳx32ⲻ4E Ⰳx32ⲻE { get; }
+            public Inners._Ⰳx32ⲻ4E Ⰳx32ⲻ4E { get; }
         }
 
         public sealed class _Ⰳx50ⲻ6E : NotOHeaderCharacter
         {
-            public _Ⰳx50ⲻ6E(_Ⰳx50ⲻ6E _Ⰳx50ⲻ6E)
+            public _Ⰳx50ⲻ6E(Inners._Ⰳx50ⲻ6E _Ⰳx50ⲻ6E)
             {
                 Ⰳx50ⲻ6E = _Ⰳx50ⲻ6E;
             }
 
-            public _Ⰳx50ⲻ6E Ⰳx50ⲻ6E { get; }
+            public Inners._Ⰳx50ⲻ6E Ⰳx50ⲻ6E { get; }
         }
 
         public sealed class _Ⰳx70ⲻ7E : NotOHeaderCharacter
         {
-            public _Ⰳx70ⲻ7E(_Ⰳx70ⲻ7E _Ⰳx70ⲻ7E)
+            public _Ⰳx70ⲻ7E(Inners._Ⰳx70ⲻ7E _Ⰳx70ⲻ7E)
             {
                 Ⰳx70ⲻ7E = _Ⰳx70ⲻ7E;
             }
 
-            public _Ⰳx70ⲻ7E Ⰳx70ⲻ7E { get; }
+            public Inners._Ⰳx70ⲻ7E Ⰳx70ⲻ7E { get; }
         }
     }
 }
