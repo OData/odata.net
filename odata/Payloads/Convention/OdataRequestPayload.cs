2cdf86a796a9166ab52ee9732f1afd9a9df2040e
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-25 10:29:57 -0700

    asdf

---
+++ odata/Payloads/Convention/OdataRequestPayload.cs
@@ -0,0 +1,23 @@
+﻿namespace Payloads.Convention
+{
+    //// TODO i'm trying to define "syntactic" CSTs; it seems like there will need to be a subsequent step to translate from "syntactic" CST to "semantic" CST (to do things like validate the `metadata` format parameters against the `odata-version` header, for example); then, there can be an optional step to convert from a "semantic" CST to a "convention payload" (meaning, a CLR type that doesn't care about ordering or syntax or anything, just about the *meaning* of the values)
+
+    public abstract class OdataRequestPayload
+    {
+        private OdataRequestPayload()
+        {
+        }
+
+        public sealed class Json : OdataRequestPayload
+        {
+            private Json(FormatRequestHeader? accept)
+            {
+                Accept = accept;
+            }
+
+            //// TODO you need to go through the "protocol" doc to capture the things that apply to all requests/responses/etc
+            
+            public FormatRequestHeader? Accept { get; }
+        }
+    }
+}
---
+++ odata/Payloads/SyntacticCst/FormatRequestHeader.cs
@@ -0,0 +1,24 @@
+﻿namespace Payloads
+{
+    public abstract class FormatRequestHeader
+    {
+        private FormatRequestHeader()
+        {
+        }
+
+        /// <summary>
+        /// https://docs.oasis-open.org/odata/odata-json-format/v4.01/odata-json-format-v4.01.html#sec_RequestingtheJSONFormat
+        /// </summary>
+        public sealed class Json : FormatRequestHeader
+        {
+            public Json(Payloads.Json.FormatRequestHeader formatRequestHeader)
+            {
+                FormatRequestHeader = formatRequestHeader;
+            }
+
+            public Payloads.Json.FormatRequestHeader FormatRequestHeader { get; }
+        }
+
+        //// TODO i don't think other formats are allowed by the standard at the moment; you should figure out how custom formats get supported and add that derived type, though
+    }
+}
