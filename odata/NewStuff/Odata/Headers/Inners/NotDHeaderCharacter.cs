e93718bfd20352a82afe5345216dd13e897d58ca
Garrett DeBruin <gdebruin@microsoft.com>
2025-04-25 15:34:05 -0700

    asfd

--- odata/NewStuff/Odata/Headers/Inners/NotDHeaderCharacter.cs
+++ odata/NewStuff/Odata/Headers/Inners/NotDHeaderCharacter.cs
@@ -8,32 +8,32 @@
 
         public sealed class _Ⰳx32ⲻ44 : NotDHeaderCharacter
         {
-            public _Ⰳx32ⲻ44(_Ⰳx32ⲻ44 _Ⰳx32ⲻ44)
+            public _Ⰳx32ⲻ44(Inners._Ⰳx32ⲻ44 _Ⰳx32ⲻ44)
             {
                 Ⰳx32ⲻ44 = _Ⰳx32ⲻ44;
             }
 
-            public _Ⰳx32ⲻ44 Ⰳx32ⲻ44 { get; }
+            public Inners._Ⰳx32ⲻ44 Ⰳx32ⲻ44 { get; }
         }
 
         public sealed class _Ⰳx46ⲻ63 : NotDHeaderCharacter
         {
-            public _Ⰳx46ⲻ63(_Ⰳx46ⲻ63 _Ⰳx46ⲻ63)
+            public _Ⰳx46ⲻ63(Inners._Ⰳx46ⲻ63 _Ⰳx46ⲻ63)
             {
                 Ⰳx46ⲻ63 = _Ⰳx46ⲻ63;
             }
 
-            public _Ⰳx46ⲻ63 Ⰳx46ⲻ63 { get; }
+            public Inners._Ⰳx46ⲻ63 Ⰳx46ⲻ63 { get; }
         }
 
         public sealed class _Ⰳx70ⲻ7E : NotDHeaderCharacter
         {
-            public _Ⰳx70ⲻ7E(_Ⰳx70ⲻ7E _Ⰳx70ⲻ7E)
+            public _Ⰳx70ⲻ7E(Inners._Ⰳx70ⲻ7E _Ⰳx70ⲻ7E)
             {
                 Ⰳx70ⲻ7E = _Ⰳx70ⲻ7E;
             }
 
-            public _Ⰳx70ⲻ7E Ⰳx70ⲻ7E { get; }
+            public Inners._Ⰳx70ⲻ7E Ⰳx70ⲻ7E { get; }
         }
     }
 }
