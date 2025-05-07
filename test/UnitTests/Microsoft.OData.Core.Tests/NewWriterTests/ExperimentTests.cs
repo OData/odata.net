b4155e768fd685f4d0ad422530382451f3d3cb09
Clément Habinshuti <haby_habbes@live.com>
2025-05-07 19:08:17 +0300

    Add complex and collection properties

--- test/UnitTests/Microsoft.OData.Core.Tests/NewWriterTests/ExperimentTests.cs
+++ test/UnitTests/Microsoft.OData.Core.Tests/NewWriterTests/ExperimentTests.cs
@@ -17,9 +17,17 @@ public class ExperimentTests
     public async Task WriteClrPayloadBasedOnIEdmModel()
     {
         var model = new EdmModel();
+
+        var addressEntity = model.AddComplexType("ns", "Address");
+        addressEntity.AddStructuralProperty("City", EdmPrimitiveTypeKind.String);
+        addressEntity.AddStructuralProperty("Country", EdmPrimitiveTypeKind.String);
+
         var customerEntity = model.AddEntityType("ns", "Customer");
         var idProp = customerEntity.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32);
+
         customerEntity.AddStructuralProperty("Name", EdmPrimitiveTypeKind.String);
+        customerEntity.AddStructuralProperty("EmailAddresses", new EdmCollectionTypeReference(new EdmCollectionType(EdmCoreModel.Instance.GetString(false))));
+        customerEntity.AddStructuralProperty("HomeAddress", new EdmComplexTypeReference(addressEntity, true));
 
         var customerEntitySet = model.AddEntityContainer("ns", "DefaultContainer")
             .AddEntitySet("Customers", customerEntity);
@@ -31,12 +39,25 @@ public class ExperimentTests
             new Customer
             {
                 Id = 1,
-                Name = "John Doe"
+                Name = "John Doe",
+                EmailAddresses = ["johndoe@mailer.com"],
+                HomeAddress = new Address
+                {
+                    City = "Nairobi",
+                    Country = "Kenya"
+                }
             },
+            
             new()
             {
                 Id = 2,
-                Name = "Jane Doe"
+                Name = "Jane Doe",
+                EmailAddresses = ["janedoe@mailer.com"],
+                HomeAddress = new Address
+                {
+                    City = "Redmond",
+                    Country = "United States"
+                }
             }
         ];
 
@@ -71,14 +92,29 @@ public class ExperimentTests
         using var reader = new StreamReader(stream);
         var writtenPayload = await reader.ReadToEndAsync();
 
-        var expectedPayload = @"{""@odata.context"":""contextUrl"",""value"":[{""Id"":1,""Name"":""John Doe""},{""Id"":2,""Name"":""Jane Doe""}]}";
+        var expectedPayload =
+            "{\"@odata.context\":\"contextUrl\",\"value\":[{\"Id\":1,\"Name\":\"John Doe\",\"EmailAddresses\":[\"johndoe@mailer.com\"],\"HomeAddress\":{\"City\":\"Nairobi\",\"Country\":\"Kenya\"}},{\"Id\":2,\"Name\":\"Jane Doe\",\"EmailAddresses\":[\"janedoe@mailer.com\"],\"HomeAddress\":{\"City\":\"Redmond\",\"Country\":\"United States\"}}]}";
+            
 
         Assert.Equal(expectedPayload, writtenPayload);
     }
+
+
 
     class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<string> EmailAddresses { get; set; }
+
+        public Address HomeAddress { get; set; }
+
+    }
+
+    class Address
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 }
