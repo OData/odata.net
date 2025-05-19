12eea0f57dc026513e545bb6aa8710f294e00a18
Garrett DeBruin <gdebruin@microsoft.com>
2025-05-19 14:24:11 -0700

    asdf

---
+++ odata/NewStuff/_Design/3_Context/Sample/EmployeeUpdater.cs
@@ -0,0 +1,9 @@
+﻿namespace NewStuff._Design._3_Context.Sample
+{
+    public class EmployeeUpdater
+    {
+        public Employee? Update(string id, string displayName)
+        {
+        }
+    }
+}
