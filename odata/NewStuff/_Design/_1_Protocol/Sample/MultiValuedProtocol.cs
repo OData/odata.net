0b663f291c58a5e7d0d4ce2724e37402f4380a56
Garrett DeBruin <gdebruin@microsoft.com>
2025-05-22 10:25:53 -0700

    asdf

---
+++ odata/NewStuff/_Design/_1_Protocol/Sample/MultiValuedProtocol.cs
@@ -0,0 +1,25 @@
+﻿namespace NewStuff._Design._1_Protocol.Sample
+{
+    public sealed class MultiValuedProtocol : IMultiValuedProtocol
+    {
+        public IGetMultiValuedProtocol Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IGetSingleValuedProtocol Get(KeyPredicate key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPatchSingleValuedProtocol Patch(KeyPredicate key, SingleValuedRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPostSingleValuedProtocol Post(SingleValuedRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
