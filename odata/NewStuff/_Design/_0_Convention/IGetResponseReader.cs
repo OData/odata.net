0e2b0f3e498eb1cc688a2f8144e703fbf7784024
Garrett DeBruin <gdebruin@microsoft.com>
2025-05-23 10:36:05 -0700

    asdf

--- odata/NewStuff/_Design/_0_Convention/IGetResponseReader.cs
+++ odata/NewStuff/_Design/_0_Convention/IGetResponseReader.cs
@@ -33,9 +33,53 @@
 
             public ICustomHeaderReader<IGetResponseHeaderReader> CustomHeaderReader { get; }
         }
+
+        public sealed class Body : GetResponseHeaderToken
+        {
+            private Body()
+            {
+            }
+
+            public IGetResponseBodyReader GetResponseBodyReader { get; }
+        }
     }
 
     public interface IContentTypeHeaderReader
+    {
+        ContentType ContentType { get; }
+
+        IGetResponseHeaderReader Next();
+    }
+
+    public sealed class ContentType
+    {
+        private ContentType()
+        {
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //// TODO you are doing all of this to expose inversion of control; this is what allows the framework to be externally extensible; sure, you could just write code that has a bunch of `if` statements, but then you need to have all of this callback injection everywhere for the cases where the customer wants to do their own thing; having the inversion of control let's you codify the standard without the logic; the logic is inherent in the model
+
+
+
+
+
+
+
+
+
+
+    public interface IGetResponseBodyReader
     {
     }
 }
