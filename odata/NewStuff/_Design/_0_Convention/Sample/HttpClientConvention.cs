e6531c59c6e1b387501b455b6268d6ae3dc0f6cd
Garrett DeBruin <gdebruin@microsoft.com>
2025-05-28 10:12:37 -0700

    asdf

---
+++ odata/NewStuff/_Design/_0_Convention/Sample/HttpClientConvention.cs
@@ -0,0 +1,29 @@
+﻿namespace NewStuff._Design._0_Convention.Sample
+{
+    using System.Net.Http;
+
+    public sealed class HttpClientConvention : IConvention
+    {
+        private readonly HttpClient httpClient;
+
+        public HttpClientConvention(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public IGetRequestWriter Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPatchRequestWriter Patch()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPatchRequestWriter Post()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
