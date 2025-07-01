a521ade01da1f8477069079b219d55835c4ea6d7
John Gathogo <john.gathogo@microsoft.com>
2025-07-01 16:02:29 +0300

    OData client - Do not write @odata.type annotations for declared properties

---
+++ test/EndToEndTests/Tests/Client/Microsoft.OData.Client.E2E.Tests/AnnotationSerializationTests/Client/AnnotationSerializationDataModel.cs
@@ -0,0 +1,29 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="AnnotationSerializationDataModel.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System.Collections.ObjectModel;
+
+namespace Microsoft.OData.Client.E2E.Tests.AnnotationSerializationTests.Client.Models
+{
+    public partial class Order
+    {
+        public OrderStatus NextStatus { get; set; }
+        public ObservableCollection<OrderStatus>? ProhibitedStatuses { get; set; }
+        public ObservableCollection<string>? TagsHistory { get; set; }
+        public Address? PickupAddress { get; set; }
+        public ObservableCollection<Address>? ReturnAddresses { get; set; }
+    }
+
+    public partial class Address
+    {
+        public State? NeighborState { get; set; }
+    }
+
+    public partial class City
+    {
+        public State? State { get; set; }
+    }
+}
