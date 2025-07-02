08024d9cd3e9af004b3c589b1b2ca0208d1c333c
Garrett DeBruin <gdebruin@microsoft.com>
2025-07-02 10:35:53 -0700

    asdf

---
+++ odata/NewStuff/_Design/_a_waas/IOdataService.cs
@@ -0,0 +1,21 @@
+﻿namespace NewStuff._Design._a_waas
+{
+    using System.IO;
+
+    public interface IEdmModel
+    {
+    }
+
+    public interface IDataStoreMapping
+    {
+    }
+
+    public interface IOdataService
+    {
+        static abstract IOdataService Create(IEdmModel edmModel, IDataStoreMapping dataStoreMapping);
+
+        Stream Send(Stream data);
+    }
+
+
+}
