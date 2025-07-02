f74829cd536040caa16b498543b63af6cad53552
Garrett DeBruin <gdebruin@microsoft.com>
2025-07-02 11:13:19 -0700

    asdf

--- odata/NewStuff/_Design/_a_waas/IOdataService.cs
+++ odata/NewStuff/_Design/_a_waas/IOdataService.cs
@@ -16,6 +16,7 @@
 
 
 
+    //// TODO i think all 3 of these should be readers/writers?
     public interface IEdmModel //// TODO should this be an interface or a concrete type?
     {
     }
@@ -33,5 +34,84 @@
         static abstract IConventionOdataService Create(IEdmModel edmModel, IDataStoreMapping dataStoreMapping);
 
         IOdataResponse Send(IOdataRequest request);
+    }
+
+
+
+
+
+
+    public interface IEdmModelParser //// TODO this should be a reader
+    {
+        IEdmModel Parse(Stream csdl);
+    }
+
+    public sealed class FusionOdataServiceSettings
+    {
+        private FusionOdataServiceSettings(IEdmModelParser edmModelParser)
+        {
+            EdmModelParser = edmModelParser;
+        }
+
+        public IEdmModelParser EdmModelParser { get; }
+    }
+
+    public sealed class FusionOdataService : IOdataService
+    {
+        private readonly Stream edmModel;
+        private readonly IDataStoreMapping dataStoreMapping;
+        private readonly IEdmModelParser edmModelParser;
+
+        public static IOdataService Create(Stream edmModel, IDataStoreMapping dataStoreMapping)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static IOdataService Create(Stream edmModel, IDataStoreMapping dataStoreMapping, FusionOdataServiceSettings settings)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private FusionOdataService(Stream edmModel, IDataStoreMapping dataStoreMapping, FusionOdataServiceSettings settings)
+        {
+            this.edmModel = edmModel;
+            this.dataStoreMapping = dataStoreMapping;
+            this.edmModelParser = settings.EdmModelParser;
+        }
+
+        public Stream Send(Stream request)
+        {
+            var parsedEdmModel = this.edmModelParser.Parse(this.edmModel);
+            var fusionConventionOdataService = FusionConventionOdataService.Create(parsedEdmModel, this.dataStoreMapping);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public sealed class FusionConventionOdataService : IConventionOdataService
+    {
+        public static IConventionOdataService Create(IEdmModel edmModel, IDataStoreMapping dataStoreMapping)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IOdataResponse Send(IOdataRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
