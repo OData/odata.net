3115f3af914ef3d981008187b37feaa53cec26d4
Garrett DeBruin <gdebruin@microsoft.com>
2025-07-18 11:19:42 -0400

    asdf

--- odata/NewStuff/_Design/_0_Convention/V2/IGetResponseReader.cs
+++ odata/NewStuff/_Design/_0_Convention/V2/IGetResponseReader.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Security.Cryptography;
     using System.Threading.Tasks;
 
     using NewStuff._Design._0_Convention.V2;
@@ -368,21 +369,49 @@
     {
     }
 
-    public readonly ref struct NonRootObjectPropertyValueToken<TObjectReader>
+    public readonly ref struct NonRootObjectPropertyValueToken
+        <
+            TNonRootObjectPrimitivePropertyValueReader,
+            TNonRootObjectNullPropertyValueReader,
+            TNonRootObjectMultiValuedPropertyValueReader3,
+                TNonRootObjectPrimitiveElementReader, TNonRootObjectMultiValuedPropertyValueReader,
+                TNonRootObjectComplexElementReader, TNonRootObjectReader, TNonRootObjectMultiValuedPropertyValueReader2, //// TODO can this "2" one actually re-use the original?
+            TNonRootObjectComplexPropertyValueReader,
+                TNonRootObjectReader2,
+            TObjectReader
+        >
+        where TNonRootObjectPrimitivePropertyValueReader : INonRootObjectPrimitivePropertyValueReader<TObjectReader>, allows ref struct
+
+        where TNonRootObjectNullPropertyValueReader : INonRootObjectNullPropertyValueReader<TObjectReader>, allows ref struct
+
+        where TNonRootObjectMultiValuedPropertyValueReader3 : INonRootObjectMultiValuedPropertyValueReader<TNonRootObjectPrimitiveElementReader, TNonRootObjectMultiValuedPropertyValueReader, TNonRootObjectComplexElementReader, TNonRootObjectReader, TNonRootObjectMultiValuedPropertyValueReader2, TObjectReader>, allows ref struct
+
+            where TNonRootObjectPrimitiveElementReader : INonRootObjectPrimitiveElementReader<TNonRootObjectPrimitiveElementReader, TNonRootObjectMultiValuedPropertyValueReader, TNonRootObjectComplexElementReader, TNonRootObjectReader, TNonRootObjectMultiValuedPropertyValueReader2, TObjectReader>, allows ref struct
+            where TNonRootObjectMultiValuedPropertyValueReader : INonRootObjectMultiValuedPropertyValueReader<TNonRootObjectPrimitiveElementReader, TNonRootObjectMultiValuedPropertyValueReader, TNonRootObjectComplexElementReader, TNonRootObjectReader, TNonRootObjectMultiValuedPropertyValueReader2, TObjectReader>, allows ref struct //// TODO i'm not clear that this recursion actually works
+
+            where TNonRootObjectComplexElementReader : INonRootObjectComplexElementReader<TNonRootObjectPrimitiveElementReader, TNonRootObjectMultiValuedPropertyValueReader, TNonRootObjectComplexElementReader, TNonRootObjectReader, TNonRootObjectMultiValuedPropertyValueReader2, TObjectReader>, allows ref struct
+            where TNonRootObjectReader : INonRootObjectReader<TNonRootObjectMultiValuedPropertyValueReader2>, allows ref struct
+            where TNonRootObjectMultiValuedPropertyValueReader2 : INonRootObjectMultiValuedPropertyValueReader<TNonRootObjectPrimitiveElementReader, TNonRootObjectMultiValuedPropertyValueReader, TNonRootObjectComplexElementReader, TNonRootObjectReader, TNonRootObjectMultiValuedPropertyValueReader2, TObjectReader>, allows ref struct //// TODO i'm not clear that this recursion actually works
+
+        where TNonRootObjectComplexPropertyValueReader : INonRootObjectComplexPropertyValueReader<TNonRootObjectReader2, TObjectReader>, allows ref struct
+
+            where TNonRootObjectReader2 : INonRootObjectReader<TObjectReader>, allows ref struct
+
+        where TObjectReader : allows ref struct
     {
-        public NonRootObjectPropertyValueToken(INonRootObjectPrimitivePropertyValueReader<TObjectReader> nonRootObjectPrimitivePropertyValueReader)
+        public NonRootObjectPropertyValueToken(TNonRootObjectPrimitivePropertyValueReader nonRootObjectPrimitivePropertyValueReader)
         {
         }
 
-        public NonRootObjectPropertyValueToken(INonRootObjectNullPropertyValueReader<TObjectReader> nonRootObjectNullPropertyValueReader)
+        public NonRootObjectPropertyValueToken(TNonRootObjectNullPropertyValueReader nonRootObjectNullPropertyValueReader)
         {
         }
 
-        public NonRootObjectPropertyValueToken(INonRootObjectMultiValuedPropertyValueReader<TObjectReader> nonRootObjectMultiValuedPropertyValueReader)
+        public NonRootObjectPropertyValueToken(TNonRootObjectMultiValuedPropertyValueReader3 nonRootObjectMultiValuedPropertyValueReader)
         {
         }
 
-        public NonRootObjectPropertyValueToken(INonRootObjectComplexPropertyValueReader<TObjectReader> nonRootObjectComplexPropertyValueReader)
+        public NonRootObjectPropertyValueToken(TNonRootObjectComplexPropertyValueReader nonRootObjectComplexPropertyValueReader)
         {
         }
 
