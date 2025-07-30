af704e77f4bd13d33338c8b38bc6fbe999ca1abe
Garrett DeBruin <gdebruin@microsoft.com>
2025-07-30 13:40:15 -0400

    asdf

---
+++ odata/NewStuff/_Design/_0_Convention/V3/IEntityIdReader.cs
@@ -0,0 +1,16 @@
+﻿namespace NewStuff._Design._0_Convention.V3
+{
+    public interface IEntityIdReader<out TNextReader> : IReader<EntityId, TNextReader>
+    {
+    }
+
+    //// TODO you are in 4.1 of this doc: https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part1-protocol.html
+    //// you are trying to decide if you should go ahead and dive into iri parsing (and if you don't, what will it look like when you decide that you want to, because up to this point, you would have called the entry point reader for the "broken down" version `ientityidreader`, but then that would conflict with the current name); and also, how it's going to look to write an `entityid` when you can only write uris and not iris; basically, what should you do when reading and writing are not symmetric
+
+    public sealed class EntityId
+    {
+        private EntityId()
+        {
+        }
+    }
+}
--- odata/NewStuff/_Design/_0_Convention/V3/IReader.cs
+++ odata/NewStuff/_Design/_0_Convention/V3/IReader.cs
@@ -3,48 +3,22 @@
     using System.Diagnostics.CodeAnalysis;
     using System.Threading.Tasks;
 
-    public interface IReader<TNext> where TNext : allows ref struct
+    public interface IReader<out TNext> where TNext : allows ref struct
     {
         ValueTask Read();
 
         /// <summary>
         /// 
         /// </summary>
-        /// <returns><see langword="null"/> if <see cref="IReader{T}.Read"/> needs to be called first</returns>
-        RefNullable<TNext> Next();
+        /// <param name="moved"><see langword="false"/> if <see cref="IReader{TNext}.Read"/> needs to be called first</param>
+        /// <returns></returns>
+        TNext TryMoveNext(out bool moved);
     }
 
-    public interface IReader<TNext, TValue> : IReader<TNext>
-        where TNext : allows ref struct
+    public interface IReader<out TValue, out TNext> : IReader<TNext>
         where TValue : allows ref struct
+        where TNext : allows ref struct
     {
         TValue Value { get; }
-    }
-
-    public readonly ref struct RefNullable<T> where T : allows ref struct
-    {
-        private readonly T value;
-
-        public RefNullable(T value)
-        {
-            this.value = value;
-            this.HasValue = true;
-        }
-
-        public bool HasValue { get; }
-
-        public bool TryGetValue([MaybeNullWhen(false)] out T value)
-        {
-            if (this.HasValue)
-            {
-                value = this.value;
-                return true;
-            }
-            else
-            {
-                value = default;
-                return false;
-            }
-        }
     }
 }
