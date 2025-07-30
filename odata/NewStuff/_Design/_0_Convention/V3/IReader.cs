7eb8116ec66db3894b7affaaddc10ac15c56a54d
Garrett DeBruin <gdebruin@microsoft.com>
2025-07-30 13:00:40 -0400

    asdf

---
+++ odata/NewStuff/_Design/_0_Convention/V3/IReader.cs
@@ -0,0 +1,50 @@
+﻿namespace NewStuff._Design._0_Convention.V3
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+
+    public interface IReader<TNext> where TNext : allows ref struct
+    {
+        ValueTask Read();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns><see langword="null"/> if <see cref="IReader{T}.Read"/> needs to be called first</returns>
+        RefNullable<TNext> Next();
+    }
+
+    public interface IReader<TNext, TValue> : IReader<TNext>
+        where TNext : allows ref struct
+        where TValue : allows ref struct
+    {
+        TValue Value { get; }
+    }
+
+    public readonly ref struct RefNullable<T> where T : allows ref struct
+    {
+        private readonly T value;
+
+        public RefNullable(T value)
+        {
+            this.value = value;
+            this.HasValue = true;
+        }
+
+        public bool HasValue { get; }
+
+        public bool TryGetValue([MaybeNullWhen(false)] out T value)
+        {
+            if (this.HasValue)
+            {
+                value = this.value;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+    }
+}
