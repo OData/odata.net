e6c8af9f1d85bc2e19acf7300a5c927546e5afa1
Garrett DeBruin <gdebruin@microsoft.com>
2025-07-31 11:27:10 -0400

    asdf

--- odata/NewStuff/_Design/_0_Convention/V3/IEntityIdReader.cs
+++ odata/NewStuff/_Design/_0_Convention/V3/IEntityIdReader.cs
@@ -4,6 +4,14 @@
     {
     }
 
+    public interface IEntityIdStartReader<out TNextReader> : IReader<IIriSchemeReader<TNextReader>>
+    {
+    }
+
+    public interface IIriSchemeReader<out TNextReader>
+    {
+    }
+
     //// TODO you are in 4.1 of this doc: https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part1-protocol.html
     //// you are trying to decide if you should go ahead and dive into iri parsing (and if you don't, what will it look like when you decide that you want to, because up to this point, you would have called the entry point reader for the "broken down" version `ientityidreader`, but then that would conflict with the current name); and also, how it's going to look to write an `entityid` when you can only write uris and not iris; basically, what should you do when reading and writing are not symmetric
 
