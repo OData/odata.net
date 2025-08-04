2c0ef1d0f251e80921950f891012fc3fbc52e662
Garrett DeBruin <gdebruin@microsoft.com>
2025-08-04 15:02:30 -0400

    asdf

--- odata/NewStuff/_Design/_0_Convention/V3/IEntityIdReader.cs
+++ odata/NewStuff/_Design/_0_Convention/V3/IEntityIdReader.cs
@@ -7,6 +7,8 @@ namespace NewStuff._Design._0_Convention.V3
     //// TODO you are in 4.1 of this doc: https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part1-protocol.html
     //// you are trying to decide if you should go ahead and dive into iri parsing (and if you don't, what will it look like when you decide that you want to, because up to this point, you would have called the entry point reader for the "broken down" version `ientityidreader`, but then that would conflict with the current name); and also, how it's going to look to write an `entityid` when you can only write uris and not iris; basically, what should you do when reading and writing are not symmetric
 
+    //// TODO i think the `attempt2` namespace is the right track; finish the extension method, then implement some readers, and then write some fake calling code
+
     public interface IEntityIdHeaderReader<out TNextReader> : IReader<EntityIdHeader, TNextReader>
     {
     }
@@ -161,26 +163,52 @@ namespace NewStuff._Design._0_Convention.V3
             {
                 public static IReader<V1.IEntityIdReader<TNextReader>> ToV1<TNextReader>(this IReader<IEntityIdStartReader<TNextReader>> reader)
                 {
-                    return new EntityIdReader<TNextReader>(reader);
+                    return new EntityIdHeaderValueReader<TNextReader>(reader);
                 }
 
-                private sealed class EntityIdReader<TNextReader> : IReader<V1.IEntityIdReader<TNextReader>>
+                private sealed class EntityIdHeaderValueReader<TNextReader> : IReader<V1.IEntityIdReader<TNextReader>>
                 {
                     private readonly IReader<IEntityIdStartReader<TNextReader>> reader;
 
-                    public EntityIdReader(IReader<IEntityIdStartReader<TNextReader>> reader)
+                    public EntityIdHeaderValueReader(IReader<IEntityIdStartReader<TNextReader>> reader)
                     {
                         this.reader = reader;
                     }
 
                     public ValueTask Read()
                     {
-                        throw new NotImplementedException();
+                        return this.reader.Read();
                     }
 
                     public V1.IEntityIdReader<TNextReader> TryMoveNext(out bool moved)
                     {
+                        if (!this.reader.TryMoveNext2(out var iriSchemeReader))
+                        {
+                            moved = false;
+                            return default;
+                        }
+
                         throw new NotImplementedException();
+                    }
+
+                    private sealed class EntityIdReader : V1.IEntityIdReader<TNextReader>
+                    {
+                        public EntityIdReader()
+
+                        public ValueTask Read()
+                        {
+                            throw new NotImplementedException();
+                        }
+
+                        public EntityId TryGetValue(out bool moved)
+                        {
+                            throw new NotImplementedException();
+                        }
+
+                        public TNextReader TryMoveNext(out bool moved)
+                        {
+                            throw new NotImplementedException();
+                        }
                     }
                 }
             }
