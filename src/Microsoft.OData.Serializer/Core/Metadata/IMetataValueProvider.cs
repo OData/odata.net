3fc91e40ebfb5ea0c2edb4c13021adf5494953cd
Clément Habinshuti <haby_habbes@live.com>
2025-08-22 09:35:35 +0300

    Delete older V2  iteration from new serializer package

--- src/Microsoft.OData.Serializer/Core/Metadata/IMetataValueProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.OData.Serializer.Core;
-
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1005:Avoid excessive parameters on generic types", Justification = "<Pending>")]
-public interface IMetadataValueProvider<TContext, TState, TProperty> :
-    ICollectionCounterProvider<TContext, TState, TProperty>,
-    INextLinkHandlerProvider<TContext, TState, TProperty>,
-    IEtagHandlerProvider<TContext, TState>
-{
-}
--- src/Microsoft.OData.Serializer/Core/Resource/IResourceWriterProvider.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Microsoft.OData.Serializer.Core;
-
-public interface IResourceWriterProvider<TContext, TState>
-{
-    IODataWriter<TContext, TState, TValue> GetResourceWriter<TValue>(TContext context, TState state);
-}
