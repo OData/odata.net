7c7ae5bbb473e045ac36382b7a028415c7f4f4c8
Garrett DeBruin <gdebruin@microsoft.com>
2025-08-25 10:46:20 -0400

    asdf

---
+++ _ReaderCaller/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _ReaderCaller
+{
+    using System.Threading.Tasks;
+
+    using ImplementationV1;
+    using Odata;
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var entityIdHeaderValueReader = new EntityIdHeaderValueReader<object>();
+            var nextReader = await Read(entityIdHeaderValueReader).ConfigureAwait(false);
+        }
+        
+        public static async Task<TNextReader> Read<TNextReader>(IEntityIdHeaderValueReader<Version.V1, TNextReader> entityIdHeaderValueReader)
+        {
+            IEntityIdReader<Version.V1, TNextReader> entityIdReader;
+            while (!entityIdHeaderValueReader.TryMoveNext2(out entityIdReader))
+            {
+                await entityIdHeaderValueReader.Read().ConfigureAwait(false);
+            }
+
+            TNextReader next;
+            while (!entityIdReader.TryMoveNext2(out next))
+            {
+                await entityIdReader.Read().ConfigureAwait(false);
+            }
+
+            return next;
+        }
+    }
+}
