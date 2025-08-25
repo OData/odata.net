89e09c39b8f79a0fa431806fc6a4b4decd5cf971
Garrett DeBruin <gdebruin@microsoft.com>
2025-08-25 11:17:22 -0400

    asdf

--- _ReaderImplementer1/Program.cs
+++ _ReaderImplementer1/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace _ReaderImplementer1
 {
+    using System.Threading.Tasks;
+
+    using Odata;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("success!");
+        }
+    }
+
+    public sealed class EntityIdHeaderValueReader<TNextReader> : IEntityIdHeaderValueReader<Version.V1, TNextReader>
+    {
+        private readonly IEntityIdHeaderValueReader<Version.V1, TNextReader> delegateReader;
+
+        public EntityIdHeaderValueReader(IEntityIdHeaderValueReader<Version.V1, TNextReader> delegateReader)
+        {
+            this.delegateReader = delegateReader;
+        }
+
+        public Version.V1 Version { get; } = Odata.Version.V1.Instance;
+
+        public ValueTask Read()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEntityIdReader<Version.V1, TNextReader> TryMoveNext(out bool moved)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
