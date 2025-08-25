75d25269a53cd95d9f8f26030d427de8f51e78b6
Clément Habinshuti <clhabins@microsoft.com>
2025-08-25 12:04:59 +0300

    Create buffered reader interface

---
+++ src/Microsoft.OData.Serializer/V3/IO/IBufferedReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OData.Serializer.V3.IO;
+
+internal interface IBufferedReader<T> : IAsyncDisposable where T : struct
+{
+    bool TryRead(out BufferedReadResult<T> result);
+
+    ValueTask<BufferedReadResult<T>> ReadAsync();
+
+    void AdvanceTo(in SequencePosition consumed);
+
+    void AdvanceTo(in SequencePosition consumed, in SequencePosition position);
+}
