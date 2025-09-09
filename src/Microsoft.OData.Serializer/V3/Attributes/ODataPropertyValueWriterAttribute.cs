b7fd45e99d2f18547e117b210bc93d23d2847613
Clément Habinshuti <haby_habbes@live.com>
2025-09-09 11:38:48 +0300

    Add value writer attributes

---
+++ src/Microsoft.OData.Serializer/V3/Attributes/ODataPropertyValueWriterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OData.Serializer.V3.Attributes;
+
+/// <summary>
+/// The attribute is applied on a class to specify a custom writer for a specific property
+/// that is not necessarily defined on the class.
+/// </summary>
+/// <param name="propertyName"></param>
+/// <param name="writerType">A custom type that extends one of the ODataPropertyValueWriter variants.</param>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public sealed class ODataPropertyValueWriterAttribute(string propertyName, Type writerType) : Attribute
+{
+    public string PropertyName { get; } = propertyName;
+    public Type WriterType { get; } = writerType;
+}
---
+++ src/Microsoft.OData.Serializer/V3/Attributes/ODataValueWriterAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OData.Serializer.V3.Attributes;
+
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public sealed class ODataValueWriterAttribute(Type writerType) : Attribute
+{
+    public Type WriterType { get; } = writerType;
+}
