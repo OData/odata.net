76eeda2423ba7bc9cff1ad957b2f8a8fcbe1cfc8
Clément Habinshuti <haby_habbes@live.com>
2025-09-26 15:53:13 +0300

    Add global suppression to avoid warnings on multiple generic parameters

---
+++ src/Microsoft.OData.Serializer/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Design", "CA1005:Avoid excessive parameters on generic types",
+    Justification = "The design of the library depends on having multiple generic types to provide flexibility while minimizing allocations.",
+    Scope = "namespace",
+    Target = "~N:Microsoft.OData.Serializer")]
