fcc26b0dc33a7eabe020383b14ab1cceaec10c05
Clément Habinshuti <haby_habbes@live.com>
2025-09-29 06:13:02 +0300

    Start creating FileItem class

---
+++ samples/ODataSamples.FileServiceLib/Models/FileItem.cs
@@ -0,0 +1,24 @@
+﻿using ODataSamples.FileServiceLib.Schema.Abstractions;
+using ODataSamples.FileServiceLib.Schema.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODataSamples.FileServiceLib.Models;
+
+public class FileItem(IFileItemSchema schema, IDictionary<IPropertyDefinition, object?> data) : ISchematizedObject<IFileItemSchema>
+{
+    public IFileItemSchema Schema => schema;
+
+    public IDictionary<IPropertyDefinition, object?> Data => data;
+
+    public string Id
+    {
+        get => (string)this.Data[this.Schema.Id]!;
+        set => this.Data[this.Schema.Id] = value;
+    }
+
+
+}
