09c660cd7adae346f333c72be2f1a811d192188a
Clément Habinshuti <haby_habbes@live.com>
2025-09-29 05:18:39 +0300

    Create sample data class

---
+++ samples/ODataSamples.FileServiceLib/SampleData/SampleData.cs
@@ -0,0 +1,16 @@
+﻿using ODataSamples.FileServiceLib.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODataSamples.FileServiceLib.SampleData;
+
+public static class SampleData
+{
+    public static FindFileResponse CreateMultiFileResponseData()
+    {
+         
+    }
+}
