778855665a696ae5dc2f7f13bcea2ef8a5e15f06
Clément Habinshuti <haby_habbes@live.com>
2025-11-22 20:19:31 +0300

    Implement async enumerable element selector (inefficiently)

--- src/Microsoft.OData.Serializer/Adapters/ODataElementAsyncEnumerableSelector.cs
+++ src/Microsoft.OData.Serializer/Adapters/ODataElementAsyncEnumerableSelector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,60 @@ public class ODataElementAsyncEnumerableSelector<TResource, TElement, TCustomSta
 
     internal override bool WriteElements(TResource resource, ODataWriterState<TCustomState> state)
     {
-        throw new NotImplementedException();
+        IAsyncEnumerator<TElement> enumerator;
+        if (state.Stack.Current.CurrentEnumerator == null)
+        {
+            var elements = GetElements(resource, state);
+            enumerator = elements.GetAsyncEnumerator();
+            state.Stack.Current.CurrentEnumerator = enumerator;
+            // TODO: This may create too many tasks allocation,
+            // consider optimizing by avoiding AsTask() if value task already completed successfully
+            Task<bool> moveNextTask = enumerator.MoveNextAsync().AsTask();
+            state.Stack.Current.PendingTaskWithValue = moveNextTask;
+
+            // The task will be awaited at the serializer root.
+            // TODO: we are returning to the root for each iteration,
+            // we should avoid the overhead of the return to the root if the
+            // task is already completed successfully.
+            return false;
+        }
+
+        // Retrieve the enumerator from the state so we can resume writing from where we left off.
+        enumerator = (state.Stack.Current.CurrentEnumerator as IAsyncEnumerator<TElement>)!;
+        Debug.Assert(enumerator != null, "CurrentEnumerator should be of type IEnumerator<TProperty>. Possible bug in state management and property resume operation.");
+        bool didMoveNextSucceed = true;
+        var pendingTask = state.Stack.Current.PendingTaskWithValue;
+        if (pendingTask != null)
+        {
+            Task<bool> moveNextTask = pendingTask as Task<bool>;
+            Debug.Assert(moveNextTask != null, "PendingTaskWithValue should be Task<bool> when resuming an async enumerable.");
+            Debug.Assert(moveNextTask.IsCompletedSuccessfully, "PendingTaskWithValue should be completed successfully when resuming an async enumerable.");
+            didMoveNextSucceed = moveNextTask.GetAwaiter().GetResult();
+            state.Stack.Current.PendingTaskWithValue = null;
+        }
+
+        if (!didMoveNextSucceed)
+        {
+            // we reached the end of the collection;
+            return true;
+        }
+
+        var item = enumerator.Current;
+        if (!WriteElementImplementation(resource, item, state))
+        {
+            return false;
+        }
+
+        state.Stack.EndProperty();
+
+        state.Stack.Current.PendingTaskWithValue = enumerator.MoveNextAsync().AsTask();
+        return false;
+    }
+
+    protected virtual bool WriteElementImplementation(TResource resource, TElement element, ODataWriterState<TCustomState> state)
+    {
+        Debug.Assert(WriteElement != null, "WriteProperty should not be null if this method is called.");
+
+        return WriteElement(resource, element, DefaultValueWriter<TCustomState>.Instance, state);
     }
 }
