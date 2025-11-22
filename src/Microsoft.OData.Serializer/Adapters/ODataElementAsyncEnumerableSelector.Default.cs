634d42766f6cb0b8941ac125d8ac3afa1683904d
Clément Habinshuti <haby_habbes@live.com>
2025-11-22 20:16:14 +0300

    Create test for async elements selector

---
+++ src/Microsoft.OData.Serializer/Adapters/ODataElementAsyncEnumerableSelector.Default.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OData.Serializer;
+
+public sealed class ODataElementAsyncEnumerableSelector<TResource, TElement>
+    : ODataElementAsyncEnumerableSelector<TResource, TElement, DefaultState>
+{
+}
---
+++ src/Microsoft.OData.Serializer/Adapters/ODataElementAsyncEnumerableSelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OData.Serializer;
+
+public class ODataElementAsyncEnumerableSelector<TResource, TElement, TCustomState>
+    : ODataElementSelector<TResource, TCustomState>
+{
+    public required Func<TResource, ODataWriterState<TCustomState>, IAsyncEnumerable<TElement>> GetElements { get; set; }
+
+    public Func<TResource, TElement, IValueWriter<TCustomState>, ODataWriterState<TCustomState>, bool>? WriteElement { get; set; }
+
+    internal override bool WriteElements(TResource resource, ODataWriterState<TCustomState> state)
+    {
+        throw new NotImplementedException();
+    }
+}
